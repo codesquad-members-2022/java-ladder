efde7ffd653e6d827d1c392796a02257fb115762
nori <nohriter@gmail.com>
2022-02-14 17:24:45 +0900

    플레이어의 수와 사다리의 개수 입력받기 기능 리팩토링
    코드를 메소드로 분리하였음

--- src/App.java
+++ src/App.java
@@ -6,12 +6,22 @@ public class App {
 
     public static void main(String[] args) {
 
-        // n명의 사람과 m개의 사다리 개수를 입력할 수 있어야 한다.
-        System.out.println("참여할 사람은 몇 명인가요?");
-        int playerCount = sc.nextInt();
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        int ladderHeight = sc.nextInt();
+        // n명의 사람의 수를 입력할 수 있어야 한다.
+        int playerCount = inputRules("참여할 사람은 몇 명인가요?");
+
+        // m개의 사다리 개수를 입력할 수 있어야 한다.
+        int ladderHeight = inputRules("최대 사다리 높이는 몇 개인가요?");
+
+        
+
 
     }
+
+    private static int inputRules(String x) {
+        System.out.println(x);
+        return sc.nextInt();
+    }
+
+
 
 }
