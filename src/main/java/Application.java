c669f9bb7609df80943a7b3c0a29252adc9a33f3
WooJin Park <ak2j38@gmail.com>
2022-02-14 19:00:15 +0900

    feature : 게임 진행을 위한 메인 메소드 추가
    메인 메소드로 사다리 게임 시작
    
    end

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+import controller.GameController;
+
+public class Application {
+
+    public static void main(String[] args) {
+        GameController ladderGame = new GameController();
+        ladderGame.ladderGameStart();
+    }
+}
