845cd717c190f662e59cf3637efdd44fad31dacf
vans <spentmny@nate.com>
2022-02-14 23:10:55 +0900

    Launcehr 클래스에서 프로그램 실행 역할을 할 run() 메소드 구현

--- Level01/App.java
+++ Level01/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
-
+        Launcher launcher = new Launcher();
+        launcher.run();
     }
 }
--- Level01/Launcher.java
+++ Level01/Launcher.java
@@ -1,2 +1,13 @@
 public class Launcher {
+    private int player;
+    private int height;
+    private String[][] result;
+
+    public void run() {
+        player = Print.inputPlayer();
+        height = Print.inputHeight();
+        result = makeLadder(player,height);
+
+        Print.result(result);
+    }
 }
