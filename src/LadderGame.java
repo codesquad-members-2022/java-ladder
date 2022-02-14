adfd6c3f0158517b038c2c0638f0532012303af3
hsjang <hsjang0405@gmail.com>
2022-02-14 11:04:46 +0900

    ✨  유저의 입력을 받는 Input Class 구현

--- src/LadderGame.java
+++ src/LadderGame.java
@@ -1,3 +1,23 @@
 public class LadderGame {
 
+	private char[][] ladder;
+
+	private static final int USER_NUMBER_IDX = 0;
+	private static final int MAX_LADDER_HEIGHT_IDX = 1;
+	private int userNumber;
+	private int ladderHeight;
+
+	public void init() {
+		int[] userInput = Input.getUserInput();
+		userNumber = userInput[USER_NUMBER_IDX];
+		ladderHeight = userInput[MAX_LADDER_HEIGHT_IDX];
+	}
+
+	public void start() {
+
+
+
+	}
+
+
 }
