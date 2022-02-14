f841553bb8524df3f1b8dea15bdb695470898f83
geombong <shoy1415@naver.com>
2022-02-14 23:05:07 +0900

    사다리생성, 출력 기능 추가

--- ladder/src/main/java/ladder/Ladder.java
+++ ladder/src/main/java/ladder/Ladder.java
@@ -1,12 +1,39 @@
 package ladder;
 
+import java.util.Random;
+
 public class Ladder {
 
     private final int userCount;
     private final int ladderHigh;
+    private static final String[] ladderLineElement = {" ", "-", "-"};
+
     public Ladder(int userCount, int ladderHigh) {
         this.userCount = userCount;
         this.ladderHigh = ladderHigh;
+        createLadder();
     }
 
+    private void createLadder() {
+        String[][] ladder = new String[ladderHigh][userCount + (userCount - 1)];
+        Random random = new Random(System.currentTimeMillis());
+        for (int i = 0; i < ladder.length; i++) {
+            for (int j = 0; j < ladder[i].length; j++) {
+                if (j % 2 == 0) {
+                    ladder[i][j] = "|";
+                    continue;
+                }
+                ladder[i][j] = ladderLineElement[random.nextInt(3)];
+                if (j != 1 && ladder[i][j - 2].equals(ladderLineElement[1])) {
+                    ladder[i][j] = ladderLineElement[0];
+                }
+            }
+        }
+        printLadder(ladder);
+    }
+
+    public void printLadder(String[][] ladder) {
+        Screen screen = new Screen();
+        screen.printLadder(ladder);
+    }
 }
--- ladder/src/main/java/ladder/Screen.java
+++ ladder/src/main/java/ladder/Screen.java
@@ -9,4 +9,16 @@ public class Screen {
     public void printLadderMaxHighBar() {
         System.out.println(PrintString.LADDER_MAX_HIGH);
     }
+
+    public void printLadder(String[][] ladder) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n");
+        for (String[] strings : ladder) {
+            for (String string : strings) {
+                sb.append(string);
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
 }
