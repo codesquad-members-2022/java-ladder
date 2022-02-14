0c6ed09282c53888b050516e4555092eb8d89eba
who-hoo <who.ho3ov@gmail.com>
2022-02-14 17:44:16 +0900

    :sparkles: 입력 예외 처리 구현

--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -1,9 +1,11 @@
+import io.InputValidator;
 import io.InputView;
 import io.OutputView;
 
 public class LadderGame {
 
     private final InputView input = new InputView();
+    private final InputValidator validator = new InputValidator();
     private final OutputView output = new OutputView();
     private Ladder ladder;
 
@@ -12,10 +14,27 @@ public class LadderGame {
     }
 
     private void initGame() {
-        // TODO: 입력 예외 처리
-        int entry = Integer.parseInt(input.getEntry());
-        int height = Integer.parseInt(input.getHeight());
+        int entry = getEntry();
+        int height = getHeight();
         this.ladder = new Ladder(entry, height);
         output.print(ladder.toString());
+    }
+
+    private int getEntry() {
+        try {
+            return validator.positiveInteger(input.getEntry());
+        } catch (IllegalArgumentException e) {
+            output.print(e.getMessage());
+            return getEntry();
+        }
+    }
+
+    private int getHeight() {
+        try {
+            return validator.positiveInteger(input.getHeight());
+        } catch (IllegalArgumentException e) {
+            output.print(e.getMessage());
+            return getEntry();
+        }
     }
 }
