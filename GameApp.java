d08256062b050ea053e1c25d2257f32debe51f66
ikjo93 <auddlr100@naver.com>
2022-02-14 23:08:23 +0900

    GameApp / UserInterface / GameDisplay 클래스 생성 및 Main 클래스 수정
    사용자 입력 기능, 화면(콘솔) 출력 기능 구현

---
+++ GameApp.java
@@ -0,0 +1,10 @@
+public class GameApp {
+
+    public void run() {
+        UserInterface ui = new UserInterface();
+        int personCount = ui.inputPersonCount(), ladderHeight = ui.inputLadderCount();
+
+    }
+
+
+}
---
+++ GameDisplay.java
@@ -0,0 +1,11 @@
+public class GameDisplay {
+
+    public static void showGuideToPersonCount() {
+        System.out.println("참여할 사람은 몇 명인가요?");
+    }
+
+    public static void showGuideToLadderHeight() {
+        System.out.println("최대 사다리 높이는 몇개 인가요?");
+    }
+
+}
