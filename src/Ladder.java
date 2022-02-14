c854a326d5167dd8a0340c42660acdbac4ea91d6
leekm0310 <leekm310@gmail.com>
2022-02-14 17:52:41 +0900

    사다리게임 - Ladder 클래스 - makeLadder2 메소드
    makeLadder2 메소드에서 사다리를 출력하기 위해 기호를 배열에 넣음.

--- src/Ladder.java
+++ src/Ladder.java
@@ -4,4 +4,15 @@ public class Ladder {
     public void makeLadder(int people, int height) {
         this.ladder = new String[height][people];
     }
+
+    public void makeLadder2(String[][] ladder){
+        for (int i = 0; i < ladder.length; i++) {
+            for (int j = 0; j < ladder[i].length; j++) {
+                if (j%2 == 0){
+                    ladder[i][j] ="|";
+                }
+            }
+        }
+        this.ladder = ladder;
+    }
 }
