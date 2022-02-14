757ee3664e1dfdd8ecd3c3a5379786312fd4bc5a
WooJin Park <ak2j38@gmail.com>
2022-02-14 18:38:45 +0900

    사다리 객체
    사다리 모형 생성을 위해 짝수컬럼 초기화, 홀수 컬럼 초기화, 랜덤 논리형반환 함수를 추가
    
    end

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Random;
+
 public class Ladder {
 
     private int playersCount;
@@ -9,6 +11,34 @@ public class Ladder {
     public Ladder(int playersCount, int maxLadderHeight) {
         this.playersCount = playersCount;
         this.maxLadderHeight = maxLadderHeight;
-        ladder = new String[this.maxLadderHeight][(2 * this.playersCount) - 1];
+        this.ladder = new String[this.maxLadderHeight][(2 * this.playersCount) - 1];
+    }
+
+    public void generateLadder() {
+        initEvenColumn();
+        initOddColumn();
+    }
+
+    private void initEvenColumn() {
+        for (int i = 0; i < this.maxLadderHeight; i++) {
+            for (int j = 0; j < (2 * this.playersCount) - 1; j = j + 2) {
+                ladder[i][j] = "|";
+            }
+        }
+    }
+
+    private void initOddColumn() {
+        for (int i = 0; i < this.maxLadderHeight; i++) {
+            for (int j = 1; j < (2 * this.playersCount) - 1; j = j + 2) {
+                ladder[i][j] = " ";
+                if (getRandomBoolean()) {
+                    ladder[i][j] = "-";
+                }
+            }
+        }
+    }
+
+    private boolean getRandomBoolean() {
+        return new Random().nextBoolean();
     }
 }
