2962998b0ed6ac799de1657831e08c4660983fc0
seyeong <seyoung7555@naver.com>
2022-02-14 17:10:55 +0900

    feat: 미션 1 구현 완료
    - LadderGameManager : 사다리 게임의 진행을 담당하는 클래스 별도 생성
    - Ladder : 내부에 사다리 배열를 만드는 로직 분리

--- src/main/Application.java
+++ src/main/Application.java
@@ -6,8 +6,9 @@ import main.Controller.OutputController;
 public class Application {
     public static void main(String[] args) {
         // 사다리 게임 어플리케이션 실행부
-        int[] inputs = InputController.inputNumber();
-        Ladder ladder = new Ladder(inputs[0], inputs[1]);
-        OutputController.showLadder(ladder.getLadderArr());
+        LadderGameManager manager = new LadderGameManager();
+
+        manager.init();
+        manager.showLadder();
     }
 }
--- src/main/Ladder.java
+++ src/main/Ladder.java
@@ -7,7 +7,6 @@ public class Ladder {
     private final int height;
     private final int space;
     private final String[][] ladderArr;
-
     public Ladder(int userNum, int height) {
         this.userNum = userNum;
         this.height = height;
@@ -18,13 +17,21 @@ public class Ladder {
     private String[][] createLadder() {
         String[][] result = new String[height][userNum + space];
         IntStream.range(0, height).forEach(i -> IntStream.range(0, userNum + space).forEach(j -> {
-            if (j % 2 == 0) {
-                result[i][j] = "|";
-                return;
-            }
-            result[i][j] = " ";
+            generate(result, i, j);
         }));
         return result;
+    }
+
+    private void generate(String[][] result, int i, int j) {
+        if (j % 2 == 0) {
+            result[i][j] = "|";
+            return;
+        }
+        if (Math.random() > 0.5) {
+            result[i][j] = "-";
+            return;
+        }
+        result[i][j] = " ";
     }
 
     public String[][] getLadderArr() {
