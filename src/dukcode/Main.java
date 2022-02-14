1345a6b27372ef4238e76fe4a7d4f81c22caf92e
dukcode <duk9741@gmail.com>
2022-02-15 00:08:43 +0900

    Ladder getter를 삭제했습니다

---
+++ src/dukcode/Main.java
@@ -0,0 +1,23 @@
+package dukcode;
+
+import dukcode.controller.LadderController;
+import dukcode.model.Ladder;
+import dukcode.view.PrintView;
+import dukcode.view.ScanView;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ScanView scanView = new ScanView(sc);
+        Ladder ladder = new Ladder();
+        PrintView printView = new PrintView(ladder);
+        LadderController ladderController = new LadderController(printView, ladder);
+
+        ladderController.initLadder(scanView.getNumPlayer(), scanView.getHeight());
+        ladderController.updatePrintView();
+        sc.close();
+    }
+}
