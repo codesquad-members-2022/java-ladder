346cfab1b8b84ab6369eebbcf87301c5b3a9dbac
rkolx <olx.rko.o@gmail.com>
2022-02-15 00:22:43 +0900

    refactor: Ladder 클래스 내 MakeLadder클래스(사다리 틀) 구현

--- src/Ladder.java
+++ src/Ladder.java
@@ -1,23 +1,21 @@
 import java.util.Random;
 
 public class Ladder {
-    public void Player(int player, int height) {
-        String[][] ladder = new String[height][player * 2 - 1];
+
+    Random ran = new Random();
+
+
+    private void MakeLadder(String[][] ladder){
         String[] RandomLine = {" ", "-"};
-
-        Random ran = new Random();
-
         for (int i = 0; i < ladder.length; i++) {
             for (int j = 0; j < ladder[i].length; j++) {
-
-                if (j % 2 == 0) {
+                if (j % 2 == 0){
                     ladder[i][j] = "|";
                 } else {
                     ladder[i][j] = RandomLine[ran.nextInt(2)];
                 }
-                System.out.print(ladder[i][j]);
             }
-            System.out.println();
         }
     }
+
 }
