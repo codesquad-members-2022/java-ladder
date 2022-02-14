ab9344aad7e9f91213018004a0f70ab0e49fb3af
PhilSoGooood <peelhw@gmail.com>
2022-02-14 23:51:59 +0900

    getInput 메서드의 접근제어자를 private에서 default로 변경
    메인메서드에서 InputView 클래스의 인스턴스를 생성하고 getInput메서드를
    사용하기 위해 접근제어자를 default로 변경하였음.
    접근제어자를 그동안 public으로 설정하여왔으나, 우선 private으로 만들고
    점차 범위를 넓혀가라는 팀원의 feedback을 반영하여 default로 설정하였음.

--- InputView.java
+++ InputView.java
@@ -3,12 +3,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class InputView {
-	// 사람과 사다리 개수를 입력받는 메서드
-	private int[] getInput() throws IOException {
+	int[] getInput() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("참여할 사람은 몇 명인가요? \n>");
+		System.out.print("참여할 사람은 몇 명인가요? \n> ");
 		int peopleNumber = Integer.parseInt(br.readLine());
-		System.out.print("최대 사다리 높이는 몇 개인가요? \n>");
+
+		System.out.print("최대 사다리 높이는 몇 개인가요? \n> ");
 		int ladderHeight = Integer.parseInt(br.readLine());
 
 		return new int[] {peopleNumber, ladderHeight};
