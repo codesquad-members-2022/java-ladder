823ecd6b8aba6b4f1b3e92110c874396c256f172
Seung-Wan Jeong <sjeong4776@gmail.com>
2022-02-14 22:46:03 +0900

    Lay out overall flow of the program
    * 인원 수와 사다리 높이를 입력받고, 사다리를 생성하고, 문자열 형식으로 그려진 사다리를 콘솔에 출력한다.

---
+++ src/Ladder.java
@@ -0,0 +1,13 @@
+public class Ladder {
+    private int persons;
+    private int height;
+
+    public Ladder(int persons, int height) {
+        this.persons = persons;
+        this.height = height;
+    }
+
+    public String render() {
+        return "";
+    }
+}
