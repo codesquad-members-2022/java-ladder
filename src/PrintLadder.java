d52f915c7e63ae8a8b1c86386ae4c2ab79c628bd
hsjang <hsjang0405@gmail.com>
2022-02-14 11:34:48 +0900

    ✨  사다리 출력하는 PrintLadder Class 생성

---
+++ src/PrintLadder.java
@@ -0,0 +1,16 @@
+public class PrintLadder {
+
+	private PrintLadder() {}
+
+
+
+	public static void printLadder(char[][] ladderFrame, boolean[][] ladderfoothold) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("test");
+
+
+		sb.setLength(0);
+	}
+
+}
