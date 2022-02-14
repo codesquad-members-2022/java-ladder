4a9a0f6e215c2cf40681e2627e3600d8a671cfeb
donggi-lee-bit <devdonggilee@gmail.com>
2022-02-14 20:48:09 +0900

    사다리 출력해보기
    - 플레이할 유저와 사다리 높이를 입력 받아 출력해보았다

--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -3,22 +3,28 @@ public class LadderGame {
     String[][] arr;
     int user;
     int height;
+    String ladderUnit = "|";
+    String lineUnit = "-";
 
     LadderGame() {
 
     }
 
     public void init(int user, int height) {
-        // TODO: 2022/02/14
-        //  user 3, height 5 가 들어와있다
         this.user = user;
         this.height = height;
-
+        makeLadder();
     }
 
-    void makeLadder() {
-
+    public void makeLadder() {
         arr = new String[height][user];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = ladderUnit;
+            }
+        }
+        Output.printLadderGame(arr);
     }
 
     void randomSetLine() {
--- src/main/java/Output.java
+++ src/main/java/Output.java
@@ -7,4 +7,15 @@ public class Output {
     public static void ladderCountNoti() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
     }
+
+    public static void printLadderGame(String[][] arr) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                sb.append(arr[i][j]);
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
 }
