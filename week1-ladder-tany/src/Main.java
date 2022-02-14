cc9e0b59772b4d58e860ec8b42da87a33de245ab
tany <juni8453@naver.com>
2022-02-14 21:53:01 +0900

    Main 클래스 initOption() 메서드을 수정했습니다.
    출력을 담당하는 output 객체를 생성하는 부분을 추가했습니다.

--- week1-ladder-tany/src/Main.java
+++ week1-ladder-tany/src/Main.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
     private static Ladder ladder;
     private static Person person;
+    private static Output output;
 
     public static void main(String[] args) {
-        // TODO main 메서드 실행 후 참여할 사람, 최대 사다리 높이 입력받을 수 있도록 하기.
-        // TODO static 메서드 하나 만들어서 Scanner 로 입력받도록 하자. (Ladder, Person 생성자 사용)
         Main.initOption();
-
+        char[][] resultLadder = ladder.initLadderInformation();
+        output.printLadder(resultLadder);
     }
 
     private static void initOption() {
@@ -19,7 +19,8 @@ public class Main {
         System.out.println("최대 사다리 높이는 몇 개인가요 ?");
         int maxLengthOfLadder = sc.nextInt();
 
-        ladder = new Ladder(maxLengthOfLadder);
         person = new Person(personCount);
+        ladder = new Ladder(maxLengthOfLadder, person);
+        output = new Output(ladder);
     }
 }
