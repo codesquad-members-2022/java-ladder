c63cbb823f436554c807c3876887d4d865ad76fa
WooJin Park <ak2j38@gmail.com>
2022-02-14 17:55:14 +0900

    입력받는 객체(InputView)
    사용자에게 입력받기 위한 질문 메시지 상수처리
    
    end

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,8 @@
+package view;
+
+public class InputView {
+
+    private static final String PLAY_PEOPLE_COUNT_MESSAGE = "참여할 사람은 몇 명인가요?";
+    private static final String MAX_LADDER_LENGTH_MESSAGE = "최대 사다리 높이는 몇 개 인가요?";
+
+}
