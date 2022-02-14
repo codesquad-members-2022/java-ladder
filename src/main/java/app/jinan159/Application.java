0532b8381f23c7d0aefe8ca07ded2da2780f9dcb
jinan159 <wlsdhks0423@naver.com>
2022-02-14 18:14:29 +0900

    사다리 게임 실행로직 추가
    LadderGame는 사다리 게임의 메인 로직을 담당하는 클래스 입니다.

--- src/main/java/app/jinan159/Application.java
+++ src/main/java/app/jinan159/Application.java
@@ -1,9 +1,11 @@
 package app.jinan159;
 
+import app.jinan159.ladder.LadderGame;
+
 public class Application {
-
     public static void main(String[] args) {
-
+        LadderGame game = new LadderGame();
+        game.startGame();
     }
 
 }
