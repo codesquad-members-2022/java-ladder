b28bd9d39e018fc0d8c9ef54d1fae039966e4cec
WooJin Park <ak2j38@gmail.com>
2022-02-14 18:01:16 +0900

    입력받는 객체(InputView)
    참여인원과 사다리높이를 받는 메소드 추가
    
    end

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,8 +1,20 @@
 package view;
 
+import java.util.Scanner;
+
 public class InputView {
 
-    private static final String PLAY_PEOPLE_COUNT_MESSAGE = "참여할 사람은 몇 명인가요?";
+    private static final String PLAYERS_COUNT_MESSAGE = "참여할 사람은 몇 명인가요?";
     private static final String MAX_LADDER_LENGTH_MESSAGE = "최대 사다리 높이는 몇 개 인가요?";
+    private static Scanner sc = new Scanner(System.in);
 
+    public static String getPlayersCount() {
+        System.out.println(PLAYERS_COUNT_MESSAGE);
+        return sc.nextLine();
+    }
+
+    public static String getMaxLadderLength() {
+        System.out.println(MAX_LADDER_LENGTH_MESSAGE);
+        return sc.nextLine();
+    }
 }
