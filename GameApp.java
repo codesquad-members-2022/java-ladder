ed47e836002d26be21e48df74d335714fea3c250
ikjo93 <auddlr100@naver.com>
2022-02-14 23:12:29 +0900

    GameApp, GameDisplay 클래스 수정
    사다리 데이터 생성 및 출력 기능 구현

--- GameApp.java
+++ GameApp.java
@@ -1,10 +1,23 @@
+import java.util.Random;
+
 public class GameApp {
 
     public void run() {
         UserInterface ui = new UserInterface();
         int personCount = ui.inputPersonCount(), ladderHeight = ui.inputLadderCount();
 
+        GameDisplay.showLadderInfo(getLadderInfo(personCount, ladderHeight));
     }
 
+    public String[][] getLadderInfo(int personCount, int ladderHeight) {
+        String[][] ladderInfo = new String[ladderHeight][personCount * 2 - 1];
 
+        for (int i = 0; i < ladderHeight; i++) {
+            for (int j = 0; j < personCount * 2 - 1; j++) {
+                ladderInfo[i][j] = j % 2 == 0 ? "|" : (new Random().nextInt(2) == 0 ? " " : "-");
+            }
+        }
+
+        return ladderInfo;
+    }
 }
--- GameDisplay.java
+++ GameDisplay.java
@@ -8,4 +8,14 @@ public class GameDisplay {
         System.out.println("최대 사다리 높이는 몇개 인가요?");
     }
 
+    public static void showLadderInfo(String[][] ladderInfo) {
+        System.out.println();
+        for (int i = 0; i < ladderInfo.length; i++) {
+            for (int j = 0; j < ladderInfo[0].length; j++) {
+                System.out.printf("%s", ladderInfo[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
 }
