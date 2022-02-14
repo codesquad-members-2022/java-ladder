31fa8cab5d9f14e881a0be042c372f739dfaf894
tany <juni8453@naver.com>
2022-02-14 21:57:16 +0900

    사다리 출력을 담당하는 Output 클래스를 새로 생성했습니다.
    Ladder 사다리 객체를 생성자로 주입받도록 했고, Main 에서 인자로 받는
    
    resultLadder char 2차원 배열을 받아 차례대로 출력합니다.

--- week1-ladder-tany/src/Output.java
+++ week1-ladder-tany/src/Output.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Output {
+public class Output {
+    private final Ladder ladder;
+
+    public Output(Ladder ladder) {
+        this.ladder = ladder;
+    }
+
+    public void printLadder(char[][] resultLadder) {
+        for (char[] line : resultLadder) {
+            for (char lineOfch : line) {
+                System.out.print(lineOfch);
+            }
+            System.out.println();
+        }
+    }
 }
