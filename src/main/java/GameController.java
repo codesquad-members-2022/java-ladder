3799385d8a998a1fc9ffbad6555fc8e3fbaa1ea1
“kukim” <kukim.dev@gmail.com>
2022-02-14 20:15:55 +0900

    feat: Ladder 메인 로직 클래스 구현
    - Ladder.create 정적 팩토리 메서드통해 객체 생성 (초기화 : 플레이어 수, 사다리 층, 사다리 데이터 2차원 배열(Ladder.lines))
    - 멤버변수 Ladder.lines는 셋업하여 사용해야함 (LadderdrawRandomLines 또는 Ladder.drawLine)
    - Ladder.drawRandomLines()는 Ladder.drawLine를 랜덤하게 사용하여 Ladder.lines 멤버 변수에 랜덤하게 데이터 생성 (양 옆 0이면 연결 X, 양 옆 1이면 연결 O)

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -1,17 +1,16 @@
 public class GameController {
-
-
-    public static void main(String[] args) {
-        GameController.run();
-    }
-
     private static void run() {
         int playerCount = InputView.getInputNumer("참여할 사람은 몇 명인가요?");
         int ladderMaxHeight = InputView.getInputNumer("최대 사다리 높이는 몇 개인가요?");
 
         Ladder ladder = Ladder.create(playerCount, ladderMaxHeight);
+        ladder.drawRandomLines();
 
-        OutputView.print(ladder);
+        OutputView.print(ladder.getLines());
         InputView.close();
+    }
+
+    public static void main(String[] args) {
+        GameController.run();
     }
 }
--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -1,5 +1,46 @@
+import java.util.Random;
+
 public class Ladder {
-    public static Ladder create(int playerCount, int ladderMaxHeight) {
-        return null;
+
+    private int playerCount;
+    private int ladderMaxHeight;
+    private int[][] lines;
+    private static Random rnd = new Random();
+
+    private Ladder() {
     }
+
+    public static Ladder create(int playerCount, int ladderMaxHeight) {
+        Ladder ladder = new Ladder();
+        ladder.playerCount = playerCount;
+        ladder.ladderMaxHeight = ladderMaxHeight;
+        ladder.lines = new int[ladderMaxHeight][playerCount];
+        return ladder;
+    }
+
+    public int[][] getLines() {
+        return lines;
+    }
+
+    public void drawRandomLines() {
+        int row = ladderMaxHeight;
+        int col = playerCount;
+
+        for (int row_i = 0; row_i < row; row_i++) {
+            for (int col_i = 0; col_i < col - 1; col_i++) {
+                if (isRandomDraw())
+                    drawLine(row_i, col_i);
+            }
+        }
+    }
+
+    private boolean isRandomDraw() {
+        return rnd.nextBoolean();
+    }
+
+    public void drawLine(int row, int col) {
+        lines[row][col] = 1;
+        lines[row][col + 1] = 1;
+    }
+
 }
