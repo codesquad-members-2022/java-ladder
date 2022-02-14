c26a14275abb969425872b398c864ec5942d8fc4
who-hoo <who.ho3ov@gmail.com>
2022-02-14 17:12:55 +0900

    :sparkles: 사다리게임(LadderGame) 초기화 기능 구현
    - TODO: 입력 예외 처리

--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -1,6 +1,21 @@
+import io.InputView;
+import io.OutputView;
+
 public class LadderGame {
 
+    private final InputView input = new InputView();
+    private final OutputView output = new OutputView();
+    private Ladder ladder;
+
     public void run() {
-        System.out.println("사다리 게임 시작");
+        initGame();
+    }
+
+    private void initGame() {
+        // TODO: 입력 예외 처리
+        int entry = Integer.parseInt(input.getEntry());
+        int height = Integer.parseInt(input.getHeight());
+        this.ladder = new Ladder(entry, height);
+        output.print(ladder.toString());
     }
 }
