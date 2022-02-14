0929036fbd6e2abf1667a471149160fac70d168e
nori <nohriter@gmail.com>
2022-02-14 17:19:01 +0900

    플레이어의 수와 사다리의 개수 입력받기 기능 구현

--- src/App.java
+++ src/App.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class App {
 
+    private static final Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        // n명의 사람과 m개의 사다리 개수를 입력할 수 있어야 한다.
+        System.out.println("참여할 사람은 몇 명인가요?");
+        int playerCount = sc.nextInt();
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        int ladderHeight = sc.nextInt();
+
     }
 
 }
