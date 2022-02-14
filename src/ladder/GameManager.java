4518308c3264a740532ccfc84b3a3c90f9660f75
Jaehong Choe <qnalsfj@gmail.com>
2022-02-14 18:50:51 +0900

    feat: 메인 클래스 추가
    Game클래스에서 GameManager를 호출.
    GameManager는 inputView를 통해 Ladder인스턴스 생성 함.

---
+++ src/ladder/GameManager.java
@@ -0,0 +1,18 @@
+package src.ladder;
+
+import java.io.IOException;
+
+public class GameManager {
+
+    private InputView inputView = new InputView();
+    private Ladder ladder;
+
+    public void gameStart() {
+        Ladder ladder = createLadder();
+    }
+
+    private Ladder createLadder() {
+    }
+
+
+}
