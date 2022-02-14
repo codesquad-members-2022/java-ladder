14364b052a46543e9000368237b93de59fe18139
jiwoo <zbqmgldjfh@gmail.com>
2022-02-14 18:52:48 +0900

    step1 막구현 완료

---
+++ src/main/java/step0/Main.java
@@ -0,0 +1,9 @@
+package step0;
+
+import step0.controller.LadderController;
+
+public class Main {
+    public static void main(String[] args) {
+        new LadderController().run();
+    }
+}
---
+++ src/main/java/step01/view/InputView.java
@@ -0,0 +1,17 @@
+package step01.view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static List<Integer> requestInfo() {
+        System.out.println("참여할 사람은 몇 명인가요?");
+        int personNumbers = scanner.nextInt();
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        int ladderHeight = scanner.nextInt();
+        return Arrays.asList(personNumbers, ladderHeight);
+    }
+}
