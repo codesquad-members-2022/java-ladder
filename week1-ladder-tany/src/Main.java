9018180924fbb7299c626c24c34a368d9d65d9bc
tany <juni8453@naver.com>
2022-02-14 17:02:12 +0900

    Main 클래스에서 Ladder, Person 객체를 static 하게 정의해주고
    initOption() 메서드를 조금 수정했습니다.

--- week1-ladder-tany/src/Main.java
+++ week1-ladder-tany/src/Main.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Main {
+    private static Ladder ladder;
+    private static Person person;
+
     public static void main(String[] args) {
         // TODO main 메서드 실행 후 참여할 사람, 최대 사다리 높이 입력받을 수 있도록 하기.
         // TODO static 메서드 하나 만들어서 Scanner 로 입력받도록 하자. (Ladder, Person 생성자 사용)
@@ -16,7 +19,7 @@ public class Main {
         System.out.println("최대 사다리 높이는 몇 개인가요 ?");
         int maxLengthOfLadder = sc.nextInt();
 
-        Ladder ladder = new Ladder(maxLengthOfLadder);
-        Person person = new Person(personCount);
+        ladder = new Ladder(maxLengthOfLadder);
+        person = new Person(personCount);
     }
 }
