f65b7fac557548afc405966607a05f5c743aa963
tany <juni8453@naver.com>
2022-02-14 16:44:55 +0900

    Main 클래스에 Scanner 를 사용해 Ladder, Person 의 속성을 저장할 수 있는
    메서드인 initOption() 메서드를 정의했습니다.

--- week1-ladder-tany/src/Main.java
+++ week1-ladder-tany/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        // TODO main 메서드 실행 후 참여할 사람, 최대 사다리 높이 입력받을 수 있도록 하기.
+        // TODO static 메서드 하나 만들어서 Scanner 로 입력받도록 하자. (Ladder, Person 생성자 사용)
+        Main.initOption();
+
+    }
+
+    private static void initOption() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("참여할 사람은 몇 명인가요 ?");
+        int personCount = sc.nextInt();
+
+        System.out.println("최대 사다리 높이는 몇 개인가요 ?");
+        int maxLengthOfLadder = sc.nextInt();
+
+        Ladder ladder = new Ladder(maxLengthOfLadder);
+        Person person = new Person(personCount);
     }
 }
