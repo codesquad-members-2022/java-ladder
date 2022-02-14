1b446dd5dbc794b817ee52e05bee3d3b6f584a83
donggi-lee-bit <devdonggilee@gmail.com>
2022-02-14 21:57:24 +0900

    랜덤하게 "-" " " 추가
    - Random 클래스의 nextBoolean() 메서드를 이용했습니다. 도움을 주신
      피오에게 감사 인사를 전합니다 ㅎ_ㅎ
    - 입력 받은 사람 수에 따라 공백 넣을 칸을 늘리는 구현이 잘못되어 이
      부분도 수정하였습니다.
    - 기능 단위 커밋이 습관이 안 된 것 같아 잘 안 되고 있습니다.
    - 다음 커밋부터는 더 신경 써보겠습니다.

--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class LadderGame {
 
     String[][] arr;
@@ -5,6 +7,7 @@ public class LadderGame {
     int height;
     String ladderUnit = "|";
     String lineUnit = "-";
+    String blank = " ";
 
     LadderGame() {
 
@@ -13,6 +16,7 @@ public class LadderGame {
     public void init(int user, int height) {
         this.user = user;
         this.height = height;
+        userToBlink();
         makeLadder();
     }
 
@@ -20,19 +24,31 @@ public class LadderGame {
         arr = new String[height][user];
 
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                arr[i][j] = ladderUnit;
-            }
+            addLadderUnit(i);
         }
         Output.printLadderGame(arr);
     }
 
-    void randomSetLine() {
-
+    private void addLadderUnit(int i) {
+        for (int j = 0; j < arr[i].length; j++) {
+            if (j % 2 == 1) {
+                lineOrBlank(i, j);
+            } else {
+                arr[i][j] = ladderUnit;
+            }
+        }
     }
 
-    void game() {
-        makeLadder();
-        randomSetLine();
+    private void lineOrBlank(int i, int j) {
+        Random rand = new Random();
+        if (rand.nextBoolean()) {
+            arr[i][j] = lineUnit;
+        } else {
+            arr[i][j] = blank;
+        }
+    }
+
+    void userToBlink() {
+        user = user * 2 - 1;
     }
 }
