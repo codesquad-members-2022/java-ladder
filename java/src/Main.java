1cab8d1585d9380615cd0aaea495643893a437be
hwicode <khc3796@naver.com>
2022-02-15 02:15:12 +0900

    Main 클래스 추가
    사다리 게임을 실행시키기 위한 클래스

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,49 +1,14 @@
-import java.util.Scanner;
-
 public class Main {
 
-    public static int randomNumber() {
-        return (int) (Math.random() * 2);
-    }
-
-    public static String line(int people, int number) {
-        String[] line = {"-", " "};
-        if (number < people - 1) {
-            return line[randomNumber()];
-        }
-        return " ";
-    }
-
-    public static String[] ladderOneHight(int people) {
-        String[] ladderLine = new String[people * 2];
-        String pole = "|";
-        for (int i = 0; i < people * 2; i = i + 2) {
-            ladderLine[i] = pole;
-            ladderLine[i + 1] = line(people, i / 2);
-        }
-        return ladderLine;
-    }
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("참여할 사람은 몇 명인가요?");
-        int people = scan.nextInt();
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        int ladderHight = scan.nextInt();
-        System.out.println();
+        Input input = new Input();
+        int people = input.getPeople();
+        int hight = input.getHight();
 
-        String[][] ladder = new String[people * 2][ladderHight];
-        for (int i = 0; i < ladderHight; i++) {
-            ladder[i] = ladderOneHight(people);
-        }
-
-        for (int i = 0; i < ladderHight; i++) {
-            for (int j = 0; j < people * 2; j++) {
-                System.out.print(ladder[i][j]);
-            }
-            System.out.println();
-        }
-
+        Ladder ladder = new Ladder(people, hight);
+        Output output = new Output();
+        output.printLadder(ladder.getLadder());
 
     }
 }
