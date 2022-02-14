805ef784fc188598ae14893c2590322b1ce5cc70
WooJin Park <ak2j38@gmail.com>
2022-02-14 18:04:11 +0900

    입력받는 객체(InputView)
    최대 사다리 높이 변수명 및 메소드명 변경
    
    end

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String PLAYERS_COUNT_MESSAGE = "참여할 사람은 몇 명인가요?";
-    private static final String MAX_LADDER_LENGTH_MESSAGE = "최대 사다리 높이는 몇 개 인가요?";
+    private static final String MAX_LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개 인가요?";
     private static Scanner sc = new Scanner(System.in);
 
     public static String getPlayersCount() {
@@ -13,8 +13,8 @@ public class InputView {
         return sc.nextLine();
     }
 
-    public static String getMaxLadderLength() {
-        System.out.println(MAX_LADDER_LENGTH_MESSAGE);
+    public static String getMaxLadderHeight() {
+        System.out.println(MAX_LADDER_HEIGHT_MESSAGE);
         return sc.nextLine();
     }
 }
