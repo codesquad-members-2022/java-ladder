6d6a996288093b20818701ba09f92cc128545169
leezzangmin <ckdals12345678@gmail.com>
2022-02-16 00:13:38 +0900

    Feat: 유저 이름 입력 예외처리 추가
    입력받은 이름의 길이가 5 이상이면 다시 입력받도록 작성하였음

--- src/InputView.java
+++ src/InputView.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class InputView {
     private static Scanner input = new Scanner(System.in);
-
+    private static final int ValidNameLength = 5;
     public static int getUserNumberInput(){
         OutputView.printUserNumberMessage();
         return input.nextInt();
@@ -14,7 +15,16 @@ public class InputView {
     }
 
     public static String[] getUsersNames(){
-        String userNamesInputLine = input.nextLine();
-        return userNamesInputLine.split(",");
+        OutputView.printUserNamesMessage();
+        String[] userNamesInputLine = input.nextLine().split(",");
+        while(isNamesHaveValidLength( userNamesInputLine )!=true) {
+            OutputView.printInvalidUserNamesMessage();
+            userNamesInputLine = input.nextLine().split(",");
+        }
+        return userNamesInputLine;
+    }
+
+    private static boolean isNamesHaveValidLength(String[] userNames){
+        return Arrays.stream(userNames).filter(i -> i.length()>ValidNameLength).count() > 0 ? false : true;
     }
 }
--- src/OutputView.java
+++ src/OutputView.java
@@ -1,6 +1,8 @@
 public class OutputView {
     private static final String userNumberMessage = "참여할 사람은 몇 명인가요?";
     private static final String sadariNumberMessage = "최대 사다리 높이는 몇 개인가요?";
+    private static final String userNamesMessage = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String invalidUserNamesMessage = "이름의 길이가 너무 깁니다. 다시 입력하세요";
     private static final char sero = '|';
     private static final char garo = '-';
     private static final char empty = ' ';
@@ -24,4 +26,14 @@ public class OutputView {
         }
         System.out.println(graphicSadari);
     }
+
+    public static void printUserNamesMessage(){
+        System.out.println(userNamesMessage);
+    }
+
+    public static void printInvalidUserNamesMessage(){
+        System.out.println(invalidUserNamesMessage);
+    }
+
+
 }
