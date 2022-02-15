d5d609bc8615ecd8b40335ed75f952a01dd9e3f6
nori <nohriter@gmail.com>
2022-02-15 19:01:56 +0900

    feat: Scanner close 추가

--- src/Input.java
+++ src/Input.java
@@ -4,10 +4,14 @@ public class Input {
 
     private static final Scanner sc = new Scanner(System.in);
 
-    public static int inputRules(String x) {
-        System.out.println(x);
+    public static int inputRules(String message) {
+        System.out.println(message);
 
         return sc.nextInt();
+    }
+
+    public static void close() {
+        sc.close();
     }
 
 }
