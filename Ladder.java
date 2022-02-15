88424d49e2a8a8b215b48c145617d1343a891949
PhilSoGooood <peelhw@gmail.com>
2022-02-15 18:27:27 +0900

    메서드의 기능을 더욱 작게 분리하였습니다.
    참가 인원수를 통해 사다리의 넓이를 계산하는 메서드를 별도로 생성하였고,
    이중반복문을 통해 사다리를 생성하였던 것을 각 사다리의 행을 탐색하여
    라인을 채워주도록 getLadderLine()메서드를 따로 분리하여 깊이를 줄이고자
    하였습니다. 이 과정에서 사다리의 넓이를 재사용하고자 widht를 인스턴스
    변수로 선언해주었습니다.

--- Ladder.java
+++ Ladder.java
@@ -1,15 +1,27 @@
-
 class Ladder {
-	char[][] makeLadder(int peopleNumber, int ladderHeight){
-		char[][] ladder = new char[ladderHeight][(peopleNumber * 2) - 1];
-		LadderLine ladderLine = new LadderLine();
+	private int width;
 
-		for(int row = 0; row < ladder.length; row++){
-			for(int column = 0; column < ladder[0].length; column++){
-				ladder[row][column] = ladderLine.makeLadderLine(column);
-			}
+	LadderLine ladderLine = new LadderLine();
+
+	char[][] makeLadder(int peopleNumber, int ladderHeight){
+		this.width = getWidth(peopleNumber);
+
+		char[][] ladder = new char [ladderHeight][width];
+
+		for(int row = 0; row < ladderHeight; row++){
+			getLadderLine(row, ladder);
 		}
 
 		return ladder;
+	}
+
+	private int getWidth(int peopleNumber){
+		return peopleNumber * 2 - 1;
+	}
+
+	private void getLadderLine(int row, char[][] ladder) {
+		for (int column = 0; column < width; column++){
+			ladder[row][column] = ladderLine.makeLadderLine(column);
+		}
 	}
 }
