a9b7a0d60d2254aa7fbc6c0d76cfee50315303ce
seyeong <seyoung7555@naver.com>
2022-02-15 16:17:46 +0900

    refactor: inputNumber() 메서드 구조 수정
    - 메서드 호출 한 번에 두 값을 전부 받는 것이 아니라 각각 받으면서 message를 설정할 수 있도록 하였습니다.
    - 만약 입력해야하는 값이 추가되더라도 호출을 늘려서 해결하기 쉬워진다고 판단하였습니다.

--- src/main/Controller/InputController.java
+++ src/main/Controller/InputController.java
@@ -5,29 +5,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class InputController {
-    public static int[] inputNumber() {
-        int[] result = new int[2];
+    public static int inputNumber(String msg) {
+        int result = 0;
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            input(result, br);
+            printMessage(msg);
+            result = Integer.parseInt(br.readLine());
         } catch (IOException e) {
             e.printStackTrace();
         }
         return result;
     }
 
-    private static void input(int[] result, BufferedReader br) throws IOException {
-        showUserInputMsg();
-        result[0] = Integer.parseInt(br.readLine());
-        showHeightInputMsg();
-        result[1] = Integer.parseInt(br.readLine());
+    private static void printMessage(String msg) {
+        System.out.println(msg);
     }
-
-    private static void showUserInputMsg() {
-        System.out.println("참여할 사람은 몇 명인가요?");
-    }
-
-    private static void showHeightInputMsg() {
-        System.out.println("최대 사다리 높이는 몇 층인가요?");
-    }
-
 }
--- src/main/LadderGameManager.java
+++ src/main/LadderGameManager.java
@@ -7,8 +7,9 @@ public class LadderGameManager {
     private Ladder ladder;
 
     public void init() {
-        int[] inputs = InputController.inputNumber();
-        ladder = new Ladder(inputs[0], inputs[1]);
+        int playerNum = InputController.inputNumber("참여할 사람은 몇 명인가요?");
+        int ladderHeight = InputController.inputNumber("최대 사다리 높이는 몇 개인가요?");
+        ladder = new Ladder(playerNum, ladderHeight);
     }
 
     public void showLadder() {
