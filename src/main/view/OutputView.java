da20e331074ceec4259fc963856eaf0e900932c5
bugpigg <bugpigg@gmail.com>
2022-02-15 18:42:28 +0900

    refact : 출력문구 담당하는 Enum 클래스 삭제 및 필드 값으로 대체
    출력 문구를 저장해놓은 Enum 클래스를 삭제하고 OutputView 클래스의 필드값으로 대체하였습니다.

--- src/main/view/OutputView.java
+++ src/main/view/OutputView.java
@@ -3,11 +3,14 @@ package main.view;
 import java.util.Arrays;
 
 public class OutputView {
+    public static final String NUM_OF_ATTENDANTS = "참여할 사람은 몇 명인가요?";
+    public static final String MAXIMUM_HEIGHT_OF_LADDERS = "최대 사다리 높이는 몇 개 인가요?";
+
     public static void printNumOfPeopleQueryText() {
-        System.out.println(QueryText.NUM_OF_ATTENDANTS.getText());
+        System.out.println(NUM_OF_ATTENDANTS);
     }
     public static void printMaxHeightOfLadderQueryText() {
-        System.out.println(QueryText.MAXIMUM_HEIGHT_OF_LADDERS.getText());
+        System.out.println(MAXIMUM_HEIGHT_OF_LADDERS);
     }
 
     public static void printLadderBoard(String[][] ladderBoard) {
