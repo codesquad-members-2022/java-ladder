dd560c261a2d965039eade896351d62ace808679
leejohy-0223 <leejohy@naver.com>
2022-02-15 14:00:12 +0900

    feat: PrintView 위치 변경

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,9 +3,7 @@ import view.PrintView;
 
 public class Client {
     public static void main(String[] args) {
-        PrintView.requestPeopleNumber();
         int peopleCount = InputView.inputPeopleNumber();
-        PrintView.requestLadderHeight();
         int ladderHeight = InputView.inputLadderHeight();
 
         LadderGame ladderGame = new LadderGame(peopleCount, ladderHeight);
