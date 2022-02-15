c528d58f178027e910419883e10910ad05b1d06d
“kukim” <kukim.dev@gmail.com>
2022-02-15 12:26:11 +0900

    fix: GameController.run 접근제어자 수정
    - 게임 시작하는 run() 메서드의 접근 제어자 private -> public 변경

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -1,5 +1,5 @@
 public class GameController {
-    private static void run() {
+    public static void run() {
         int playerCount = InputView.getInputNumer("참여할 사람은 몇 명인가요?");
         int ladderMaxHeight = InputView.getInputNumer("최대 사다리 높이는 몇 개인가요?");
 
