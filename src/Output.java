b518026dee066095991b7449a4be65364e79757e
nathan29849 <phs960418@gmail.com>
2022-02-15 23:15:57 +0900

    feat : Output Class 생성
    출력을 담당할 Output Class 생성 및 관련 메서드 추가

---
+++ src/Output.java
@@ -0,0 +1,14 @@
+public class Output {
+
+    public void messageAboutNumber(){
+        System.out.println("참여할 사람은 몇 명인가요?");
+    }
+
+    public void messageAboutHeight() {
+        System.out.println("최대 사다리 높이는 몇 개 인가요?");
+    }
+
+    public void messageAboutLadder() {
+
+    }
+}
