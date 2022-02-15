22cc2d1fdfdaeceb2f6ace0f504a9d5e3c2c2448
Louie <dhdustnr0134@naver.com>
2022-02-15 13:28:17 +0900

    personCount, ladderHeight 필드 제거

--- src/main/java/LadderMap.java
+++ src/main/java/LadderMap.java
@@ -2,19 +2,15 @@ import java.util.Arrays;
 
 public class LadderMap {
     private char[][] map;
-    private int personCount;
-    private int ladderHeight;
 
     public LadderMap(int personCount, int ladderHeight) {
         if (personCount < 0 || ladderHeight < 0) {
             throw new IllegalArgumentException("매개변수는 0 이상이어야 합니다.");
         }
-        this.personCount = personCount;
-        this.ladderHeight = ladderHeight;
-        initMap();
+        initMap(personCount, ladderHeight);
     }
 
-    private void initMap() {
+    private void initMap(int personCount, int ladderHeight) {
         map = new char[ladderHeight][personCount * 2 - 1];
         for (char[] line : map) {
             for (int i = 0; i < line.length; i++) {
