66c460a0d7dfac6ccc45fed8a785ef893cb3e290
Kyle Choi <tmdfl1027@gmail.com>
2022-02-15 15:44:50 +0900

    [step2] Organizer -> Controller 클래스 이름 변경

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class Organizer {
+public class Controller {
     int[] getSadariInfo() {
         int peopleCount = Input.getPeopleCount();
         int sadariHeight = Input.getSadariHeight();
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	Organizer organizer = new Organizer();
-    organizer.init();
+	Controller controller = new Controller();
+    controller.init();
     }
 }
