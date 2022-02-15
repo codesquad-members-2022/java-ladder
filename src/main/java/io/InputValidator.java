df81c8f59e7fd5d84b7dcfc864fb69b0c22ac0cb
who-hoo <who.ho3ov@gmail.com>
2022-02-15 15:45:38 +0900

    :recycle: 예외 세분화 하는 로직 메서드 분리
    NumberFormatException을 세분화하여 경우에 따라 다른 메시지를 담은 IllegalArgumentException 반환하는 로직을 메서드로 분리

--- src/main/java/io/InputValidator.java
+++ src/main/java/io/InputValidator.java
@@ -10,13 +10,7 @@ public class InputValidator {
             }
             return n;
         } catch (NumberFormatException e) {
-            if (input.isEmpty() || input.isBlank()) {
-                throw new IllegalArgumentException("값을 입력해주세요.");
-            }
-            if (isContainNotNumberCharacter(input)) {
-                throw new IllegalArgumentException("숫자(정수)를 입력해주세요.");
-            }
-            throw new IllegalArgumentException("int 범위를 초과합니다.");
+            throw subdivideNumberFormatException(input);
         }
     }
 
@@ -34,5 +28,15 @@ public class InputValidator {
             contain = !isNumberCharacter(ch);
         }
         return contain;
+    }
+
+    private IllegalArgumentException subdivideNumberFormatException(String input) {
+        if (input.isEmpty() || input.isBlank()) {
+            return new IllegalArgumentException("값을 입력해주세요.");
+        }
+        if (isContainNotNumberCharacter(input)) {
+            return new IllegalArgumentException("숫자(정수)를 입력해주세요.");
+        }
+        return new IllegalArgumentException("int 범위를 초과합니다.");
     }
 }
