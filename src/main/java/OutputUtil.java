bbd33dc220d47660a588eda72455e91a7374fb3a
SeokHo Ham <kalejuice02@gmail.com>
2022-02-15 11:15:25 +0900

    [포키] - 미션 1단계 제출 (#12)
    * 프로젝트 생성
    
    * docs : Readme에 기능/프로그래밍 요구사항 추가
    
    * feat : Ladder 클래스 및 테스트 작성
    
    - 2차원 배열 형태의 사다리를 문자열로 출력하는 toString 메서드 작성
    - 유효하지 않은 인자에 대한 테스트작성
    
    * feat : InputUtil 클래스 작성
    
    * feat : OutputUtil 클래스 작성
    
    * mission1 구현 완료

---
+++ src/main/java/OutputUtil.java
@@ -0,0 +1,13 @@
+public class OutputUtil {
+    public static void printUserCountQuestion() {
+        System.out.println("참여할 사람은 몇 명인가요?");
+    }
+
+    public static void printLineHeightQuestion() {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+    }
+
+    public static void printLadder(String ladder) {
+        System.out.println(ladder);
+    }
+}
