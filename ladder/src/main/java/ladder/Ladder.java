0961be5d2f9e04ec7d425545ea2dc3ead9e4222f
geombong <shoy1415@naver.com>
2022-02-15 18:26:42 +0900

    Ladder 클래스 변수명 변경

--- ladder/src/main/java/ladder/Ladder.java
+++ ladder/src/main/java/ladder/Ladder.java
@@ -5,31 +5,38 @@ import java.util.Random;
 public class Ladder {
 
     private final int userCount;
-    private final int ladderHigh;
-    private static final String[] ladderLineElement = {" ", "-", "-"};
+    private final int ladderHeight;
+    private static final String PILLAR = "|";
+    private static final String SPACE = " ";
+    private static final String LINE = "-";
+    private final String[] ladderUnit = {SPACE, LINE, LINE};
 
     public Ladder(int userCount, int ladderHigh) {
         this.userCount = userCount;
-        this.ladderHigh = ladderHigh;
+        this.ladderHeight = ladderHigh;
         createLadder();
     }
 
     private void createLadder() {
-        String[][] ladder = new String[ladderHigh][userCount + (userCount - 1)];
+        String[][] ladder = new String[ladderHeight][ladderWidth()];
         Random random = new Random(System.currentTimeMillis());
         for (int i = 0; i < ladder.length; i++) {
             for (int j = 0; j < ladder[i].length; j++) {
                 if (j % 2 == 0) {
-                    ladder[i][j] = "|";
+                    ladder[i][j] = PILLAR;
                     continue;
                 }
-                ladder[i][j] = ladderLineElement[random.nextInt(3)];
-                if (j != 1 && ladder[i][j - 2].equals(ladderLineElement[1])) {
-                    ladder[i][j] = ladderLineElement[0];
+                ladder[i][j] = ladderUnit[random.nextInt(ladderUnit.length)];
+                if (j != 1 && ladder[i][j - 2].equals(ladderUnit[1])) {
+                    ladder[i][j] = ladderUnit[0];
                 }
             }
         }
         printLadder(ladder);
+    }
+
+    private int ladderWidth() {
+        return userCount + (userCount - 1);
     }
 
     public void printLadder(String[][] ladder) {
