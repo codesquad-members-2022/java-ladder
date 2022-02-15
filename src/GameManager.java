dac3349d221797d9ee3310ffae0bba34097e32cb
tokaji <fpzk5656@gmail.com>
2022-02-15 12:27:23 +0900

    사다리 객체에 2차원 배열로 문자열 형태를 저장할 수 있게 하였습니다.

--- src/GameManager.java
+++ src/GameManager.java
@@ -2,11 +2,10 @@ public class GameManager {
 
     public void StartGame(){
         InputItem inputItem = new InputItem();
-        PrintItem printItem = new PrintItem();
         int userNum = inputItem.inputUserNumber();
         int ladderNum = inputItem.inputLadderNumber();
-        Ladder ladder = new Ladder(userNum, ladderNum);
 
-        printItem.printLadder(ladder.userNum, ladder.ladderNum);
+        Ladder ladder = new Ladder(userNum, ladderNum);
+        ladder.print();
     }
 }
--- src/Ladder.java
+++ src/Ladder.java
@@ -1,9 +1,28 @@
-public class Ladder {
-    int userNum = 0;
-    int ladderNum = 0;
+import java.util.Random;
 
-    public Ladder(int _userNum, int _ladderNum){
-        userNum = _userNum;
-        ladderNum = _ladderNum;
+public class Ladder {
+    String[][] bundle;
+
+    public Ladder(int userNum, int ladderNum){
+        bundle = new String[ladderNum][userNum];
+        for(int i = 0; i < ladderNum; i++){
+            for(int j = 0; j < userNum; j++){
+                bundle[i][j] = "|";
+            }
+        }
+    }
+
+    public void print(){
+        Random random = new Random();
+        int userNum = bundle.length;
+        for(int i = 0; i < userNum; i++){
+            System.out.print("|");
+            for(int j = 1; j < bundle[i].length; j++){
+                boolean isJoin = random.nextBoolean();
+                String horizon = isJoin ? "-" : " ";
+                System.out.print(horizon + bundle[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
