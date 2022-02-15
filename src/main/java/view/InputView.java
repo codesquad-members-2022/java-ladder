9373b75a8b019bbcafe9e5dd5a0a1cb272aca927
leejohy-0223 <leejohy@naver.com>
2022-02-15 13:59:56 +0900

    feat: Scanner close 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,13 +3,22 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner sc = new Scanner(System.in);
+    private static Scanner sc;
 
     public static int inputPeopleNumber() {
-        return sc.nextInt();
+        PrintView.requestPeopleNumber();
+        return inputIntValue();
     }
 
     public static int inputLadderHeight() {
-        return sc.nextInt();
+        PrintView.requestLadderHeight();
+        return inputIntValue();
+    }
+
+    private static int inputIntValue() {
+        sc = new Scanner(System.in);
+        int value = sc.nextInt();
+        sc.close();
+        return value;
     }
 }
