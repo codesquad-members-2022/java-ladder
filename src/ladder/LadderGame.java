a2d6066dc665c5ee08c0b185e5c2e9818e474799
nak253 <nwon4413@naver.com>
2022-02-15 12:03:02 +0900

    메서드의 길이를 줄이기 위해서 creatGhostLeg()메서드를 분리, createArr()메서드를 새로 작성

--- src/ladder/LadderGame.java
+++ src/ladder/LadderGame.java
@@ -13,17 +13,21 @@ public class LadderGame {
     private int[][] creatGhostLeg(int numberOfParticipants, int LegLength) {
         int row = LegLength;
         int column = numberOfParticipants + (numberOfParticipants - 1);
-        int[][] ghostLeg = new int[row][column];
+        int[][] ghostLeg = createArr(row,column);
+        return ghostLeg;
+    }
 
+    private int[][] createArr(int row, int column) {
+        int[][] arr = new int[row][column];
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j += 2) {
-                ghostLeg[i][j] = 1;
+                arr[i][j] = 1;
             }
             for (int j = 1; j < column; j += 2) {
-                ghostLeg[i][j] = (int) (Math.random() * 2);
+                arr[i][j] = (int) (Math.random() * 2);
             }
         }
-        return ghostLeg;
+        return arr;
     }
 
     public void start() {
