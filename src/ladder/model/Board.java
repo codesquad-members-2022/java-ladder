38e05d2fc7547fec837e7afbb58714b257dac9e5
jun <devjun1023@gmail.com>
2022-02-16 02:09:20 +0900

    복구
    Contents
    - 잘못된 내용 커밋
    
    Todo
    - 각 메서드 인덴트 줄이기

--- src/ladder/model/Board.java
+++ src/ladder/model/Board.java
@@ -2,9 +2,6 @@ package ladder.model;
 
 import ladder.utils.RandomGenerator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Board {
 
     private final int height;
@@ -14,76 +11,41 @@ public class Board {
     private static final String BLANK = " ";
     private static final String LADDER = "-";
 
-    private List<String> board;
+    private String[][] board;
 
     public Board(int width, int height) {
         this.width = getWidth(width);
         this.height = height;
-        this.board = createBoard();
+        this.board = createBoard(width);
     }
 
     private Width getWidth(int width) {
         return new Width(width);
     }
 
-    private List<String> createBoard() {
-        List<String> elementList = new ArrayList<>();
-        int totalCount = this.width.getValue() * this.height;
-
-        for (int row = 0; row < totalCount; row += this.width.getValue()) {
-            inputElementsTo(elementList, row);
-        }
-        return elementList;
+    public String[][] getBoard() {
+        return createBoard(this.height);
     }
 
-    private void inputElementsTo(List<String> elementList, int index) {
-        for (int col = index; col < index + this.width.getValue(); col++) {
-            getSymbol(elementList, col % (this.width.getValue()));
-        }
-    }
-
-    private void getSymbol(List<String> elementList, int value) {
-        if (isColumn(value)) {
-            elementList.add(COLUMN);
-            return;
-        }
-        if (isLadder(RandomGenerator.getValue())) {
-            elementList.add(LADDER);
-            return;
-        }
-        elementList.add(BLANK);
-    }
-
-    private boolean isColumn(int value) {
-        return plusable(value) && isEvenNumber(value);
-    }
-
-    private boolean plusable(int value) {
-        return value < this.width.getValue();
-    }
-
-    private boolean isEvenNumber(int col) {
-        return col % 2 == 0;
-    }
-
-    /**
-     * 중복 코드이지만 가독성을 위해
-     * 한 번 더 사용하는 것이 옳은지?
-     * */
-    private boolean isLadder(int value) {
-        return value % 2 == 0;
-    }
-
-    public static void main(String[] args) throws Exception {
-        Board board = new Board(5, 7);
-
-        List<String> createBoard = board.createBoard();
-
-        for (int index = 0; index < createBoard.size(); index += board.width.getValue()) {
-            System.out.println();
-            for (int col = index; col < index + board.width.getValue(); col++) {
-                System.out.print(createBoard.get(col));
+    private String[][] createBoard(int height) {
+        String[][] board = new String[height][width.getValue()];
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width.getValue(); col++) {
+                board[row][col] = getSymbol(board, row, col);
             }
         }
+        return board;
+    }
+
+    private String getSymbol(String[][] board, int row, int col) {
+        if (col % 2 == 0) {
+            return board[row][col] = COLUMN;
+        }
+        if (RandomGenerator.getValue() % 2 == 0) {
+            return board[row][col] = LADDER;
+        }
+        return board[row][col] = BLANK;
+
     }
 }
+
