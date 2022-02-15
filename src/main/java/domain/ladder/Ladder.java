415f0b6c293cb124573647f86bd38610c004a01f
ttasjwi <ttasjwi920@gmail.com>
2022-02-15 23:25:28 +0900

    Ladder 클래스 불필요한 메서드 제거, ladderElements 앞에 final 붙임
    - width, height 메서드 제거 (현 상태에서 더 이상 안 쓰인다.)
    - ladderElements :  현재로서는 final을 붙이든, 안 붙이든 변경의 가능성이 있지만 일단 안 붙이는 것보다는 낫다.

--- src/main/java/domain/ladder/Ladder.java
+++ src/main/java/domain/ladder/Ladder.java
@@ -2,18 +2,10 @@ package domain.ladder;
 
 public class Ladder {
 
-    private LadderElement[][] ladderElements;
+    private final LadderElement[][] ladderElements;
 
     public Ladder(LadderElement[][] ladderElements) {
         this.ladderElements = ladderElements;
-    }
-
-    public int width() {
-        return ladderElements[0].length;
-    }
-
-    public int height() {
-        return ladderElements.length;
     }
 
     public LadderElement[][] getLadderElements() {
