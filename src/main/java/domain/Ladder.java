44724d0a5f6c394ee911c050dc8a34456c44e40c
WooJin Park <ak2j38@gmail.com>
2022-02-15 19:21:31 +0900

    feature : Ladder 수정
    1. Ladder 싱글톤으로 관리할 수 있도록 수정
    2. 클린코드를 위해 depth 최대 1로 수정
    3. Random객체를 계속 새로 생성하지 않고 생성자에서 최초 1회 생성하도록 변경
    
    end

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -4,55 +4,83 @@ import java.util.Random;
 
 public class Ladder {
 
-    private int playersCount;
-    private int maxLadderHeight;
-    private char[][] ladder;
+    private static final char LADDER_VERTICAL_LINE = '|';
+    private static final char LADDER_HORIZONTAL_LINE = '-';
+    private static final char LADDER_BLANK = ' ';
 
-    public Ladder(int playersCount, int maxLadderHeight) {
+    private static Ladder ladderInstance;
+    private static int playersCount;
+    private static int maxLadderHeight;
+    private static char[][] ladder;
+    private static Random random;
+
+    private Ladder(int playersCount, int maxLadderHeight) {
         this.playersCount = playersCount;
         this.maxLadderHeight = maxLadderHeight;
-        this.ladder = new char[this.maxLadderHeight][(2 * this.playersCount) - 1];
+        this.ladder = new char[this.maxLadderHeight][getWidth()];
+        this.random = new Random();
+    }
+
+    public static Ladder getLadder(int playersCount, int maxLadderHeight) {
+        if (ladderInstance == null) {
+            ladderInstance = new Ladder(playersCount, maxLadderHeight);
+            generateLadder();
+        }
+        return ladderInstance;
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < ladder.length; i++) {
-            for (int j = 0; j < (2 * this.playersCount) - 1; j++) {
-                sb.append(ladder[i][j]);
-            }
-            sb.append("\n");
+        for (int i = 0; i < ladder.length * getWidth(); i++) {
+            int row = i / ladder[0].length;
+            int col = i % ladder[0].length;
+            sb.append(ladder[row][col]);
+            sb.append(newLine(ladder[0].length, col));
         }
 
         return sb.toString();
     }
 
-    public void generateLadder() {
-        initEvenColumn();
-        initOddColumn();
+    private static String newLine(int height, int col) {
+        if (col == height - 1) {
+            return "\n";
+        }
+        return "";
     }
 
-    private void initEvenColumn() {
-        for (int i = 0; i < this.maxLadderHeight; i++) {
-            for (int j = 0; j < (2 * this.playersCount) - 1; j = j + 2) {
-                ladder[i][j] = '|';
-            }
+    private static int getWidth() {
+        return (2 * playersCount) - 1;
+    }
+
+    public static void generateLadder() {
+        initLadder();
+    }
+
+    private static void initLadder() {
+        for (int i = 0; i < maxLadderHeight; i++) {
+            initLadderColumn(i);
         }
     }
 
-    private void initOddColumn() {
-        for (int i = 0; i < this.maxLadderHeight; i++) {
-            for (int j = 1; j < (2 * this.playersCount) - 1; j = j + 2) {
-                ladder[i][j] = ' ';
-                if (getRandomBoolean()) {
-                    ladder[i][j] = '-';
-                }
-            }
+    private static void initLadderColumn(int i) {
+        for (int j = 0; j < getWidth(); j++) {
+            ladder[i][j] = setElement(j);
         }
     }
 
-    private boolean getRandomBoolean() {
-        return new Random().nextBoolean();
+    private static char setElement(int j) {
+        if (j % 2 == 0) {
+            return LADDER_VERTICAL_LINE;
+        }
+        if (getRandomBoolean()) {
+            return LADDER_HORIZONTAL_LINE;
+        }
+        return LADDER_BLANK;
+    }
+
+    private static boolean getRandomBoolean() {
+        return random.nextBoolean();
     }
 }
