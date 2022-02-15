70066472ab57ca849961fb6f632f1531f86803bc
donggi-lee-bit <devdonggilee@gmail.com>
2022-02-15 14:28:24 +0900

    의미 없는 생성자 삭제
    - 말씀하신대로 Client 클래스의 input() 메서드를 사용할 목적으로 Client
      객체를 호출하는 건 제가 의도한 것과는 다르고 의미가 없는 게 맞는 것
      같습니다.
    - 제가 의도한 것은 Client 클래스의 input() 메서드를 이용해 사용자 입력을
      받는 것이었기 때문에, Client 객체를 만들고 input()를 호출하는 방식으로
      수정하였습니다.
    - 아무 생각 없이 사용하고 있었는데 앞으로 사용하지 않는 객체를 만드는
      거에 주의하도록 하겠습니다.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        run();
-    }
+        Client client = new Client();
 
-    private static void run() {
-        new Client();
+        client.input();
+
         LadderGame ladderGame = new LadderGame();
+
         ladderGame.build();
     }
 }
