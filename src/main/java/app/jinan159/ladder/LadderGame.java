d0c63b1019c15864aab378051b00e72ae7208fec
jinan159 <wlsdhks0423@naver.com>
2022-02-15 10:58:27 +0900

    LadderGame 1 indent 리펙토링
    2 indent 이상인 메소드들의 indent가 1 이 되도록 리펙토링하였습니다.
    - readParticipantCount()
    - readHeight()
    - prepareGameMap()
    - startGame()

--- src/main/java/app/jinan159/ladder/LadderGame.java
+++ src/main/java/app/jinan159/ladder/LadderGame.java
@@ -14,52 +14,51 @@ public class LadderGame {
         int height = readHeight(sc);
         this.gameMap = setGameMapSize(participantCount * 2 - 1, height);
 
-        prepareGameMap();
+        prepareGameMap(this.gameMap);
     }
 
     private int readParticipantCount(Scanner sc) {
-        int count;
-
         try {
             System.out.println("참여할 사람은 몇 명 인가요?");
-            count = sc.nextInt();
+            return sc.nextInt();
         } catch (Exception e) {
             return readParticipantCount(sc);
         }
-
-        return count;
     }
 
     private int readHeight(Scanner sc) {
-        int height;
-
         try {
             System.out.println("최대 사다리 높이는 몇 개인가요?");
-            height = sc.nextInt();
+            return sc.nextInt();
         } catch (Exception e) {
             return readHeight(sc);
         }
-
-        return height;
     }
 
     private Elements[][] setGameMapSize(int width, int height) {
         return new Elements[height][width];
     }
 
-    private void prepareGameMap() {
+    private void prepareGameMap(Elements[][] gameMap) {
         for (int h = 0; h < gameMap.length; h++) {
-            for (int w = 0; w < gameMap[0].length; w++) {
-                if (w % 2 == 0) setValueOnGameMap(w, h, Elements.V_LINE);
-                if (w % 2 == 1) setHorizontalValueOnGameMap(w, h);
-            }
+            prepareRow(gameMap, h);
         }
+    }
+
+    private void prepareRow(Elements[][] gameMap, int h) {
+        for (int w = 0; w < gameMap[0].length; w++) {
+            prepareColumn(w, h);
+        }
+    }
+
+    private void prepareColumn(int w, int h) {
+        if (w % 2 == 0) setValueOnGameMap(w, h, Elements.V_LINE);
+        if (w % 2 == 1) setHorizontalValueOnGameMap(w, h);
     }
 
     private void setValueOnGameMap(int width, int height, Elements elements) {
         gameMap[height][width] = elements;
     }
-
     private void setHorizontalValueOnGameMap(int width, int height) {
         if (isEmptyPosition(width, height)) {
             setValueOnGameMap(width, height, Elements.EMPTY);
@@ -69,6 +68,7 @@ public class LadderGame {
         setValueOnGameMap(width, height, Elements.H_LINE);
     }
 
+    // 빈 공간이 들어올 자리인지, 아닌지를 반환하는 가상의 알고리즘
     private boolean isEmptyPosition(int width, int height) {
         return ((width * height) + (width + height)) % 3 == 0;
     }
@@ -76,11 +76,14 @@ public class LadderGame {
     // public method
     public void startGame() {
         for(Elements[] row : gameMap) {
-            for(Elements col : row) {
-                System.out.print(col.getMark());
-            }
+            printRow(row);
             System.out.println();
         }
     }
 
+    private void printRow(Elements[] row) {
+        for(Elements col : row) {
+            System.out.print(col.getMark());
+        }
+    }
 }
