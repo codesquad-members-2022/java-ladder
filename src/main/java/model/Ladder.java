9e90a8778f00d7e59d794ead8122f63b43177178
naneun <yhsep7@gmail.com>
2022-02-15 21:23:37 +0900

    Refactor: [Ladder] isLine 메서드 -> allocElement 으로 이름 변경 및 반환값 수정
    - [Ladder] allocElement: 기존 isLine 메서드는 단순히 commonFactorExist 메서드 의 반환 값을 전달해주는 역할만 수행했기에 존재의 의미가 불명확했습니다. 따라서, commonFactorExist 의 반환 값을 사용하여 특정 요소를 반환해주는 팩토리 메서드 역 할을 수행하도록 수정했습니다.

--- src/main/java/model/Ladder.java
+++ src/main/java/model/Ladder.java
@@ -31,13 +31,13 @@ public class Ladder {
     }
     private void assignLine(int h) {
         for (int w = 0; w < width; ++w) {
-            shape[h][w] = isLine(h, w) ? Mark.LINE.ch : Mark.BLANK.ch;
+            shape[h][w] = allocElement(h, w);
         }
     }
-    private boolean isLine(int h, int w) {
+    private char allocElement(int h, int w) {
         int indexValue = (h + 1) * (w + 1) + w + 1;
         int randomValue = (int) (Math.random() * height * width + 1);
-        return commonFactorExist(indexValue, randomValue);
+        return commonFactorExist(indexValue, randomValue) ? Mark.LINE.ch : Mark.BLANK.ch;
     }
     private boolean commonFactorExist(int indexValue, int randomValue) {
         return IntStream.rangeClosed(2, (int) Math.sqrt(Math.max(indexValue, randomValue)))
