34d05c2fbf769ae7f5712be6855eedd8a6922763
PhilSoGooood <peelhw@gmail.com>
2022-02-15 14:51:58 +0900

    InputView 클래스의 getInput()메서드 기능을 작게 분리하였습니다.
    메서드는 한가지 기능만 하도록 노력하기 및 호눅스의 코드리뷰에 따라
    기존 getInput()메서드를 통해 정수형 배열 {참여인원, 사다리의 높이}로
    두 값을 한번에 반환하던 것에서 getPeopelNumber()메서드를 통해
    참여인원을 반환하고 getLadderHeight()메서드를 통해서는 사다리의 높이를
    각각 반환하도록 분리하였습니다.
    
    또한 각 값을 입력하도록 요청하던 문장을 바로 출력하는 방식이 아니라
    따로 final String 변수로 저장하여 출력하도록 수정하였습니다.

--- InputView.java
+++ InputView.java
@@ -3,14 +3,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class InputView {
-	int[] getInput() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("참여할 사람은 몇 명인가요? \n> ");
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	final String REQUEST_PEOPLE_NUMBER = "참여할 사람은 몇 명인가요? \n> ";
+	final String REQUEST_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요? \n> ";
+
+	int getPeopleNumber() throws IOException {
+		System.out.print(REQUEST_PEOPLE_NUMBER);
 		int peopleNumber = Integer.parseInt(br.readLine());
 
-		System.out.print("최대 사다리 높이는 몇 개인가요? \n> ");
+		return peopleNumber;
+	}
+
+	int getLadderHeight() throws IOException {
+		System.out.print(REQUEST_LADDER_HEIGHT);
 		int ladderHeight = Integer.parseInt(br.readLine());
 
-		return new int[] {peopleNumber, ladderHeight};
+		return ladderHeight;
 	}
 }
