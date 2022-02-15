98bb057ee633c229dd81e6e64fede4cb21466ac7
ttasjwi <ttasjwi920@gmail.com>
2022-02-15 22:19:28 +0900

    ValidatorImpl - 조건부를 메서드로 분리
    - ValidatorImpl의 유효성 검사 분기문에서, 조건문이 긴 부분을 따로 메서드를 분리했다.
    - isValidEntry : 유효한 인원수인지 판단하는 메서드
    - isValidHeight : 유효한 높이인지 판단하는 메서드

--- src/main/java/view/input/ValidatorImpl.java
+++ src/main/java/view/input/ValidatorImpl.java
@@ -18,21 +18,31 @@ public class ValidatorImpl implements Validator {
 
     @Override
     public void entryValidate(int entry) {
-        if (!(VALID_ENTRY_MIN <= entry && entry < VALID_ENTRY_MAX)) {
-            throw new IllegalArgumentException(
-                    String.format("참가자 수의 유효범위는 %d 이상 %d 이하입니다.",
-                            VALID_ENTRY_MIN,
-                            VALID_ENTRY_MAX));
+        if (isValidEntry(entry)) {
+            return;
         }
+        throw new IllegalArgumentException(
+                String.format("참가자 수의 유효범위는 %d 이상 %d 이하입니다.",
+                        VALID_ENTRY_MIN,
+                        VALID_ENTRY_MAX));
     }
 
     @Override
     public void heightValidate(int height) {
-        if (!(VALID_HEIGHT_MIN <= height && height < VALID_HEIGHT_MAX)) {
-            throw new IllegalArgumentException(
-                    String.format("높이의 유효범위는 %d 이상 %d 이하입니다.",
-                            VALID_HEIGHT_MIN,
-                            VALID_HEIGHT_MAX));
+        if (isValidHeight(height)) {
+            return;
         }
+        throw new IllegalArgumentException(
+                String.format("높이의 유효범위는 %d 이상 %d 이하입니다.",
+                        VALID_HEIGHT_MIN,
+                        VALID_HEIGHT_MAX));
+    }
+
+    private boolean isValidEntry(int entry) {
+        return VALID_ENTRY_MIN <= entry && entry < VALID_ENTRY_MAX;
+    }
+
+    private boolean isValidHeight(int height) {
+        return VALID_HEIGHT_MIN <= height && height < VALID_HEIGHT_MAX;
     }
 }
