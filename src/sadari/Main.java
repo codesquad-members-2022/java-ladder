d809da892528f2e6e494c7da518b595a6fc8249f
leekm0310 <leekm310@gmail.com>
2022-02-15 21:19:02 +0900

    step2 - Main 클래스 수정
    메인안에 있던 여러가지들을 Run클래스에 옮겼기 때문에
    이에 맞게 메인클래스도 수정함.

--- src/sadari/Main.java
+++ src/sadari/Main.java
@@ -1,15 +1,9 @@
 package sadari;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        InputView i = new InputView();
-        int people = i.inputPeople();
-        int height = i.inputHeight();
-        Ladder l = new Ladder();
-        l.makeLadder(people,height);
-        i.close();
+        Run r = new Run();
+        r.run();
     }
 }
 
