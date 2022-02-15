c2039b2478188add8609611c1af16a41efa583cf
tany <juni8453@naver.com>
2022-02-16 02:07:53 +0900

    사다리, 유저 객체의 데이터를 스캐너로 담을 수 있게 해주는 객체를 새로
    2개 만들었습니다. 원래 Main 클래스에서 스캐너를 통해 받을 수 있었는데,
    
    역할을 분리해주기 위해 새로 생성했습니다.

--- week1-ladder-tany/src/LadderData.java
+++ week1-ladder-tany/src/LadderData.java
@@ -1,21 +1,14 @@
 import java.util.Scanner;
 
-public class Data {
+public class LadderData {
 
-    private int userCount;
     private int ladderLength;
 
-    public void inputData() {
+    public void inputLadderData() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("참여할 사람은 몇 명인가요 ?");
-        userCount = sc.nextInt();
 
         System.out.println("최대 사다리 높이는 몇 개인가요 ?");
         ladderLength = sc.nextInt();
-    }
-
-    public int getUserCount() {
-        return userCount;
     }
 
     public int getLadderLength() {
