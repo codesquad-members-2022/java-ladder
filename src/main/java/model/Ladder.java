eeb56decbb7c0efda27b06334859f35768af59d3
naneun <yhsep7@gmail.com>
2022-02-15 12:38:17 +0900

    Refactor: assignLine 메서드 분리
    - [Ladder] assignLine(): shape 의 행 순회
    - [Ladder] assignLine(int h): shape 의 특정 행에 속한 열 순회

--- src/main/java/model/Ladder.java
+++ src/main/java/model/Ladder.java
@@ -23,11 +23,12 @@ public class Ladder {
     }
     private void assignLine() {
         for (int h = 0; h < height; ++h) {
-            for (int w = 0; w < width; ++w) {
-                if (isLine(h, w)) {
-                    shape[h][w] = '-';
-                }
-            }
+            assignLine(h);
+        }
+    }
+    private void assignLine(int h) {
+        for (int w = 0; w < width; ++w) {
+            shape[h][w] = isLine(h, w) ? '-' : ' ';
         }
     }
     private boolean isLine(int h, int w) {
