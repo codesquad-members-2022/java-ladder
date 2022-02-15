bf6f0538533becc1ff75434c77e9f245f7836560
seyeong <seyoung7555@naver.com>
2022-02-15 11:47:21 +0900

    refactor: Ladder 클래스 내부 메서드 리팩토링
    - 사다리의 가로장을 무작위로 생성하는 로직을 메소드로 분리하였습니다.
    - 모습을 표현하기 위한 문자열 상수들을 클래스 필드로 작성하였습니다.

--- src/main/Ladder.java
+++ src/main/Ladder.java
@@ -3,10 +3,14 @@ package main;
 import java.util.stream.IntStream;
 
 public class Ladder {
+    private static final String BLANK = " ";
+    private static final String RUNG = "-";
+    private static final String RAIL = "|";
     private final int userNum;
     private final int height;
     private final int space;
     private final String[][] ladderArr;
+
     public Ladder(int userNum, int height) {
         this.userNum = userNum;
         this.height = height;
@@ -16,22 +20,21 @@ public class Ladder {
 
     private String[][] createLadder() {
         String[][] result = new String[height][userNum + space];
-        IntStream.range(0, height).forEach(i -> IntStream.range(0, userNum + space).forEach(j -> {
-            generate(result, i, j);
-        }));
+        IntStream.range(0, height).forEach(i -> IntStream.range(0, userNum + space)
+                .forEach(j -> generate(result, i, j)));
         return result;
     }
 
     private void generate(String[][] result, int i, int j) {
         if (j % 2 == 0) {
-            result[i][j] = "|";
+            result[i][j] = RAIL;
             return;
         }
-        if (Math.random() > 0.5) {
-            result[i][j] = "-";
-            return;
-        }
-        result[i][j] = " ";
+        result[i][j] = generateRandomRung();
+    }
+
+    private String generateRandomRung() {
+        return (Math.random() > 0.5) ? BLANK : RUNG;
     }
 
     public String[][] getLadderArr() {
