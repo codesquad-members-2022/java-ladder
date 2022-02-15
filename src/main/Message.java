068c89118ba99c4d2a2c44d8c4fb75e25592b132
rxdcxdrnine <rkckr1@naver.com>
2022-02-15 11:28:01 +0900

    refactor: printGame 메서드 분리 및 Message 열거형 내 요소 추가
    - printGame 메서드 내에서 printLine 메서드 호출
    - Message 열거형 내 OUTPUT_HORIZONTAL_LINE 추가

--- src/main/Message.java
+++ src/main/Message.java
@@ -3,7 +3,8 @@ package main;
 public enum Message {
 
     INPUT_NUM_PERSON("참여할 사람은 몇 명인가요?"),
-    INPUT_NUM_LADDER("최대 사다리 높이는 몇 개인가요?");
+    INPUT_NUM_LADDER("최대 사다리 높이는 몇 개인가요?"),
+    OUTPUT_HORIZONTAL_LINE("\n----------\n");
 
     private final String message;
 
