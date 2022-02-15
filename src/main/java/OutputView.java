7e783709bcce29db24ec88d5251277ea9f1cb23b
“kukim” <kukim.dev@gmail.com>
2022-02-15 10:53:18 +0900

    refactor: OutputView 클래스
    - OutputView.print(int[][] lines) 메서드를 작은 메서드로 나누었습니다.
    - OutputView는 정적 메서드로 사용하기 때문에 private 생성자를 추가했습니다.

--- src/main/java/OutputView.java
+++ src/main/java/OutputView.java
@@ -1,22 +1,32 @@
 public class OutputView {
+    private OutputView() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static void printMessage(String str) {
         System.out.println(str);
     }
 
-    public static void print(int[][] rows) {
+    public static void print(int[][] lines) {
         StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < rows.length; i++) {
-            for (int j = 0; j < rows[0].length - 1; j++) {
-                if (rows[i][j] == 1 && rows[i][j + 1] == 1) {
-                    sb.append("|-");
-                    continue;
-                }
-                sb.append("| ");
-            }
-            sb.append("|\n");
+        for (int row_i = 0; row_i < lines.length; row_i++) {
+            appendLine(sb, lines, row_i);
         }
+        System.out.println(sb);
+    }
 
-        System.out.println(sb.toString());
+    private static void appendLine(StringBuilder sb, int[][] lines, int row_i) {
+        for (int col_i = 0; col_i < lines[0].length - 1; col_i++) {
+            appendRadder(sb, lines, row_i, col_i);
+        }
+        sb.append("|\n");
+    }
+
+    private static void appendRadder(StringBuilder sb, int[][] lines, int row_i, int col_i) {
+        if (lines[row_i][col_i] == 1 && lines[row_i][col_i + 1] == 1) {
+            sb.append("|-");
+            return;
+        }
+        sb.append("| ");
     }
 }
