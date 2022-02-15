c44b3487ce66d76bf66d4b64683e354255f3651f
vans <92678171+ffinn92@users.noreply.github.com>
2022-02-15 14:19:21 +0900

    [반스] 사다리 게임 1단계 - 기본 기능 구현 (#35)
    * ✏️미션1의 요구사항 README 작성
    
    * 1단계 구현에 필요한 클래스 구현
    
    * Launcehr 클래스에서 프로그램 실행 역할을 할 run() 메소드 구현
    
    * 입력 안내 프롬프트를 보여줄 input 메소드 구현
    
    * 2차원 배열로 사다리를 만드는 makeLadder() 메소드 구현
    
    * 버팀대를 이어줄 디딤대를 만드는 makeLegBridge() 메소드 구현
    
    * 배열의 홀수, 짝수에 따라 버팀대, 디딤대로 변환하는 insertLegAndBridge() 구현
    
    * 랜덤하게 디딤대를 넣어주기 위한 randomMakeBridge() 구현
    
    * 게임 실행 결과를 출력하기 위한 result() 구현

---
+++ Level01/Launcher.java
@@ -0,0 +1,56 @@
+import java.util.Random;
+
+public class Launcher {
+    private int player;
+    private int height;
+    private String[][] result;
+
+    public void run() {
+        player = Print.inputPlayer();
+        height = Print.inputHeight();
+        result = makeLadder(player,height);
+
+        Print.result(result);
+    }
+
+    private String[][] makeLadder(int player, int height) {
+        int row = height;
+        int column = 2 * player - 1;
+
+        String[][] ladder = new String[row][column];
+        ladder = makeLegBridge(ladder);
+
+        return ladder;
+    }
+
+    private String[][] makeLegBridge(String[][] ladder) {
+        Launcher launcher = new Launcher();
+
+        for (int i = 0; i < ladder.length; i++) {
+            for (int j = 0; j < ladder[i].length; j++) {
+                insertLegAndBridge(ladder, launcher, i, j);
+            }
+        }
+
+        return ladder;
+    }
+
+    private void insertLegAndBridge(String[][] ladder, Launcher launcher, int i, int j) {
+        if (j % 2 == 0) {
+            ladder[i][j] = "|";
+        } else {
+            launcher.randomMakeBridge();
+            ladder[i][j] = randomMakeBridge();
+        }
+    }
+
+    private String randomMakeBridge() {
+        Random random = new Random();
+        int a = random.nextInt(2);
+
+        if(a == 0){
+            return "-";
+        }
+        return " ";
+    }
+}
---
+++ Level01/Print.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Print {
+    public static int inputPlayer() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("참여할 사람은 몇 명인가요?");
+
+        return sc.nextInt();
+    }
+
+    public static int inputHeight() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+
+        return sc.nextInt();
+    }
+
+    public static void result(String[][] makeLadder) {
+        System.out.println();
+        for(String[] i : makeLadder){
+            for(String j : i){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
