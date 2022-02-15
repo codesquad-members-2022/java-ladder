3dcec29b521cbf17249cd0347874825b10e91e5c
seyeong <seyoung7555@naver.com>
2022-02-15 14:06:28 +0900

    refactor: InputController 내부의 강한 결합 개선
    - InputController 내부에서 OutputController를 통해 안내 메시지를 출력하고 있었습니다.
    - OutputController에 강하게 결합되어있어 내부에서 처리하는 것으로 수정하였습니다.

--- src/main/Controller/InputController.java
+++ src/main/Controller/InputController.java
@@ -8,13 +8,21 @@ public class InputController {
     public static int[] inputNumber() {
         int[] result = new int[2];
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            OutputController.showUserInputMsg();
+            showUserInputMsg();
             result[0] = Integer.parseInt(br.readLine());
-            OutputController.showHeightInputMsg();
+            showHeightInputMsg();
             result[1] = Integer.parseInt(br.readLine());
         } catch (IOException e) {
             e.printStackTrace();
         }
         return result;
     }
+    private static void showUserInputMsg() {
+        System.out.println("참여할 사람은 몇 명인가요?");
+    }
+
+    private static void showHeightInputMsg() {
+        System.out.println("최대 사다리 높이는 몇 층인가요?");
+    }
+
 }
--- src/main/Controller/OutputController.java
+++ src/main/Controller/OutputController.java
@@ -8,14 +8,6 @@ public class OutputController {
         Arrays.stream(a).forEach(System.out::println);
         System.out.println("");
     };
-    public static void showUserInputMsg() {
-        System.out.println("참여할 사람은 몇 명인가요?");
-    }
-
-    public static void showHeightInputMsg() {
-        System.out.println("최대 사다리 높이는 몇 층인가요?");
-    }
-
     public static void showLadder(String[][] ladderArr) {
         Arrays.stream(ladderArr).forEach(printLadder);
     }
