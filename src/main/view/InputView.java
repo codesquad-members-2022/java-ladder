6b3cc4ac71df04da63b26585bfd72183e7ec09be
bugpigg <bugpigg@gmail.com>
2022-02-15 19:12:28 +0900

    feat : 사용자 입력 예외처리 메소드 추가
    - 사용자의 입력이 문자인 경우 예외처리를 수행하는 메소드를 추가하였습니다.
    - 사용자의 입력이 음의 정수인 경우 예외처리를 수행하는 메소드를 추가하였습니다.

--- src/main/view/InputView.java
+++ src/main/view/InputView.java
@@ -1,15 +1,32 @@
 package main.view;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
     public static final Scanner scanner = new Scanner(System.in);
 
     public static int giveInt() {
-        return scanner.nextInt();
+        int num = getInt();
+        isPositiveInt(num);
+        return num;
     }
 
     public static void closeScanner() {
         scanner.close();
+    }
+
+    public static int getInt() throws InputMismatchException {
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            throw new InputMismatchException("Incorrect input: an integer is required");
+        }
+    }
+
+    public static void isPositiveInt(int num) {
+        if (num <= 0){
+            throw new IllegalArgumentException("Incorrect input: positive integer is required");
+        }
     }
 }
