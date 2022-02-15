6dbdb9772e3839ff81938d0273d587f3fead1676
rkolx <olx.rko.o@gmail.com>
2022-02-16 01:41:54 +0900

    Ladder 클래스 내 입력 메소드분리하여 InputLadder 클래스 생성 후 1차 구현

---
+++ src/InputLadder.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class InputLadder {
+
+    Scanner sc = new Scanner(System.in);
+
+    public int ladderInfo(){
+        return sc.nextInt();
+    }
+
+    public void inputClose(){
+        sc.close();
+    }
+}
