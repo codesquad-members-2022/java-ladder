d3a4597c0e2a7022fa8bb46861482b03bda97dc7
ttasjwi <ttasjwi920@gmail.com>
2022-02-15 23:11:00 +0900

    LadderGameServiceImpl - 구현순서 변경
    - LadderGame은 혹여나 게임을  다시 재설정 하는 상황이 발생시 변경 가능성이 있다.
    - 한번 의존성이 주입되어, 변경될 수 없는 LadderFactory를 좀 더 위에 오게 했다.
    - 같은 수준에 있더라도 자주 변경되지 않는, 혹은 불변에 가까운 상태는 좀 더 위에 보내고 싶었다.

--- src/main/java/domain/game/LadderGameServiceImpl.java
+++ src/main/java/domain/game/LadderGameServiceImpl.java
@@ -7,8 +7,8 @@ public class LadderGameServiceImpl implements LadderGameService {
 
     private static LadderGameServiceImpl instance;
 
-    private LadderGame ladderGame;
     private final LadderFactory ladderFactory;
+    private LadderGame ladderGame;
 
     private LadderGameServiceImpl(LadderFactory ladderFactory) {
         this.ladderFactory = ladderFactory;
