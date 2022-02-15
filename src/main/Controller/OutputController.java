c69177a48dbbb67ba84e66fae9de45861aa89047
seyeong <seyoung7555@naver.com>
2022-02-15 12:00:09 +0900

    refactor: OutputController 내부 함수 리팩토링
    - stream 내부에 또 stream을 가지는 로직을 분리하여 Consumer를 따로 정의하였습니다.

--- src/main/Controller/OutputController.java
+++ src/main/Controller/OutputController.java
@@ -1,8 +1,13 @@
 package main.Controller;
 
 import java.util.Arrays;
+import java.util.function.Consumer;
 
 public class OutputController {
+    private static final Consumer<String[]> printLadder = a -> {
+        Arrays.stream(a).forEach(System.out::println);
+        System.out.println("");
+    };
     public static void showUserInputMsg() {
         System.out.println("참여할 사람은 몇 명인가요?");
     }
@@ -12,9 +17,6 @@ public class OutputController {
     }
 
     public static void showLadder(String[][] ladderArr) {
-        Arrays.stream(ladderArr).forEach(a -> {
-            Arrays.stream(a).forEach(System.out::print);
-            System.out.println("");
-        });
+        Arrays.stream(ladderArr).forEach(printLadder);
     }
 }
