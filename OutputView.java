f426b7f25d6c616db716805f727a6e1d51117db9
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-02-15 12:30:45 +0900

    사다리 출력을 위한 OutputView Class 구현

---
+++ OutputView.java
@@ -0,0 +1,18 @@
+public class OutputView {
+    private static OutputView ov;
+
+    private OutputView(){}
+
+    public static OutputView getInstance(){
+        if(ov == null){
+            ov = new OutputView();
+        }
+        return ov;
+    }
+
+    public void printLadder(Ladder ladder){
+        System.out.println(ladder);
+    }
+
+
+}
