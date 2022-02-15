8cec8eada8a18e133660aeea3652f79a4faee4f8
nathan29849 <phs960418@gmail.com>
2022-02-15 23:32:38 +0900

    feat : Main Class 생성
    전반적인 흐름을표현할 Main Class 생성

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        int n; // number of people
+        int m; // height of ladder
+        Output.messageAboutNumber();
+        n = Input.getNumber();
 
+        Output.messageAboutHeight();
+        m = Input.getNumber();
+
+        Input.scannerClose();
     }
 }
