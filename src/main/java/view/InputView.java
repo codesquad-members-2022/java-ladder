3e86bd9d38cc9858341695e2a858e042800c0b3a
WooJin Park <ak2j38@gmail.com>
2022-02-15 19:18:41 +0900

    feature : InputView 수정
    1. Scanner 생성 메소드, 종료 메소드 추가
    
    end

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,7 +6,7 @@ public class InputView {
 
     private static final String PLAYERS_COUNT_MESSAGE = "참여할 사람은 몇 명인가요?";
     private static final String MAX_LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개 인가요?";
-    private static Scanner sc = new Scanner(System.in);
+    private static Scanner sc;
 
     public static int getPlayersCount() {
         System.out.println(PLAYERS_COUNT_MESSAGE);
@@ -16,5 +16,17 @@ public class InputView {
     public static int getMaxLadderHeight() {
         System.out.println(MAX_LADDER_HEIGHT_MESSAGE);
         return sc.nextInt();
+    }
+
+    public static void createScanner() {
+        if (sc == null) {
+            sc = new Scanner(System.in);
+        }
+    }
+
+    public static void closeScanner() {
+        if (sc != null) {
+            sc.close();
+        }
     }
 }
