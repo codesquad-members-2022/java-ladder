b07d76b842425cb8002c86defd6ae12f9b9e79a4
sally-ksh <sally.devz@gmail.com>
2022-02-17 02:33:16 +0900

    refactor: 메서드 라인 10줄 이내로 줄이기
    - Server 객체를 통해 설정 값들과 게임 진행시 이용될 인스턴스들을 생성하고 의존성 주입시켜줘서 진행되게 했다

--- src/com/sh/Main.java
+++ src/com/sh/Main.java
@@ -1,38 +1,12 @@
 package com.sh;
 
 import static com.sh.utils.Input.*;
-import static com.sh.utils.Output.*;
 
-import java.util.List;
-
-import com.sh.domains.Ladder;
-import com.sh.domains.LadderService;
-import com.sh.domains.Players;
 
 public class Main {
-    private static Configuration configuration = Configuration.getInstance();
-
     public static void main(String[] args) {
-        run();
-    }
-
-    private static void run() {
-        try {
-            var settings = configuration.getSettings();
-            List<String> names = settings.getListOfNames();
-            int height = settings.getHeight(names.size());
-
-            Players players = new Players(names);
-            Ladder ladder = new Ladder(players.numberOf(), height);
-
-            LadderService service = new LadderService(players, ladder);
-            String result = service.resultOfPlay();
-            println.accept(result);
-
-            scanClose();
-        } catch (IllegalArgumentException | NullPointerException exception) {
-            println.accept(exception.getMessage());
-            run();
-        }
+        Server server = Server.getInstance();
+        server.run();
+        scanClose();
     }
 }
---
+++ src/com/sh/Server.java
@@ -0,0 +1,48 @@
+package com.sh;
+
+import static com.sh.utils.Output.*;
+
+import java.util.List;
+
+import com.sh.domains.Ladder;
+import com.sh.domains.LadderService;
+import com.sh.domains.Players;
+
+public class Server {
+	private static final Server SERVER = new Server();
+
+	private Server() {
+	}
+
+	public static Server getInstance() {
+		return SERVER;
+	}
+
+	private static Configuration configuration = Configuration.getInstance();
+	private List<String> names;
+	private int height;
+	private Players players;
+	private Ladder ladder;
+	private LadderService ladderService;
+
+	void run() {
+		try {
+			ready();
+			players = new Players(names);
+			ladder = new Ladder(players.numberOf(), height);
+
+			ladderService = new LadderService(players, ladder);
+			String result = ladderService.resultOfPlay();
+			println.accept(result);
+		} catch (IllegalArgumentException | NullPointerException exception) {
+			println.accept(exception.getMessage());
+			run();
+		}
+	}
+
+	private void ready() {
+		var settings = configuration.getSettings();
+		names = settings.getListOfNames();
+		settings.getHeight(names.size());
+	}
+}
