3007627340bc35fed551afc23b9f35a7de57b0be
who-hoo <who.ho3ov@gmail.com>
2022-02-16 15:13:42 +0900

    :sparkles: Player 이름 예외 케이스 추가, 검증 메서드 분리
    - null, empty, blank인 경우 추가
    - 입력받은 이름을 검증하는 메서드 따로 분리

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -5,13 +5,20 @@ class Player {
     private String name;
 
     public Player(String name) {
-        if (name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException("플레이어 이름의 최대 길이는 " + MAX_NAME_LENGTH + "입니다.");
-        }
+        validateName(name);
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isEmpty() || name.isBlank()) {
+            throw new IllegalArgumentException("플레이어의 이름을 입력해주세요.");
+        }
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("플레이어 이름의 최대 길이는 " + MAX_NAME_LENGTH + "입니다.");
+        }
     }
 }
