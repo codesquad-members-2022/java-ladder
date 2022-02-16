8d7e1f40e525b2a612193e15985bb8e5031412c3
Minseok-Choi <cms05041@gmail.com>
2022-02-16 16:29:50 +0900

    refactor : InputView 상수 수정 및 메서드 수정
    askHowManyPlayers() -> askPlayers()
    PLAYER_COUNT_COMMAND -> PLAYER_NAMES_COMMAND
    PLAYER_NAMES_RESTRITIONS 추가

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final String PLAYER_COUNT_COMMAND ="참여할 사람은 몇 명인가요?";
-    private static final String LADDER_HEIGHT_COMMAND ="최대 사다리 높이는 몇 개인가요?";
+    private static final String PLAYER_NAMES_COMMAND = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String PLAYER_NAMES_RESTRICTIONS = "[플레이어의 이름은 5자 이내(알파벳,숫자만가능), 가능 플레이어의 수는 2명이상 10명이하입니다.]";
+    private static final String LADDER_HEIGHT_COMMAND = "최대 사다리 높이는 몇 개인가요?";
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static int askHowManyPlayers() {
-        System.out.println(PLAYER_COUNT_COMMAND);
-        return Integer.parseInt(scanner.nextLine());
+    public static String askPlayers() {
+        System.out.println(PLAYER_NAMES_COMMAND);
+        System.out.println(PLAYER_NAMES_RESTRICTIONS);
+        return scanner.nextLine().replaceAll(" ","");
     }
 
     public static int askLadderHeight() {
