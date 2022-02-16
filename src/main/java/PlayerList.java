3b8a35508d192d0fea95f2cbcd9d1dc6723babfb
who-hoo <who.ho3ov@gmail.com>
2022-02-16 15:29:35 +0900

    :sparkles: PlayerList 타입 생성

---
+++ src/main/java/PlayerList.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerList {
+
+    private List<Player> players = new ArrayList<>();
+
+    private PlayerList() {
+    }
+    
+    public static PlayerList of(String... names) {
+        PlayerList players = new PlayerList();
+        for (String name : names) {
+            players.add(new Player(name));
+        }
+        return players;
+    }
+
+    public void add(Player player) {
+        players.add(player);
+    }
+
+    public int count() {
+        return players.size();
+    }
+}
