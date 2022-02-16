e047a86aad144f4e8c727d2fd7f1e276ba0b9eb4
ikjo93 <auddlr100@naver.com>
2022-02-16 13:25:19 +0900

    GameDisplay 클래스 수정 : 개선된 사다리 모양 출력을 위한 메서드 구현
    수정사항
    1. showLadderInfo 메서드는 당초 2차원 배열을 입력받았었지만 이를 수정하여 플레이어 이름을 저장하고 있는 1차원 ArrayList와 지도 정보를저장하고 있는 2차원 ArrayList를 입력받도록 했습니다.
    2. 플레이어 이름을 먼저 출력시킨 후에 지도 정보를 출력시키도록 했습니다.
    3. getPaddingString을 통해 플레이어 이름을 5글자 기준으로 출력되도록 했습니다.
     - ex. ikjo → " ikjo", honux → "hounx", i → "  i  "

--- GameDisplay.java
+++ GameDisplay.java
@@ -1,23 +1,48 @@
-package step2;
+package step3;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class GameDisplay {
 
-    public static void showGuideToPlayerCnt() {
-        System.out.println("참여할 사람은 몇 명인가요?");
+    private static final int paddingValue = 5;
+
+    public static void guideInputPlayerName() {
+        System.out.println("참여할 사람의 이름을 입력하세요.");
     }
 
-    public static void showGuideToLadderHeight() {
+    public static void showError() {
+        System.out.println("잘못된 형식의 데이터를 입력하셨습니다..");
+    }
+
+    public static void guideInputLadderHeight() {
         System.out.println("최대 사다리 높이는 몇개 인가요?");
     }
 
-    public static void showLadderInfo(String[][] ladderInfo) {
+    public static void showLadderInfo(ArrayList<String> players, ArrayList<ArrayList<String>> ladderInfo) {
+        System.out.printf("%n  ");
+        for (String player : players) {
+            System.out.printf(getPaddingString(player) + " ");
+        }
         System.out.println();
-        for (String[] item : ladderInfo) {
-            Arrays.stream(item).forEach(System.out::print);
+        for (ArrayList<String> item : ladderInfo) {
+            System.out.print("    ");
+            item.forEach(System.out::print);
             System.out.println();
         }
+    }
+
+    private static String getPaddingString(String player) {
+        if (paddingValue <= player.length()) return player;
+        // Middle Padding
+        StringBuilder sb = new StringBuilder(paddingValue);
+        for (int i = 0; i < (paddingValue - player.length()) / 2; i++) {
+            sb.append(" ");
+        }
+        sb.append(player);
+        while (sb.length() < paddingValue) {
+            sb.append(" ");
+        }
+        return sb.toString();
     }
 
 }
