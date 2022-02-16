c046b9642af47445e70c3ac2bc52a36c7497986e
naneun <yhsep7@gmail.com>
2022-02-17 03:13:40 +0900

    Refactor: GameMessage 하드코딩 메시지 수정
    - 상수 값을 사용하여 제한 글자수를 나타내도록 수정했습니다.

--- src/main/java/application/message/GameMessage.java
+++ src/main/java/application/message/GameMessage.java
@@ -1,8 +1,10 @@
 package application.message;
 
+import static application.validator.InputValidator.MAX_NAME_LENGTH;
+
 public class GameMessage {
     public static final String REQUEST_PLAYER_NAMES = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     public static final String REQUEST_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
-    public static final String PLAYER_NAME_LENGTH_ERROR_MESSAGE = "플레이어 명은 5글자를 초과할 수 없습니다.";
+    public static final String PLAYER_NAME_LENGTH_ERROR_MESSAGE = "플레이어 명은 " + MAX_NAME_LENGTH + "글자를 초과할 수 없습니다.";
     public static final String RUNTIME_ERROR_MESSAGE = "게임 실행 중 에러가 발생했습니다.";
 }
