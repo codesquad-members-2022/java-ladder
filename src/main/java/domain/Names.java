0d70d0f20a1abfc5072b17a01bc8023b38820137
Minseok-Choi <cms05041@gmail.com>
2022-02-16 15:02:01 +0900

    feat : Names 클래스 생성
    플레이어의 이름들을 list에 저장하고, 향후 사다리 결과를 인덱스로 처리하거나 리팩토링 해야함

--- src/main/java/domain/Names.java
+++ src/main/java/domain/Names.java
@@ -1,2 +1,20 @@
-package domain;public class Names {
+package domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Names {
+
+    private List<String> names;
+
+    public Names(String names) {
+        this.names = new ArrayList<>();
+        putNames(names);
+    }
+
+    private void putNames(String names) {
+        String[] arr = names.split(",");
+        Collections.addAll(this.names, arr);
+    }
 }
