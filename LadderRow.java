c74f561b397f6e1ccbdad91d59a375245bdf3863
PhilSoGooood <peelhw@gmail.com>
2022-02-17 02:35:02 +0900

    사다리의 한줄을 만드는 기능 구현
    출력시 간격을 맞추기 위한 공백 BLANK와 사다리의 넓이만큼
    사다리의 라인을 형성하여 리스트로 반환한다.

--- LadderRow.java
+++ LadderRow.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class LadderRow {
+import java.util.ArrayList;
+import java.util.List;
+
+class LadderRow {
+	private static final String BLANK = "  ";
+	private final List<String> row = new ArrayList<>();
+	private final LadderLine ladderLine = new LadderLine();
+
+	List<String> make(int width){
+		row.add(BLANK);
+		for(int column = 0; column < width; column++){
+			String ladderElement = ladderLine.make(column);
+			row.add(ladderElement);
+		}
+		return row;
+	}
 }
