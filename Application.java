fbf4a645990ccdc783c8db879019100a577d5ddd
PhilSoGooood <peelhw@gmail.com>
2022-02-17 02:46:58 +0900

    메인 메서드와 실행을 담당하는 메서드 분리
    메인 메소드는 최대한 간단하게 하라는 코드리뷰를 보아
    실행 메서드를 분리하였습니다.

--- Application.java
+++ Application.java
@@ -2,16 +2,22 @@ import java.io.IOException;
 import java.util.List;
 
 public class Application {
-	public static void main(String[] args) throws IOException {
+	private static void run() throws IOException {
 		InputView inputView = new InputView();
 		List<String> playerNames = inputView.getPlayerName();
 		int ladderHeight = inputView.getLadderHeight();
 
 
 		Ladder ladder = new Ladder();
-		char[][] ladderForGame = ladder.make(playerNames, ladderHeight);
+		ladder.setWidth(playerNames);
+		List<List<String>> ladderForGame = ladder.make(ladderHeight);
 
 		OutView outView = new OutView();
+		outView.printPlayerName(playerNames);
 		outView.print(ladderForGame);
+	}
+
+	public static void main(String[] args) throws IOException {
+		run();
 	}
 }
