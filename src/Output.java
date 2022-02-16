8ebf913c6ba7ea6d22aaacd4bff268b8b4554529
rkolx <olx.rko.o@gmail.com>
2022-02-16 11:35:45 +0900

    Output 클래스 내 printLadder 메소드의 인덴트를 줄이기 위해 이중포문을 makeColumn 메소드로 분리하여 구현

--- src/Output.java
+++ src/Output.java
@@ -1,13 +1,20 @@
 public class Output {
 
+    StringBuilder sb = new StringBuilder();
 
     public void printLadder(char[][] ladder){
-        for (int i = 0; i < ladder.length; i++) {
-            for (int j = 0; j < ladder[0].length; j++) {
-                System.out.print(ladder[i][j]);
-            }
-            System.out.println();
+        for (int row = 0; row < ladder.length; row++) {
+            makeColumn(ladder, row);
+            sb.append("\n");
         }
+        System.out.println(sb);
+    }
+
+    public char[][] makeColumn(char[][] ladder, int row){
+        for (int column = 0; column < ladder[row].length; column++) {
+            sb.append(ladder[row][column]);
+        }
+        return ladder;
     }
 
 }
