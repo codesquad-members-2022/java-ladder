d1ab9a5a7b55cbf517cc37b0ba63608d4aa9d268
seyeong <seyoung7555@naver.com>
2022-02-16 14:54:41 +0900

    refactor: LadderGameManager 내부 메서드 분리
    - init() 함수 내부에서 입력받는 함수 호출부를 따로 메서드로 분리했습니다.

--- src/main/laddergame/LadderGameManager.java
+++ src/main/laddergame/LadderGameManager.java
@@ -5,18 +5,24 @@ import main.controller.InputController;
 import java.io.IOException;
 
 public class LadderGameManager {
+    private static final int MAX_NAME_LENGTH = 5;
+
     private LadderGame ladderGame;
 
     public void init() {
         try {
-            int playerNum = InputController.inputNumber("참여할 사람은 몇 명인가요?");
-            int ladderHeight = InputController.inputNumber("최대 사다리 높이는 몇 개인가요?");
-            String[] playerNames = InputController.inputStringWithDelimiter("참여할 사람 이름(최대 5글자)을 입력하세요. (이름은 쉼표(,)로 구분하세요)", ",");
-            ladderGame = new LadderGame(playerNum, ladderHeight, playerNames);
+            createGameWithInput();
             InputController.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void createGameWithInput() throws IOException {
+        int playerNum = InputController.inputPositiveNumber("참여할 사람은 몇 명인가요?");
+        int ladderHeight = InputController.inputPositiveNumber("최대 사다리 높이는 몇 개인가요?");
+        String[] playerNames = InputController.inputStringWithDelimiter("참여할 사람 이름(최대 5글자)을 입력하세요. (이름은 쉼표(,)로 구분하세요)", ",", MAX_NAME_LENGTH);
+        ladderGame = new LadderGame(playerNum, ladderHeight, playerNames);
     }
 
     public void showLadder() {
