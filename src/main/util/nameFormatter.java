49e3296caf51ce2d533d241041e1bbf061283997
bugpigg <bugpigg@gmail.com>
2022-02-16 16:57:59 +0900

    feat : 사용자 이름 수정 클래스 추가
    - 이름 길이가 5이하인 경우, 부족한 부분을  "  " 채워 모든 이름이 5의 길이를 가지도록 하였습니다.
    - 이를 수행하는 nameFormatter 클래스를 생성하였습니다.

---
+++ src/main/util/nameFormatter.java
@@ -0,0 +1,18 @@
+package main.util;
+
+import main.util.InputValidator;
+
+public class nameFormatter {
+
+    public static final int MAX_LENGTH = InputValidator.MAXIMUM_NAME_LENGTH;
+
+    public static String blankPadding(String name) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < MAX_LENGTH; i++) {
+            sb.append(" ");
+        }
+        int startIndex = MAX_LENGTH/2 - name.length()/2;
+        sb.replace(startIndex, startIndex+name.length(), name);
+        return sb.toString();
+    }
+}
