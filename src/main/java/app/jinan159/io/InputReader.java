c0a20dfeea9ba408f873888cf99967fda516cee5
jinan159 <wlsdhks0423@naver.com>
2022-02-17 01:03:55 +0900

    LadderGame 의 참여자 수, 사다리 높이 입력 로직을 InputReader 클래스로 분리
    - InputReader 클래스 추가
      입력값 검증 로직은 InputReader 에서 모두 처리합니다.
    - 예외 처리 흐름 개선
      예상치 못한 예외가 발생하면 상위로 throw 하여, Application 클래스에서 처리합니다.

---
+++ src/main/java/app/jinan159/io/InputReader.java
@@ -0,0 +1,78 @@
+package app.jinan159.io;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class InputReader implements Closeable {
+
+    private final static String Q_NUMBER_OF_PARTICIPANTS = "참여할 사람은 몇 명 인가요?(1명 이상)";
+    private final static String Q_MAX_LADDER_HEIGHT = "참여할 사람은 몇 명 인가요?(1명 이상)";
+    private final static String ALERT_NUMBER_REQUIRED = "(주의) 숫자만 입력해 주세요.";
+    private final static String ALERT_VALIDATION_FAILED = "(주의) 입력하신 숫자를 다시한번 확인해 주세요.";
+    private final static int READ_FAILED = -1;
+
+    public final Scanner sc;
+
+    public InputReader() {
+        this.sc = new Scanner(System.in);
+    }
+
+    public InputReader(InputStream inputStream) {
+        this.sc = new Scanner(inputStream);
+    }
+
+    public int readParticipantCount() {
+        try {
+            System.out.println(Q_NUMBER_OF_PARTICIPANTS);
+            int count = readPositiveNumber();
+            validateIsReadFaild(count);
+            return count;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return readParticipantCount();
+        }
+    }
+
+    public int readHeight() {
+        try {
+            System.out.println(Q_MAX_LADDER_HEIGHT);
+            int height = readPositiveNumber();
+            validateIsReadFaild(height);
+            return height;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return readHeight();
+        }
+    }
+
+    private void validateIsReadFaild(int input) {
+        if (input == READ_FAILED) {
+            throw new IllegalArgumentException(ALERT_VALIDATION_FAILED);
+        }
+    }
+
+    private int readPositiveNumber() {
+        try {
+            int input = sc.nextInt();
+            validateIsPositive(input);
+            return sc.nextInt();
+        } catch (NoSuchElementException e) {
+            System.out.println(ALERT_NUMBER_REQUIRED);
+            return READ_FAILED;
+        }
+    }
+
+    private void validateIsPositive(int input) {
+        if (input <= 0) {
+            throw new IllegalArgumentException(ALERT_VALIDATION_FAILED);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        sc.close();
+    }
+}
--- src/main/java/app/jinan159/ladder/LadderGame.java
+++ src/main/java/app/jinan159/ladder/LadderGame.java
@@ -1,10 +1,12 @@
 package app.jinan159.ladder;
 
+import app.jinan159.io.InputReader;
 import app.jinan159.ladder.meta.LadderElement;
 import app.jinan159.ladder.meta.GameMap;
 import app.jinan159.ladder.stretegy.DefaultGameMapStretegy;
 import app.jinan159.ladder.stretegy.LadderGameMapStretegy;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class LadderGame {
@@ -12,54 +14,19 @@ public class LadderGame {
     private final GameMap gameMap;
     private final LadderGameMapStretegy stretegy;
 
-    public LadderGame() {
+    public LadderGame() throws IOException {
         this(new DefaultGameMapStretegy());
     }
 
-    public LadderGame(LadderGameMapStretegy stretegy) {
-        try (Scanner sc = new Scanner(System.in)) {
-            int participantCount = readParticipantCount(sc);
-            int height = readHeight(sc);
+    public LadderGame(LadderGameMapStretegy stretegy) throws IOException {
+        try (InputReader reader = new InputReader()) {
+            int participantCount = reader.readParticipantCount();
+            int height = reader.readHeight();
             this.gameMap = new GameMap(participantCount, height);
         }
 
         this.stretegy = stretegy;
         prepareGameMap(this.gameMap);
-    }
-
-    // ------- initialize private method ---------
-    private int readParticipantCount(Scanner sc) {
-        try {
-            System.out.println("참여할 사람은 몇 명 인가요?(1명 이상)");
-            int count = sc.nextInt();
-            validateParticipantCount(count);
-            return count;
-        } catch (Exception e) {
-            return readParticipantCount(sc);
-        }
-    }
-
-    private void validateParticipantCount(int count) {
-        if (count <= 0) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private int readHeight(Scanner sc) {
-        try {
-            System.out.println("최대 사다리 높이는 몇 개인가요?(1개 이상)");
-            int height = sc.nextInt();
-            validateHeight(height);
-            return height;
-        } catch (Exception e) {
-            return readHeight(sc);
-        }
-    }
-
-    private void validateHeight(int height) {
-        if (height <= 0) {
-            throw new IllegalArgumentException();
-        }
     }
 
     private void prepareGameMap(GameMap gameMap) {
