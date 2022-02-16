fa0a9d9c27d45d350084a7dce50472b9dbeceb40
KTH-96 <75709176+KTH-96@users.noreply.github.com>
2022-02-16 10:04:01 +0900

    [Yan] 사다리 게임 2단계 - 리팩토링 맛보기 (#92)
    * Changes not staged for commit 문제해결을 위한 커밋
    
    * 메세지 없이 하고싶었지만 왜인지 vim편집기가 실행이 되지않는다
    
    * 이건 왜 빠져있었을까
    
    * 이건 왜 빠져있었을까
    
    * 이건 왜 빠져있었을까
    
    * refeator : getDrawingLadder 메서드 변경
    기존에 이중 포문으로 되어있던 부분을 두번째 반복문을 getMakeLine이라는 메소드로 빼냈습니다
    
    * redase --contiue를 하면서 차근차근 받아온다는걸 알았습니다

---
+++ src/main/java/Ladder.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+
+public class Ladder {
+    public String[][] makeLadder(int n, int m) {
+        return getDrawingLadder(getLadderArr(getLine(n), m));
+    }
+
+    private int getLine(int n) {
+        return (2 * n) - 1;
+    }
+
+    private String[][] getLadderArr(int line, int m) {
+        String[][] ladder = new String[line][m];
+        return ladder;
+    }
+
+    private String[][] getDrawingLadder(String[][] ladder) {
+        for (int i = 0; i < ladder.length; i++) { //세로
+            ladder = getMakeLine(ladder, i);
+        }
+        return ladder;
+    }
+
+    private String[][] getMakeLine(String[][] ladder, int i) {
+        for (int j = 0; j < ladder[i].length; j++) {//가로
+            ladder = drawingLine(ladder, i, j);
+            ladder = drawingStick(ladder, i, j);
+        }
+        return ladder;
+    }
+
+    private String[][] drawingLine(String[][] ladder, int i, int j) {
+        if (getRandomNum() == 0) {
+            ladder[i][j] = "--";
+            return ladder;
+        }
+        ladder[i][j] = "  ";
+        return ladder;
+    }
+
+    private String[][] drawingStick(String[][] ladder, int i, int j) {
+        if (j % 2 == 0){
+            ladder[i][j] = "|";
+        }
+        return ladder;
+    }
+
+    private int getRandomNum() {
+        Random random = new Random();
+        return random.nextInt(2);
+    }
+}
