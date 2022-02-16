b5dd915860e8fcab248b6eb958d77adfca681b94
who-hoo <who.ho3ov@gmail.com>
2022-02-17 01:55:06 +0900

    :sparkles: players 필드 추가

--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -9,16 +9,37 @@ public class LadderGame {
     private final OutputView output = new OutputView();
 
     private Ladder ladder;
+    private PlayerList players;
 
     public void run() {
         initGame();
     }
 
     private void initGame() {
-        int entry = getEntry();
+        initPlayers();
+        int entry = players.count();
         int height = getHeight();
         this.ladder = new Ladder(entry, height);
         output.print(ladder.toString());
+    }
+
+    private void initPlayers() {
+        String[] names = getPlayerNames();
+        try {
+            this.players = PlayerList.of(names);
+        } catch (IllegalArgumentException e) {
+            output.print(e.getMessage());
+            initPlayers();
+        }
+    }
+
+    private String[] getPlayerNames() {
+        try {
+            return validator.names(input.getPlayerNames());
+        } catch (IllegalArgumentException e) {
+            output.print(e.getMessage());
+            return getPlayerNames();
+        }
     }
 
     private int getEntry() {
