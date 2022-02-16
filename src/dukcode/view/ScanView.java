82df5d3c3f8ae57486f241bccba1de7f8e73505c
dukcode <duk9741@gmail.com>
2022-02-16 10:59:27 +0900

    이름 앞 뒤로 공백 삭제

--- src/dukcode/view/ScanView.java
+++ src/dukcode/view/ScanView.java
@@ -15,7 +15,7 @@ public class ScanView {
         System.out.println("참여할 사람의 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요");
         String[] namePlayers = sc.nextLine().split(",");
         for (String name : namePlayers) {
-            name = getNameLengthBelow5(name);
+            name = getNameLengthBelow5(name.trim());
         }
         return namePlayers;
     }
