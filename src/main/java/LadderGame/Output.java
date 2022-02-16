bfe63157e13769a0ea01d31e30f471b695cf97dc
donggi-lee-bit <devdonggilee@gmail.com>
2022-02-16 15:49:33 +0900

    add : user 입력 메서드로 분리
    - input() 메서드에서 user와 ladder height를 다 받고 있었는데 유저 이름을
      출력해주기 위해서 역할을 나누기 위해 inputUser() 메서드를
      만들었습니다.

--- src/main/java/LadderGame/Output.java
+++ src/main/java/LadderGame/Output.java
@@ -5,7 +5,7 @@ public class Output {
     private static StringBuilder sb = new StringBuilder();
 
     public static void printUserCountNotification() {
-        System.out.println("참여할 사람은 몇 명인가요?");
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
     }
 
     public static void printLadderCountNotification() {
