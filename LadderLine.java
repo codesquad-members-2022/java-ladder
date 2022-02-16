d31739688e79b4d5936c07578fc60723d070bcf6
PhilSoGooood <peelhw@gmail.com>
2022-02-17 02:15:54 +0900

    사다리 라인 길이 수정 및 랜덤 결정 기능 별도 분리
    사다리 라인의 길이를 기능 요구사항에 맞게 5로 수정하면서 타입을
    String으로 분리하였음
    
    사다리 라인의 여부를 랜덤으로 정하는 기능을 setRandom이라는 별도의
    메서드로 분리하였고 사다리 프레임 "|"을 반환할때마다 실행함.

--- LadderLine.java
+++ LadderLine.java
@@ -1,21 +1,26 @@
 import java.util.Random;
 
 class LadderLine {
-	private static final char LADDER_FRAME = '|';
-	private static final char LADDER_LINE = '-';
-	private static final char BLANK = ' ';
+	private static final	Random random = new Random();
+	private static final String LADDER_FRAME = "|";
+	private static final String LADDER_LINE = "-----";
+	private static final String BLANK = "     ";
+	private static boolean hasLine = true;
 
-	char make(int column){
-		Random random = new Random();
-
+	String make(int column){
 		if(column % 2 == 0){
+			setRandom();
 			return LADDER_FRAME;
 		}
 
-		if(random.nextBoolean()){
+		if(hasLine){
 			return LADDER_LINE;
 		}
 
 		return BLANK;
+	}
+
+	void setRandom() {
+		hasLine = random.nextBoolean();
 	}
 }
