a4cc25780bd09196c8c8bb8ebb71a73b74d56d19
nak253 <nwon4413@naver.com>
2022-02-16 10:22:01 +0900

    OutputView클래스의 printGhostLeg() 내부 간소화
    - 두 개의 for문을 하나의 for문으로 간소화 작업을 진행했습니다.

--- src/ladder/OutputView.java
+++ src/ladder/OutputView.java
@@ -1,18 +1,38 @@
 package ladder;
 
 public class OutputView {
+    private String row;
+    private String column;
+    private String empty;
+
+    public OutputView() {
+        this.row = "|";
+        this.column = "-";
+        this.empty = " ";
+
+    }
+
     public void printGhostLeg(int[][] ghostLeg) {
         for (int i = 0; i < ghostLeg.length; i++) {
-            System.out.print("|");
-            for (int j = 1; j < ghostLeg[i].length; j += 2) {
-                if (ghostLeg[i][j] == 1) {
-                    System.out.print("-");
-                }else {
-                    System.out.print(" ");
-                }
-                System.out.print("|");
+            for (int j = 0; j < ghostLeg[i].length; j++) {
+                printElements(ghostLeg[i][j]);
             }
             System.out.println();
+        }
+    }
+
+    private void printElements(int i) {
+        if (i == 0) {
+            System.out.print(empty);
+            return;
+        }
+        if (i == 1) {
+            System.out.print(column);
+            return;
+        }
+        if (i == 2) {
+            System.out.print(row);
+            return;
         }
     }
 }
