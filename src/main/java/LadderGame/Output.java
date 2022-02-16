3beaa7ca6d8809c3864ca0ad74c867de1929c42f
donggi-lee-bit <devdonggilee@gmail.com>
2022-02-16 15:32:58 +0900

    fix : 메서드 명 줄인 거 수정
    - Noti -> Notification 으로 수정하였습니다.

--- src/main/java/LadderGame/Output.java
+++ src/main/java/LadderGame/Output.java
@@ -4,11 +4,11 @@ public class Output {
 
     private static StringBuilder sb = new StringBuilder();
 
-    public static void printUserCountNoti() {
+    public static void printUserCountNotification() {
         System.out.println("참여할 사람은 몇 명인가요?");
     }
 
-    public static void printLadderCountNoti() {
+    public static void printLadderCountNotification() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
     }
 
