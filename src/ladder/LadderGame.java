36877a12979284bad51f6a888c6a0a3f9dcb992d
nak253 <nwon4413@naver.com>
2022-02-16 10:29:28 +0900

    LadderGame클래스 createGhostLeg()와 createArr() 메서드 내부 간소화
    - 내부에서 사용된 수식을 가독성있게 수정하였습니다.
    - 두개의 for문을 수식을 사용하여 하나의 for문으로 나타냈습니다.

--- src/ladder/LadderGame.java
+++ src/ladder/LadderGame.java
@@ -12,18 +12,19 @@ public class LadderGame {
 
     private int[][] creatGhostLeg(int numberOfParticipants, int LegLength) {
         int row = LegLength;
-        int column = numberOfParticipants + (numberOfParticipants - 1);
-        int[][] ghostLeg = createArr(row,column);
+        int column = (numberOfParticipants * 2) - 1;
+        int[][] ghostLeg = createArr(row, column);
         return ghostLeg;
     }
 
     private int[][] createArr(int row, int column) {
         int[][] arr = new int[row][column];
         for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j += 2) {
-                arr[i][j] = 1;
-            }
-            for (int j = 1; j < column; j += 2) {
+            for (int j = 0; j < column; j++) {
+                if (j % 2 == 0) {
+                    arr[i][j] = 2;
+                    continue;
+                }
                 arr[i][j] = (int) (Math.random() * 2);
             }
         }
