9407610ee2a6263b173c03804a96019b0e2f44b2
who-hoo <who.ho3ov@gmail.com>
2022-02-16 16:38:07 +0900

    :sparkles: 플레이어 이름 입력 받기 기능 추가 구현

--- src/main/java/io/InputView.java
+++ src/main/java/io/InputView.java
@@ -18,4 +18,10 @@ public class InputView {
         System.out.print(PROMPT);
         return sc.nextLine();
     }
+
+    public String getPlayers() {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        System.out.print(PROMPT);
+        return sc.nextLine();
+    }
 }
