111c8df843726c82bff6d43b6011bf0a76a12b0e
hwicode <khc3796@naver.com>
2022-02-16 18:46:43 +0900

    Ladder 클래스의 String 2차 배열을 char 2차 배열로 수정 및 이름 변경

--- java/src/Output.java
+++ java/src/Output.java
@@ -1,12 +1,12 @@
 public class Output {
 
-    private void printOneHight(String[] ladderOneHight) {
+    private void printOneHight(char[] ladderOneHight) {
         for (int i = 0; i < ladderOneHight.length; i++) {
             System.out.print(ladderOneHight[i]);
         }
     }
 
-    public void printLadder(String[][] ladder) {
+    public void printLadder(char[][] ladder) {
         for (int i = 0; i < ladder.length; i++) {
             printOneHight(ladder[i]);
             System.out.println();
