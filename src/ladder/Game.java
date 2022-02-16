35f7856fab4a09f1b3fdbeb194d6a0c8aac8aa94
Jaehong Choe <qnalsfj@gmail.com>
2022-02-16 11:22:06 +0900

    refactor: 불필요한 클래스 제거
    - GameManager클래스를 불필요하게 한번 더 클래스로 감싸던 로직을 제거. GameManager를
    제거하고 GameManager의 코드를 Game클래스로 이동.

--- src/ladder/Game.java
+++ src/ladder/Game.java
@@ -2,9 +2,28 @@ package src.ladder;
 
 public class Game {
 
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private Ladder ladder;
+
     public static void main(String[] args) {
-        GameManager gameManager = new GameManager();
-        gameManager.gameStart();
+        Game game = new Game();
+        game.start();
+    }
+
+    private void start() {
+        ladder = createLadder();
+        outputView.printLadder(ladder);
+    }
+
+    private Ladder createLadder() {
+        outputView.printInputPeopleCountGuide();
+        int peopleCount = inputView.inputNumber();
+
+        outputView.printInputLadderHeightInputGuide();
+        int ladderHeight = inputView.inputNumber();
+
+        return new Ladder(peopleCount, ladderHeight);
     }
 
 }
