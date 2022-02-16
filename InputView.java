9b1710ce60921da577217096f1fad64f972fb16b
PhilSoGooood <peelhw@gmail.com>
2022-02-17 02:28:26 +0900

    참가자 이름을 조건에 맞는 리스트로 반환하도록 수정
    입력한 참가자의 이름을 NameEditor 클래스를 이용해
    조건에 맞도록 수정하여 리스트로 반환한다.

--- InputView.java
+++ InputView.java
@@ -1,21 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 class InputView {
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static final String REQUEST_PLAYER_NAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분) \n> ";
 	private static final String REQUEST_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요? \n> ";
-	private List<String> nameList = new ArrayList<>();
 
 
 	List<String> getPlayerName() throws IOException {
 		System.out.print(REQUEST_PLAYER_NAME);
 		String[] names = br.readLine().replace(" ", "").split(",");
-		nameList.addAll(Arrays.asList(names));
+		NameEditor nameEditor = new NameEditor();
+		List<String> nameList = nameEditor.makeList(names);
+
 		return nameList;
 	}
 
@@ -24,10 +23,5 @@ class InputView {
 		int ladderHeight = Integer.parseInt(br.readLine());
 
 		return ladderHeight;
-	}
-
-	int getPlayerNumber() {
-		int playerNumber = nameList.size();
-		return playerNumber;
 	}
 }
