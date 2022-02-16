b0df5605d49d5339f68c67973ebd83cabe78f384
leezzangmin <ckdals12345678@gmail.com>
2022-02-16 11:26:50 +0900

    Feat: OutputView 메소드 분리
    ? 연산자 대신 같은 역할을 하는 메소드를 두개 추가하였음

--- src/OutputView.java
+++ src/OutputView.java
@@ -5,7 +5,7 @@ public class OutputView {
     private static final String invalidUserNamesMessage = "이름의 길이가 너무 깁니다. 다시 입력하세요";
     private static final char sero = '|';
     private static final String garo = "-----";
-    private static final String empty = "     ";
+    private static final String space = "     ";
 
     public static void printUserNumberMessage(){
 
@@ -21,10 +21,25 @@ public class OutputView {
         StringBuilder graphicSadari = new StringBuilder();
         for(int i = 0; i < sadari.length * sadari[0].length; i++) {
             graphicSadari.append(sero);
-            graphicSadari.append( sadari[i/sadari[0].length][i% sadari[0].length] == true ? garo : empty );
-            graphicSadari.append( i % sadari[0].length == sadari[0].length-1 ? sero+"\n" : "" );
+            graphicSadari.append( garoOrSpace( sadari[i/sadari[0].length][i% sadari[0].length] ));
+            graphicSadari.append( seroOrEmpty( i % sadari[0].length, sadari[0].length) );
         }
         System.out.println(graphicSadari);
+    }
+
+    private static String garoOrSpace(boolean sadariElement){
+        if(sadariElement == true){
+            return garo;
+        }
+        return space;
+    }
+
+    // x축 포인터(currentGaroPointer)가 사다리 가로의 마지막 원소를 가리키면 sero+"\n"을 리턴, 아니면 ""(아무것도 아님)을 리턴
+    private static String seroOrEmpty(int lengthOfSadariGaro, int currentGaroPointer){
+        if(lengthOfSadariGaro == currentGaroPointer - 1){
+            return sero+"\n";
+        }
+        return "";
     }
 
     public static void printUserNamesMessage(){
