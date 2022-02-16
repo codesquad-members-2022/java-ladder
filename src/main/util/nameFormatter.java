b37251d5f6a8eb39dacf7016f27616b1ca2b7820
bugpigg <bugpigg@gmail.com>
2022-02-16 19:45:48 +0900

    refact : blankPadding 메서드 수정
    - blankPadding 메서드의 for문을, String의 repeat 메서드를 사용하여 수정하였습니다.

--- src/main/util/nameFormatter.java
+++ src/main/util/nameFormatter.java
@@ -6,9 +6,7 @@ public class nameFormatter {
 
     public static String blankPadding(String name) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < MAX_LENGTH; i++) {
-            sb.append(" ");
-        }
+        sb.append(" ".repeat(MAX_LENGTH));
         int startIndex = MAX_LENGTH/2 - name.length()/2;
         sb.replace(startIndex, startIndex+name.length(), name);
         return sb.toString();
