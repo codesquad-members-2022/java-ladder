90ca34781ce1e57dbab98e708563cfa703b1079e
ikjo93 <auddlr100@naver.com>
2022-02-16 13:17:36 +0900

    UserInterface 클래스 수정 : 사용자 입력 데이터(문자열) 검증 및 ArrayList 변환 메서드 구현
    [수정사항]
    1. checkInput 메서드를 통해 정규표현식을 통해 사용자가 입력한 플레이어 이름들이 ,(쉼표)를 통해 구분되도록 했고 이름 글자수가 최소 1글자 ~ 최대 5글자로 입력되도록 구현했습니다.
      - 정규 표현식 외에 사용자 데이터 입력 시 null return(게임 종료)
    2. 검증이 완료 된 후에는 getPlayerList 메서드를 통해 입력된 플레이어 이름들이 저장된 ArrayList를 불러오도록 했습니다.
    
    기타
    1. inputPlayerCnt 메서드 이름 정정(→ inputPlayerList)

--- UserInterface.java
+++ UserInterface.java
@@ -1,6 +1,8 @@
-package step2;
+package step3;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class UserInterface {
 
@@ -10,14 +12,36 @@ public class UserInterface {
         sc = new Scanner(System.in);
     }
 
-    public int inputPlayerCnt() {
-        GameDisplay.showGuideToPlayerCnt();
-        return Integer.parseInt(sc.nextLine());
+    public ArrayList<String> inputPlayerList() {
+        GameDisplay.guideInputPlayerName();
+        String userInput = sc.nextLine();
+        if (checkInput(userInput)) return null;
+
+        return getPlayerList(userInput);
     }
 
     public int inputLadderHeight() {
-        GameDisplay.showGuideToLadderHeight();
+        GameDisplay.guideInputLadderHeight();
         return Integer.parseInt(sc.nextLine());
+    }
+
+    private boolean checkInput(String userInput) {
+        String inputPattern = "^([^,\s]{1,5})+(,([^,\s]{1,5}))*$";
+        if (!Pattern.matches(inputPattern, userInput)) {
+            GameDisplay.showError();
+            return true;
+        }
+        return false;
+    }
+
+    private ArrayList<String> getPlayerList(String input) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        String[] players = input.split(",");
+        for (int i = 0; i < players.length; i++) {
+            arrayList.add(players[i]);
+        }
+
+        return arrayList;
     }
 
 }
