3f9120eaee0891d215a40f7cb82d4ad9d3a74675
“kukim” <kukim.dev@gmail.com>
2022-02-16 14:43:54 +0900

    feat: Line 클래스 구현
    - Line.createLineWithPlayerCount() 메서드를 통해 Line을 생성한다.
    - Line 클래스의 사람 수는 List<Boolean> points 이다. (false = 비어있음, 연속된 true 2개 =  사다리 있음)
    - Line.drawRadder() 메서드를 통해 사다리를 그린다. (사람이 3명이라면 2개의 사다리만 그릴 수 있음)
    - Line.checkPos()를 통해 입력에 대한 예외처리 한다.

---
+++ src/main/java/Line.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Line {
+    private List<Boolean> points;
+    private int maxLength;
+
+    private Line() {
+    }
+
+    public static Line createLineWithPlayerCount(int count) {
+        Line line = new Line();
+        line.maxLength = count;
+        line.points = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            line.points.add(false);
+        }
+        return line;
+    }
+
+    public boolean isRadder(int radderPos) {
+        checkPos(radderPos);
+        return points.get(radderPos) && points.get(radderPos + 1);
+    }
+
+    private void checkPos(int point) {
+        if (point < 0 || point >= maxLength - 1) {
+            throw new IllegalArgumentException("pos가 정상 범위를 넘었습니다.");
+        }
+    }
+
+    public void drawRadder(int radderPos) {
+        checkPos(radderPos);
+        points.set(radderPos, true);
+        points.set(radderPos + 1, true);
+    }
+}
