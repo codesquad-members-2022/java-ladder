a83c644837014370fce009436074c71b3be4c7da
nak253 <nwon4413@naver.com>
2022-02-16 10:56:48 +0900

    OutputView클래스의 메서드 내부에서 사용되는 지역 변수 이름을 수정하였다.
    - 지역 변수의 이름을 알아보기 쉬운 이름으로 변경하였다.

--- src/ladder/OutputView.java
+++ src/ladder/OutputView.java
@@ -13,24 +13,24 @@ public class OutputView {
     }
 
     public void printGhostLeg(int[][] ghostLeg) {
-        for (int i = 0; i < ghostLeg.length; i++) {
-            for (int j = 0; j < ghostLeg[i].length; j++) {
-                printElements(ghostLeg[i][j]);
+        for (int row = 0; row < ghostLeg.length; row++) {
+            for (int column = 0; column < ghostLeg[row].length; column++) {
+                printElements(ghostLeg[row][column]);
             }
             System.out.println();
         }
     }
 
-    private void printElements(int i) {
-        if (i == 0) {
+    private void printElements(int element) {
+        if (element == 0) {
             System.out.print(empty);
             return;
         }
-        if (i == 1) {
+        if (element == 1) {
             System.out.print(column);
             return;
         }
-        if (i == 2) {
+        if (element == 2) {
             System.out.print(row);
             return;
         }
