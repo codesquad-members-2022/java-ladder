26375b64cacafc3bfdbaaee2b7ad6012b83ff2de
Minseok-Choi <cms05041@gmail.com>
2022-02-16 17:19:50 +0900

    refactor : LINE 상수 수정("-" -> "-----")

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class Line {
 
-    private static final String LINE = "-";
+    private static final String LINE = "-----";
     private static final String BLANK = " ";
     private final Random random;
 
