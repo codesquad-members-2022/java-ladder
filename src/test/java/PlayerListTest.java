b60af8bb83d1387acbcae81c60c927b56cbf4174
who-hoo <who.ho3ov@gmail.com>
2022-02-16 15:31:15 +0900

    :white_check_mark: PlayerList 테스트 코드 작성

---
+++ src/test/java/PlayerListTest.java
@@ -0,0 +1,21 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PlayerListTest {
+
+    @Test
+    @DisplayName("유효한 이름만 입력하여 플레이어 리스트 생성")
+    void createPlayerList_O() {
+        PlayerList players = PlayerList.of("pobi", "honux", "crong", "jk");
+        assertThat(players.count()).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 이름을 포함하면 플레이어 리스트 생성 실패")
+    void createPlayerList_X() {
+        assertThatThrownBy(() -> PlayerList.of("pobi", "hohonux", "crong", "jk"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
