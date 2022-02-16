00729dd9ecdd6f85975208e4c5f3e5db96901dd3
who-hoo <who.ho3ov@gmail.com>
2022-02-17 03:24:25 +0900

    :zap: 이차원 배열 -> ArrayList<LadderRow>로 구조 개선

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -1,66 +1,27 @@
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Ladder {
 
-    private static final Random random = new Random();
-
-    private final Element[][] ladder;
+    private final List<LadderRow> ladder;
 
     public Ladder(int width, int height) {
-        this.ladder = new Element[height][width];
-        initLadder();
+        this.ladder = new ArrayList<>();
+        initLadder(width, height);
     }
 
-    private boolean isRail(int col) {
-        return col % 2 == 0;
-    }
-
-    private boolean isOverlap(int row, int col) {
-        return 2 < col && ladder[row][col - 2] != Element.EMPTY;
-    }
-
-    private Element createRandomStep() {
-        int n = random.nextInt(2);
-        return (n == 0) ? Element.EMPTY : Element.STEP;
-    }
-
-    private void fillLadderElement(int row, int col) {
-        if (isRail(col)) {
-            ladder[row][col] = Element.RAIL;
-            return;
+    private void initLadder(int width, int height) {
+        for (int row = 0; row < height; row++) {
+            ladder.add(new LadderRow(width));
         }
-        if (isOverlap(row, col)) {
-            ladder[row][col] = Element.EMPTY;
-            return;
-        }
-        ladder[row][col] = createRandomStep();
-    }
-
-    private void initLadderRow(int row) {
-        for (int col = 0; col < ladder[row].length; col++) {
-            fillLadderElement(row, col);
-        }
-    }
-
-    private void initLadder() {
-        for (int row = 0; row < ladder.length; row++) {
-            initLadderRow(row);
-        }
-    }
-
-    private StringBuilder appendLadderRow(StringBuilder sb, int row) {
-        for (Element col : ladder[row]) {
-            sb.append(col.getValue());
-        }
-        sb.append("\n");
-        return sb;
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (int row = 0; row < ladder.length; row++) {
-            appendLadderRow(sb, row);
+        for (LadderRow ladderRow : ladder) {
+            sb.append(ladderRow.toString());
+            sb.append("\n");
         }
         return sb.toString();
     }
