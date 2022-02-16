4fa7c132a9d7a6f334eb05b1799679b933bcfa56
WooJin Park <ak2j38@gmail.com>
2022-02-16 15:08:13 +0900

    feature : 게임 참여하는 플레이어의 이름을 입력받는 기능 추가
    1. InputView에서 플레이어 이름을 입력받고 최대 5자리인지 검증함
    
    end

--- src/main/java/controller/LadderGame.java
+++ src/main/java/controller/LadderGame.java
@@ -4,7 +4,11 @@ import domain.Ladder;
 import view.InputView;
 import view.OutputView;
 
+import java.util.List;
+
 public class LadderGame {
+
+    private List<String> playerNameList;
 
     public void start() {
         Ladder ladder = init();
@@ -13,7 +17,8 @@ public class LadderGame {
 
     private Ladder init() {
         InputView.init();
-        int playersCount = InputView.getPlayersCount();
+        playerNameList = InputView.getPlayersName();
+        int playersCount = playerNameList.size();
         int maxLadderHeight = InputView.getMaxLadderHeight();
         InputView.close();
 
