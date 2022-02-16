79b7945a0c8454336a7070e5010b9d1ddf71ecd9
leezzangmin <ckdals12345678@gmail.com>
2022-02-16 10:52:07 +0900

    Feat: 사다리 가로 겹치지 않게 로직 수정

--- src/Sadari.java
+++ src/Sadari.java
@@ -8,21 +8,21 @@ public class Sadari {
 
     //https://coozplz.me/2011/11/09/2%EC%B0%A8%EC%9B%90-%EB%B0%B0%EC%97%B4for%EB%AC%B8-%ED%95%98%EB%82%98%EB%A1%9C-%EC%B6%9C%EB%A0%A5/
     private void generateRandomSadari() {
-        for(int i = 0; i < sadari.length * sadari[0].length; i++) {
-            sadari[i/sadari[0].length][i% sadari[0].length] = getRandomBoolean();
+        for (int i = 0; i < sadari.length * sadari[0].length; i++) {
+            sadari[i / sadari[0].length][i % sadari[0].length] = getRandomBoolean();
+            i += sadari[i / sadari[0].length][i % sadari[0].length] == true ? 1 : 0 ;
         }
     }
 
-        private boolean getRandomBoolean () {
-            return Math.random() > 0.5 ? false : true;
-        }
-
-
-        public boolean[][] getSadari () {
-            return sadari;
-        }
-
-
+    private boolean getRandomBoolean () {
+        return Math.random() > 0.5 ? false : true;
     }
 
 
+
+    public boolean[][] getSadari () {
+        return sadari;
+    }
+
+
+}
