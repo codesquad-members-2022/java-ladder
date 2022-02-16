d2ce2f5e8329646224bd64e70d7781a63b93a36c
mybloom <baejungeun11@gmail.com>
2022-02-16 11:22:01 +0900

    REFACTOR: 입력 데이터 배열로 받았던 부분 Ladder 객체로 변경

---
+++ src/Ladder.java
@@ -0,0 +1,18 @@
+public class Ladder {
+
+  private int numOfPeople;
+  private int height;
+
+  public Ladder(int numOfPeople, int height) {
+    this.numOfPeople = numOfPeople;
+    this.height = height;
+  }
+
+  public int getNumOfPeople() {
+    return numOfPeople;
+  }
+
+  public int getHeight() {
+    return height;
+  }
+}
