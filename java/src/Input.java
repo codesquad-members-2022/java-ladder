ab84ed6a14f063a9057dd41ad8219e2eb2ff41b3
hwicode <khc3796@naver.com>
2022-02-16 18:32:12 +0900

    Input 클래스에 예외처리를 위한 기능 추가
    입력값을 int로 변환하지 못할 시 예외처리하는 기능 추가
    입력값중에서 하나라도 오류인 경우 get메소드 사용시 0으로 반환하도록 수정

--- java/src/Input.java
+++ java/src/Input.java
@@ -2,23 +2,56 @@ import java.util.Scanner;
 
 public class Input {
     private Scanner scanner;
+    private int people;
+    private int hight;
 
     public Input() {
-        Scanner scan = new Scanner(System.in);
-        scanner = scan;
+        scanner = new Scanner(System.in);
+        start();
+    }
+
+    private void start() {
+        askNumberOfPeople();
+        askHight();
+    }
+
+    private int validateValue(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            System.out.println("값을 잘못입력하셨습니다.");
+            return -1;
+        }
+    }
+
+    private void askNumberOfPeople() {
+        System.out.println("참여할 사람은 몇 명인가요?");
+        String value = scanner.nextLine();
+        int parseInt = validateValue(value);
+        Number number = new Number(parseInt);
+        people = number.getNumber();
+    }
+
+    private void askHight() {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        String value = scanner.nextLine();
+        int parseInt = validateValue(value);
+        Number number = new Number(parseInt);
+        hight = number.getNumber();
+    }
+
+    private int checkZero(int value) {
+        if (people == 0 || hight == 0) {
+            return 0;
+        }
+        return value;
     }
 
     public int getPeople() {
-        System.out.println("참여할 사람은 몇 명인가요?");
-        int input = scanner.nextInt();
-        Number number = new Number(input);
-        return number.getNumber();
+        return checkZero(people);
     }
 
     public int getHight() {
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
-        int input = scanner.nextInt();
-        Number number = new Number(input);
-        return number.getNumber();
+        return checkZero(hight);
     }
 }
