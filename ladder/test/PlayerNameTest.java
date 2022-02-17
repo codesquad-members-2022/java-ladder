9f31f5421ce6ff13142a0680b3b10e2f4fffa77e
ikjo93 <auddlr100@naver.com>
2022-02-17 14:36:17 +0900

    PlayerNameTest 테스트 코드 파일 생성 : 사다리 결과를 출력할 때 최대 5 글자를 정확히 출력하는지 처리하는 로직에 대한 테스트
    JUnit5 assertThat을 활용하여 테스트를 진행하였습니다. 참고로 inputPlayerList 메서드의 경우 System.in으로 별도 사용자 입력을 요청하고 있기 때문에 System.setIn을 통해 별도로 입력을 주었습니다.

---
+++ ladder/test/PlayerNameTest.java
@@ -0,0 +1,34 @@
+package ladder.test;
+
+import ladder.view.UserInterface;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PlayerNameTest {
+    @DisplayName("플레이어 이름 최대 글자 수 테스트")
+    @Test
+    void inputPlayerList() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        // given
+        String input = "ikjo,honux,crong,jk";
+        InputStream is = new ByteArrayInputStream(input.getBytes());
+        System.setIn(is);
+        UserInterface ui = new UserInterface();
+        Method method = ui.getClass().getDeclaredMethod("inputPlayerList");
+
+        // when
+        ArrayList<String> arrayList = (ArrayList<String>) method.invoke(ui);
+
+        // then
+
+        boolean result = arrayList.stream().allMatch(v -> v.length() <= 5);
+        assertThat(result).isTrue();
+    }
+}
