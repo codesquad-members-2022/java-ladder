aceb0996a816123b54d42fa609253ff1222dbad8
Minseok-Choi <cms05041@gmail.com>
2022-02-17 15:54:43 +0900

    refactor : Board 생성자 및 generateFrame() 메서드 수정
    void에서 List<Floor>로 반환해서 생성자에 frame을 만들도록 구현

--- src/main/java/domain/Board.java
+++ src/main/java/domain/Board.java
@@ -6,19 +6,16 @@ import java.util.List;
 
 public class Board {
 
-    private static final String VERTICAL = "|";
     private static final String EMPTY_PART = "    ";
 
     private final int players;
     private final int height;
-    private final List<List<String>> frame;
-    private final Line line;
+    private final List<Floor> frame;
 
     public Board(int players, int height) {
         this.players = Validation.checkPlayers(players);
         this.height = Validation.checkHeight(height);
-        frame = new ArrayList<>();
-        line = new Line();
+        frame = generateFrame();
     }
 
     public int countPlayers() {
@@ -29,49 +26,19 @@ public class Board {
         return height;
     }
 
-    public void generateFrame() {
+    public List<Floor> generateFrame() {
+        List<Floor> frame = new ArrayList<>();
         for (int row = 0; row < height; row++) {
-            frame.add(draw(row));
+            frame.add(new Floor(players*2-1));
         }
+        return frame;
     }
 
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
-        frame.forEach(floor -> floorToString(stringBuilder, floor));
+        frame.forEach(floor -> stringBuilder.append(floor.toString()));
         return stringBuilder.toString();
     }
-
-    private List<String> draw(int row) {
-        List<String> floor = new ArrayList<>();
-        for (int col = 0; col < players * 2 - 1; col++) {
-            floor.add(drawLine(floor, col));
-        }
-        return floor;
-    }
-
-    private String drawLine(List<String> floor, int col) {
-        if (col % 2 == 0) {
-            return VERTICAL;
-        }
-        return line.generateRandomLine(floor, col);
-    }
-
-    private void floorToString(StringBuilder stringBuilder, List<String> floor) {
-        stringBuilder.append(EMPTY_PART);
-        for (String s : floor) {
-            stringBuilder.append(formatFrame(s));
-        }
-        stringBuilder.append('\n');
-    }
-
-    private String formatFrame(String part) {
-        if (part.equals(VERTICAL)) {
-            return part;
-        }
-        return String.format("%5s", part);
-    }
-
-
 }
 
