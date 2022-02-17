60dbe517563c21874124c896fc918cb62d7ea9fe
함석호 <kalejuice02@gmail.com>
2022-02-17 16:36:35 +0900

    feat : User 클래스를 생성 및 검증하는 로직 작성
    - 유저 이름이 1자 미만, 5자 초과일 경우 예외를 던지는 검증 로직 작성
    - toString 오버라이드
    - user목록을 List로 변환하는 메서드 작성
    - user의 이름이 5자 이하일 경우 패딩을 붙여주는 메서드 작성
    
    * 테스트를 위해서 user 목록을 readOnly 형태의 list로 반환하도록
    작성했는데 이것도 좋지 않은 방법인지 고민이 됩니다.

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,7 +1,66 @@
 package domain;
 
-public class User {
-    public User(String[] user) {
+import java.util.*;
 
+public class User {
+    private static final int USERNAME_MAXLENGTH = 5;
+    private static final int USERNAME_MINLENGTH = 1;
+    private List<String> users;
+
+    public User(String[] users) {
+        if(!validateUserName(users)){
+            throw new InputMismatchException("이름의 길이를 1자 이상, 5자 이하로 작성해주세요.");
+        }
+        this.users = getUserByList(users);
     }
+
+    public List<String> getUsers() {
+        return Collections.unmodifiableList(users);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        users.forEach(user -> sb.append(user));
+        return sb.toString();
+    }
+
+    private List<String> getUserByList(String[] users) {
+        List<String> list = new ArrayList<>();
+
+        Arrays.stream(users).forEach(user->{
+            list.add(addPadding(user));
+        });
+        return list;
+    }
+
+    private String addPadding(String user) {
+        if (user.length() == USERNAME_MAXLENGTH) {
+            return user;
+        }
+        StringBuilder sb = new StringBuilder();
+        int whiteSpace = USERNAME_MAXLENGTH - user.length();
+        int backSpace = whiteSpace / 2;
+        int frontSpace = whiteSpace - backSpace;
+
+        addWhite(frontSpace, sb);
+        sb.append(user);
+        addWhite(backSpace, sb);
+
+        return sb.toString();
+    }
+
+    private StringBuilder addWhite(int count, StringBuilder sb) {
+        for (int i = 0; i < count; i++) {
+            sb.append(" ");
+        }
+        return sb;
+    }
+
+
+    private boolean validateUserName(String[] users) {
+        return Arrays.stream(users).allMatch((s) -> (s.length() >= USERNAME_MINLENGTH) && (s.length() <= USERNAME_MAXLENGTH));
+    }
+
+
 }
