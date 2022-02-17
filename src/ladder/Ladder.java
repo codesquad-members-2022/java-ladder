bbb6984c175fed4979ef99e7609166675d396fc0
Jaehong Choe <qnalsfj@gmail.com>
2022-02-17 22:20:36 +0900

    Ladder 구성요소 및 생성로직 변경
    - ladderRows: 사다리 정보를 가지고 있는 String타입 이중 배열을 삭제하고, 사다리 한줄에 대한 정보를
    가지고 있는 LadderRow클래스의 인스턴스를 저장한다.
    - 여러 개의 LadderRow인스턴스를 생성할 것이므로 RAIL, STEP 등 사다리 요소를
    Ladder클래스에서 제거하고 별도 클래스로 분리하여 static으로 선언하고 재사용 가능하도록 함.

--- src/ladder/Ladder.java
+++ src/ladder/Ladder.java
@@ -6,22 +6,16 @@ import java.util.Random;
 
 public class Ladder {
 
-    private static final String RAIL = "|";
-    private static final String STEP = "-";
-    private static final String BLANK = " ";
-    private static final String NEW_LINE = "\n";
-    private static final Random RANDOM = new Random();
-    private String[][] ladder;
+    private List<LadderRow> ladderRows;
     private List<Player> players = new ArrayList<>();
 
     public Ladder(List<String> playerNames, int ladderHeight) {
         init(playerNames, ladderHeight);
-        this.ladder = new String[ladderHeight][players.size() * 2 - 1];
-        createLadder(players.size(), ladderHeight);
     }
 
     private void init(List<String> playerNames, int ladderHeight) {
         setPlayers(playerNames);
+        createLadder(players.size(), ladderHeight);
     }
 
     private void setPlayers(List<String> playerNames) {
@@ -30,29 +24,29 @@ public class Ladder {
         }
     }
 
-    private void createLadder(int peopleCount, int ladderHeight) {
+    private void createLadder(int playerCount, int ladderHeight) {
         for (int i = 0; i < ladderHeight; i++) {
-            insertSingleLine(i);
+            ladderRows.add(new LadderRow(playerCount));
         }
     }
 
-    private void insertSingleLine(int currentHeight) {
-        for (int i = 0; i < ladder[currentHeight].length; i++) {
-            insertLadderBar(currentHeight, i);
-        }
-    }
-
-    private void insertLadderBar(int currentHeight, int horizontalIndex) {
-        if (horizontalIndex % 2 == 0) {
-            ladder[currentHeight][horizontalIndex] = RAIL;
-            return;
-        }
-        if (RANDOM.nextBoolean()) {
-            ladder[currentHeight][horizontalIndex] = STEP;
-            return;
-        }
-        ladder[currentHeight][horizontalIndex] = BLANK;
-    }
+//    private void insertSingleLine(int currentHeight) {
+//        for (int i = 0; i < ladder[currentHeight].length; i++) {
+//            insertLadderBar(currentHeight, i);
+//        }
+//    }
+//
+//    private void insertLadderBar(int currentHeight, int horizontalIndex) {
+//        if (horizontalIndex % 2 == 0) {
+//            ladder[currentHeight][horizontalIndex] = RAIL;
+//            return;
+//        }
+//        if (RANDOM.nextBoolean()) {
+//            ladder[currentHeight][horizontalIndex] = STEP;
+//            return;
+//        }
+//        ladder[currentHeight][horizontalIndex] = BLANK;
+//    }
 
     @Override
     public String toString() {
---
+++ src/ladder/LadderMaterial.java
@@ -0,0 +1,13 @@
+package src.ladder;
+
+import java.util.Random;
+
+public class LadderMaterial {
+
+    public static final String RAIL = "|";
+    public static final String STEP = "-----";
+    public static final String BLANK = "     ";
+    public static final String NEW_LINE = "\n";
+    public static final Random RANDOM = new Random();
+
+}
