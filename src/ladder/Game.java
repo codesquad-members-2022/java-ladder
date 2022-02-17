a195f0aa18df60d8f515ecaa2122fc5c07daf94c
Jaehong Choe <qnalsfj@gmail.com>
2022-02-17 19:34:53 +0900

    Ladder 생성자에 입력되는 파라미터 변경
    - playerCount를 넘기는 대신 입력받은 플레이어 이름이 담긴 List를 전달. Ladder클래스에
    Player인스턴스를 저장할 List 인스턴스 변수를 생성하기 위함.

--- src/ladder/Game.java
+++ src/ladder/Game.java
@@ -1,5 +1,7 @@
 package src.ladder;
 
+import java.util.List;
+
 public class Game {
 
     private final InputView inputView = new InputView();
@@ -17,13 +19,9 @@ public class Game {
     }
 
     private Ladder createLadder() {
-        outputView.printInputPeopleCountGuide();
-        int peopleCount = inputView.inputNumber();
-
-        outputView.printInputLadderHeightInputGuide();
-        int ladderHeight = inputView.inputNumber();
-
-        return new Ladder(peopleCount, ladderHeight);
+        List<String> players = inputView.inputPlayers("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        int ladderHeight = inputView.inputNumber("최대 사다리 높이는 몇 개인가요?");
+        return new Ladder(players, ladderHeight);
     }
 
 }
