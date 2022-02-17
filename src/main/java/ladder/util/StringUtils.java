1cadde2b716a3dcc880282e57a5c34c54adb28d8
“kukim” <kukim.dev@gmail.com>
2022-02-17 15:25:19 +0900

    refactor: StringUtils.center() 메서드 수정
    - 가독성을 높이기 위해 메서드 추출
    - 중앙정렬 할 수 없다면 str이 정해진 length보다 크다면 ".." 붙여 리턴
    - 중앙정렬 할 수 있다면 패딩 채운 문자열 생성 후 리턴
    - 테스트 코드 추가
    - StringUtils 유틸 클래스, 객체 생성 방지 private 생성자 추가

--- src/main/java/ladder/util/StringUtils.java
+++ src/main/java/ladder/util/StringUtils.java
@@ -1,16 +1,28 @@
 package ladder.util;
 
 public class StringUtils {
+
+    private StringUtils() { throw new IllegalStateException("Utility class"); }
+
     public static String center(String str, int length, char padChar) {
         int strSize = str.length();
-        if (strSize > length)
+        if (isStrSizeGreaterThanLength(length, strSize))
             return str.substring(0, length - 2) + "..";
+
+        return createCenterString(str, length, padChar, strSize);
+    }
+
+    private static boolean isStrSizeGreaterThanLength(int length, int strSize) {
+        return strSize > length;
+    }
+
+    private static String createCenterString(String str, int length, char padChar, int strSize) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < (length - strSize) / 2; i++)
-            sb.append(padChar);
+        sb.append(String.valueOf(padChar).repeat((length - strSize) / 2));
         sb.append(str);
-        while (sb.length() < length)
-            sb.append(padChar);
+        sb.append(String.valueOf(padChar).repeat(length - sb.length()));
         return sb.toString();
     }
+
+
 }
--- src/test/java/StringUtilsTest.java
+++ src/test/java/StringUtilsTest.java
@@ -15,11 +15,19 @@ public class StringUtilsTest {
     }
 
     @Test
-    void 문자열_중앙정렬() {
+    void 문자열_중앙정렬_초과시_점점표시() {
         String name = "honux";
         String overName = "hoooooooooooooooooooonux";
 
+        assertThat(StringUtils.center("a", 5, ' ')).isEqualTo("  a  ");
+        assertThat(StringUtils.center("aa", 5, ' ')).isEqualTo(" aa  ");
+        assertThat(StringUtils.center("aaa", 5, ' ')).isEqualTo(" aaa ");
+        assertThat(StringUtils.center("aaaa", 5, ' ')).isEqualTo("aaaa ");
+        assertThat(StringUtils.center("aaaaa", 5, ' ')).isEqualTo("aaaaa");
+        assertThat(StringUtils.center("aaaaaa", 5, ' ')).isEqualTo("aaa..");
+        assertThat(StringUtils.center("aaaaaaa", 5, ' ')).isEqualTo("aaa..");
         assertThat(StringUtils.center(name, 10, ' ')).isEqualTo("  honux   ");
         assertThat(StringUtils.center(overName, 10, ' ')).isEqualTo("hooooooo..");
+
     }
 }
