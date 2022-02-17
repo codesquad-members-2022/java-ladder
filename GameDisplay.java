6291915fa2c3091ea9beca6a4edb31a0e0729a3d
Hoyoung Jung <honux77@gmail.com>
2022-02-17 10:57:19 +0900

    Revert "GameDisplay 클래스 수정 : 개선된 사다리 모양 출력을 위한 메서드 구현"
    This reverts commit 9a3044f8c94bf74ed01ec0cb6a2d9a2486452d1e.

--- GameDisplay.java
+++ GameDisplay.java
@@ -1,48 +1,23 @@
-package step3;
+package step2;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GameDisplay {
 
-    private static final int paddingValue = 5;
-
-    public static void guideInputPlayerName() {
-        System.out.println("참여할 사람의 이름을 입력하세요.");
+    public static void showGuideToPlayerCnt() {
+        System.out.println("참여할 사람은 몇 명인가요?");
     }
 
-    public static void showError() {
-        System.out.println("잘못된 형식의 데이터를 입력하셨습니다..");
-    }
-
-    public static void guideInputLadderHeight() {
+    public static void showGuideToLadderHeight() {
         System.out.println("최대 사다리 높이는 몇개 인가요?");
     }
 
-    public static void showLadderInfo(ArrayList<String> players, ArrayList<ArrayList<String>> ladderInfo) {
-        System.out.printf("%n  ");
-        for (String player : players) {
-            System.out.printf(getPaddingString(player) + " ");
-        }
+    public static void showLadderInfo(String[][] ladderInfo) {
         System.out.println();
-        for (ArrayList<String> item : ladderInfo) {
-            System.out.print("    ");
-            item.forEach(System.out::print);
+        for (String[] item : ladderInfo) {
+            Arrays.stream(item).forEach(System.out::print);
             System.out.println();
         }
-    }
-
-    private static String getPaddingString(String player) {
-        if (paddingValue <= player.length()) return player;
-        // Middle Padding
-        StringBuilder sb = new StringBuilder(paddingValue);
-        for (int i = 0; i < (paddingValue - player.length()) / 2; i++) {
-            sb.append(" ");
-        }
-        sb.append(player);
-        while (sb.length() < paddingValue) {
-            sb.append(" ");
-        }
-        return sb.toString();
     }
 
 }
