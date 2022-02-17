50ccbceeb29bd1b2dfb6a72f12ef88ee5c8af9cb
leekm0310 <leekm310@gmail.com>
2022-02-17 23:50:12 +0900

    step3 - Run 클래스 수정
    1. 기존 inputView를 줄여서 i라고 명명해두었는데 inputView로 수정함.
    2. inputView에서 만든 플레이어 리스트를 run 메소드 안에서 가질 수 있도록 함.

--- src/sadari/Run.java
+++ src/sadari/Run.java
@@ -1,13 +1,17 @@
 package sadari;
 
+import java.util.List;
+
 public class Run {
-    public void run(){
-        InputView i = new InputView();
-        i.inputPlayers();
-        int people = i.numberOfPlayers();
-        int height = i.inputHeight();
-        i.printNames();
-        Ladder l = new Ladder(people,height);
-        i.close();
+    public void run() {
+        InputView inputView = new InputView();
+
+        List<String> playerNames = inputView.inputPlayers();
+        int people = inputView.numberOfPlayers();
+        int height = inputView.inputHeight();
+
+        inputView.printNames();
+        Ladder ladder = new Ladder(people, height);
+        inputView.close();
     }
 }
