d7f243b470d09da183477df24a484d778c916f5f
Kyle Choi <tmdfl1027@gmail.com>
2022-02-18 00:08:34 +0900

    [step3] Print 클래스 내 게임 출력 메서드 세분화
    - Print.printSadari() -> Print.printGame()으로 변경.
    - printGame() 메서드에서 플레이어 리스트를 출력하는 printPlayerList(), 사다리를
    출력하는 printSadari()를 호출.

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -1,9 +1,6 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import static com.company.Input.getNamesList;
 
 public class Controller {
 
@@ -12,7 +9,7 @@ public class Controller {
         System.out.println(namesList.toString());
         int height = Input.getSadariHeight();
         Sadari sadari = new Sadari(namesList, height);
-        Print.printSadari(sadari);
+        Print.printGame(sadari);
     }
 
 }
--- src/com/company/Print.java
+++ src/com/company/Print.java
@@ -1,34 +1,40 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Print {
 
-    public static void printSadari(Sadari sadari) {
+    public static void printGame(Sadari sadari) {
         ArrayList<String> namesList = sadari.getNamesList();
-        for (String name : namesList) {
-            StringBuilder sb = new StringBuilder();
-            final int SPACE_PER_NAME = 5;
-            int leftPadding = switch (name.length()) {
-                case 4, 3 -> 1;
-                case 2, 1 -> 2;
-                default -> 0;
-            };
-            for (int i = 0; i < leftPadding; i++) {
-                sb.append(" ");
-            }
-            sb.append(name);
-            for (int i = 0; i < SPACE_PER_NAME - leftPadding - name.length(); i++) {
-                sb.append(" ");
-            }
-            System.out.print(sb.toString());
-            System.out.print(" ");
-        }
-        System.out.println();
+        printPlayerList(namesList);
 
         ArrayList<ArrayList<String>> sadariArray = sadari.getSadariList();
+        printSadari(sadariArray);
 
+    }
+
+    public static void printPlayerList(ArrayList<String> namesList) {
+        for (String name : namesList) {
+            System.out.print(addPaddingToName(name));
+        }
+        System.out.println();
+    }
+
+    public static String addPaddingToName(String name) {
+        StringBuilder sb = new StringBuilder();
+        final int SPACE_PER_NAME = 5;
+        int leftPadding = switch (name.length()) {
+            case 4, 3 -> 1;
+            case 2, 1 -> 2;
+            default -> 0;
+        };
+        sb.append(" ".repeat(leftPadding));
+        sb.append(name);
+        sb.append(" ".repeat(SPACE_PER_NAME - leftPadding - name.length() + 1));
+        return sb.toString();
+    }
+
+    public static void printSadari(ArrayList<ArrayList<String>> sadariArray) {
         for (ArrayList<String> row : sadariArray) {
             String rowString = interpretRow(row);
             System.out.println(rowString);
