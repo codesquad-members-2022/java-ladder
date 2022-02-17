bb2bd061ebb0a99b393dc7f6a72f1f155dc70f70
donggi-lee-bit <devdonggilee@gmail.com>
2022-02-18 02:32:34 +0900

    feature : 한 라인의 좌표 값을 가지는 객체를 추가하고있었다
    - Line 객체를 만들어서 하나의 라인 좌표값을 가지고 있게하려고 했다.
    - 아직 어떻게 구현해야할지 미비함

---
+++ src/main/java/LadderGame/Line.java
@@ -0,0 +1,17 @@
+package LadderGame;
+
+import java.util.ArrayList;
+
+public class Line {
+
+    private ArrayList<Boolean> points = new ArrayList<>();
+    private int countOfPlayer;
+
+    public Line(int countOfPlayer) {
+        this.countOfPlayer = countOfPlayer;
+
+
+    }
+
+
+}
