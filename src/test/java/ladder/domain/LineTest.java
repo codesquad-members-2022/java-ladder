a4cf7315695bfc1c1d90279fcdfd37a4303b5ca3
“kukim” <kukim.dev@gmail.com>
2022-02-17 16:48:07 +0900

    test: LineTest를 BDD 형태로 변경
    - 기존 단순 단위 테스트를 행동 기반으로 변경
    - 테스트의 계층화 설정

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -1,80 +1,106 @@
 package ladder.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
+@DisplayName("Line 클래스")
 class LineTest {
 
-    @Test
-    void 라인_3명_초기값_사다리없음() {
-        Line line = Line.createLineWithPlayerCount(3);
+    @Nested
+    @DisplayName("createLineWithPlayerCount 메소드는")
+    class Describe_createLineWithPlayerCount {
 
-        assertThat(line.isLadder(0)).isFalse();
-        assertThat(line.isLadder(1)).isFalse();
+        @Nested
+        @DisplayName("만약 음수값이 주어진다면")
+        class Context_with_negative {
+            @Test
+            @DisplayName("IllegalArgumentException 예외를 던진다.")
+            void it_throws_a_exception() {
+                assertThatThrownBy(() -> Line.createLineWithPlayerCount(-1))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+        }
+
+        @Nested
+        @DisplayName("만약 양수 n이 주어진다면")
+        class Context_with_positive {
+            @Test
+            @DisplayName("주어진 n개의 point를 갖고, 사다리가 비어있는 Line 객체를 리턴한다.")
+            void it_returns_a_vaild_line() {
+                Line result = Line.createLineWithPlayerCount(3);
+
+                assertThat(result.isLadder(0)).isFalse();
+                assertThat(result.isLadder(1)).isFalse();
+            }
+        }
     }
 
-    @Test()
-    void 라인_3명_사다리_1개() {
-        Line line = Line.createLineWithPlayerCount(3);
+    @Nested
+    @DisplayName("drawLadder 메소드는")
+    class Describe_isLadder {
 
-        line.drawLadder(0);
+        @Nested
+        @DisplayName("먄약 Line(3) 객체에 첫 번째 위치에 1개의 사다리를 그린다면 ")
+        class Context_with_one_ladder {
+            @Test
+            @DisplayName("Line의 첫 번째 위치에만 사다리를 그린다.")
+            void it_draws_a_ladder() {
+                Line line = Line.createLineWithPlayerCount(3);
+                line.drawLadder(0);
 
-        assertThat(line.isLadder(0)).isTrue();
-    }
+                assertThat(line.isLadder(0)).isTrue();
+                assertThat(line.isLadder(1)).isFalse();
+            }
+        }
 
-    @Test()
-    void 라인_3명_연속된_사다리_2개_불가능() {
-        Line line = Line.createLineWithPlayerCount(3);
+        @Nested
+        @DisplayName("먄약 Line(3) 객체에 두 개의 사다리를 그린다면")
+        class Context_with_two_ladder {
+            @Test
+            @DisplayName("연속되게 그릴 수 없기 때문에 처음 그린 위치에만 사다리를 그린다.")
+            void it_draws_a_ladder() {
+                Line line = Line.createLineWithPlayerCount(3);
+                line.drawLadder(0);
+                line.drawLadder(1);
 
-        line.drawLadder(0);
-        line.drawLadder(1);
+                assertThat(line.isLadder(0)).isTrue();
+                assertThat(line.isLadder(1)).isFalse();
+            }
+        }
 
-        assertThat(line.isLadder(0)).isTrue();
-        assertThat(line.isLadder(1)).isFalse();
-    }
+        @Nested
+        @DisplayName("만약 그릴 수 없는 곳에 사다리를 그린다면")
+        class Context_with_draw_ladder_in_error_pos {
+            @Test
+            @DisplayName("IllegalArgumentException 예외를 던진다.")
+            void it_throws_a_exception() {
+                Line line = Line.createLineWithPlayerCount(3);
 
-    @Test()
-    void 라인_5명_사다리_2개() {
-        Line line = Line.createLineWithPlayerCount(5);
+                assertThatThrownBy(() -> line.drawLadder(-1))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+        }
 
-        line.drawLadder(0);
-        line.drawLadder(0);
-        line.drawLadder(2);
+        @Nested
+        @DisplayName("먄약 Line(5) 객체에 5개의 사다리를 그린다면")
+        class Context_with_fully_draw_ladder {
+            @Test
+            @DisplayName("연속되게 그릴 수 없기 때문에 처음, 중간만 그린다.")
+            void it_draws_a_ladder() {
+                Line line = Line.createLineWithPlayerCount(5);
+                line.drawLadder(0);
+                line.drawLadder(1);
+                line.drawLadder(2);
+                line.drawLadder(3);
 
-        assertThat(line.isLadder(0)).isTrue();
-        assertThat(line.isLadder(1)).isFalse();
-        assertThat(line.isLadder(2)).isTrue();
-        assertThat(line.isLadder(3)).isFalse();
-    }
-
-    @Test()
-    void 라인_5명_사다리_4개() {
-        Line line = Line.createLineWithPlayerCount(5);
-
-        line.drawLadder(0);
-        line.drawLadder(1);
-        line.drawLadder(2);
-        line.drawLadder(3);
-
-        assertThat(line.isLadder(0)).isTrue();
-        assertThat(line.isLadder(1)).isFalse();
-        assertThat(line.isLadder(2)).isTrue();
-        assertThat(line.isLadder(3)).isFalse();
-    }
-
-    @Test()
-    void 라인_7명_사다리_2개() {
-        Line line = Line.createLineWithPlayerCount(7);
-
-        line.drawLadder(3);
-        line.drawLadder(0);
-
-        assertThat(line.isLadder(0)).isTrue();
-        assertThat(line.isLadder(1)).isFalse();
-        assertThat(line.isLadder(2)).isFalse();
-        assertThat(line.isLadder(3)).isTrue();
-        assertThat(line.isLadder(4)).isFalse();
-        assertThat(line.isLadder(5)).isFalse();
+                assertThat(line.isLadder(0)).isTrue();
+                assertThat(line.isLadder(1)).isFalse();
+                assertThat(line.isLadder(2)).isTrue();
+                assertThat(line.isLadder(3)).isFalse();
+            }
+        }
     }
 }
