f66bfbb07d02e6122ea91d069f772cf3a7052da4
hsjang <hsjang0405@gmail.com>
2022-02-18 00:07:27 +0900

    🎨  LadderGame class가 너무 커서 Ladder class 추출

---
+++ src/ladder/domain/Ladder.java
@@ -0,0 +1,73 @@
+package ladder.domain;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Ladder {
+
+	private static final String BLANK = " ";
+
+
+	private final List<LadderLine> ladder;
+	private final Map<User, String> gameResult;
+	private final List<String> userResultInput;
+	private final List<User> users;
+	private final int userNumber;
+	private final int ladderHeight;
+
+	public Ladder(int userNumber, int ladderHeight, List<User> users,
+		List<String> userResultInput) {
+		this.ladder = new ArrayList<>();
+		this.gameResult = new HashMap<>();
+		this.users = users;
+		this.userResultInput = userResultInput;
+		this.userNumber = userNumber;
+		this.ladderHeight = ladderHeight;
+	}
+
+	public void makeLadderAndCheckResult() {
+		for (int i = 0; i < ladderHeight; i++) {
+			ladder.add(new LadderLine(userNumber));
+		}
+		linkUserAndResult();
+	}
+	public void linkUserAndResult() {
+		for (int i = 0; i < userNumber; i++) {
+			gameResult.put(users.get(i), checkResult(i));
+		}
+	}
+	private String checkResult(int idx) {
+		int line = idx * 6 + 1;
+		for (int i = 0; i < ladderHeight; i++) {
+			line = userPath(line, i);
+		}
+		return userResultInput.get((line - 1) / 6);
+	}
+
+	private int userPath(int line, int i) {
+		if (isExistLeftStep(line, i)) {
+			line -= 6;
+		} else if (isExistRightStep(line, i)) {
+			line += 6;
+		}
+		return line;
+	}
+
+	private boolean isExistRightStep(int line, int i) {
+		return getLadderLineWithPadding(i).substring(line + 1, line + 2).contains("-");
+	}
+
+	private boolean isExistLeftStep(int line, int i) {
+		return getLadderLineWithPadding(i).substring(line - 1, line).contains("-");
+	}
+
+	private String getLadderLineWithPadding(int i) {
+		return BLANK + ladder.get(i).toString() + BLANK;
+	}
+
+	public List<LadderLine> getLadder() {
+		return ladder;
+	}
+}
--- src/ladder/domain/LadderGame.java
+++ src/ladder/domain/LadderGame.java
@@ -1,38 +1,28 @@
 package ladder.domain;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import ladder.view.Input;
 import ladder.view.PrintLadder;
 
 public class LadderGame {
 
-	private static final String BLANK = " ";
-
-	private List<LadderLine> ladder;
 	private List<User> users;
 	private List<String> userResultInput;
-	private Map<User, String> gameResult;
+	private Ladder ladder;
 	private int userNumber;
-	private int ladderHeight;
 
 	public void start() {
 		init();
-		PrintLadder.printLadder(users, ladder, userResultInput);
+		PrintLadder.printLadder(users, ladder.getLadder(), userResultInput);
 		//todo 결과를 보고 싶은 사람 & 실행 결과 (gameResult 매개변수)
 		// while(ture) 로 계속 돌려야할듯
 		Input.close();
 	}
 	private void init() {
-		ladder = new ArrayList<>();
-		gameResult = new HashMap<>();
 		getUserName();
 		getUserResult();
-		getLadderHeight();
-		makeLadder();
-		linkUserAndResult();
+		this.ladder = new Ladder(userNumber, getLadderHeight(), users, userResultInput);
+		ladder.makeLadderAndCheckResult();
 	}
 	private void getUserName() {
 		users = Input.getUserName();
@@ -44,46 +34,8 @@ public class LadderGame {
 			userResultInput = Input.getUserResult();
 		}
 	}
-	private void getLadderHeight() {
-		ladderHeight = Input.getLadderHeight("최대 사다리 높이는 몇 개인가요?");
-	}
-	private void makeLadder() {
-		for (int i = 0; i < ladderHeight; i++) {
-			ladder.add(new LadderLine(userNumber));
-		}
-	}
-	private void linkUserAndResult() {
-		for (int i = 0; i < userNumber; i++) {
-			gameResult.put(users.get(i), checkResult(i));
-		}
-	}
-	private String checkResult(int idx) {
-		int line = idx * 6 + 1;
-		for (int i = 0; i < ladderHeight; i++) {
-			line = userPath(line, i);
-		}
-		return userResultInput.get((line - 1) / 6);
-	}
-
-	private int userPath(int line, int i) {
-		if (isExistLeftStep(line, i)) {
-			line -= 6;
-		} else if (isExistRightStep(line, i)) {
-			line += 6;
-		}
-		return line;
-	}
-
-	private boolean isExistRightStep(int line, int i) {
-		return getLadderLineWithPadding(i).substring(line + 1, line + 2).contains("-");
-	}
-
-	private boolean isExistLeftStep(int line, int i) {
-		return getLadderLineWithPadding(i).substring(line - 1, line).contains("-");
-	}
-
-	private String getLadderLineWithPadding(int i) {
-		return BLANK + ladder.get(i).toString() + BLANK;
+	private int getLadderHeight() {
+		return Input.getLadderHeight("최대 사다리 높이는 몇 개인가요?");
 	}
 
 }
