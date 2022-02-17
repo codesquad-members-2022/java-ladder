9d2998d8d3ab04a22c325705890960a81b347a9e
Minseok-Choi <cms05041@gmail.com>
2022-02-17 21:02:33 +0900

    refactor : 불필요한 메서드를 제거하고 로직을 단순화
    showNames()를 삭제하고, Main LadderGame 실행 로직 단순화

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-
         LadderGame ladderGame = new LadderGame();
-        ladderGame.init();
         ladderGame.run();
-        ladderGame.showResult();
     }
 }
--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -5,11 +5,8 @@ import domain.Names;
 
 public class OutputView {
 
-    public static void showBoard(Ladder board) {
-        System.out.println(board.toString());
+    public static void showLadder(Ladder ladder) {
+        System.out.println(ladder.toString());
     }
 
-    public static void showNames(Names names) {
-        System.out.println(names.toString());
-    }
 }
