b3c584c6e94d3b93ea6a7de8c7d907c5ed7bc89e
nak253 <nwon4413@naver.com>
2022-02-17 16:38:24 +0900

    참가자의 이름과 사다리의 높이에 대한 질문에 해당하는 멤버 변수를 인스턴스 변수에서 클래스 변수로 변경

--- src/ladder/InputView.java
+++ src/ladder/InputView.java
@@ -6,8 +6,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private String participantsQuestion = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
-    private String heightQuestion = "사다리의 높이는 얼마인가요?";
+    private static final String participantsQuestion = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String heightQuestion = "사다리의 높이는 얼마인가요?";
     private Scanner scanner;
     private Validator validator;
 
