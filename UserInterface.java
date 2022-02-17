e3851198bac82aa52535e10b0d2f3d4d98f7ad87
Hoyoung Jung <honux77@gmail.com>
2022-02-17 10:57:19 +0900

    Revert "UserInterface 클래스 수정 : 사용자 입력 데이터(문자열) 검증 및 ArrayList 변환 메서드 구현"
    This reverts commit 0d2b5bfb0fe74bd806e9893e4e692ede99621db4.

--- UserInterface.java
+++ UserInterface.java
@@ -1,8 +1,6 @@
-package step3;
+package step2;
 
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class UserInterface {
 
@@ -12,36 +10,14 @@ public class UserInterface {
         sc = new Scanner(System.in);
     }
 
-    public ArrayList<String> inputPlayerList() {
-        GameDisplay.guideInputPlayerName();
-        String userInput = sc.nextLine();
-        if (checkInput(userInput)) return null;
-
-        return getPlayerList(userInput);
-    }
-
-    public int inputLadderHeight() {
-        GameDisplay.guideInputLadderHeight();
+    public int inputPlayerCnt() {
+        GameDisplay.showGuideToPlayerCnt();
         return Integer.parseInt(sc.nextLine());
     }
 
-    private boolean checkInput(String userInput) {
-        String inputPattern = "^([^,\s]{1,5})+(,([^,\s]{1,5}))*$";
-        if (!Pattern.matches(inputPattern, userInput)) {
-            GameDisplay.showError();
-            return true;
-        }
-        return false;
-    }
-
-    private ArrayList<String> getPlayerList(String input) {
-        ArrayList<String> arrayList = new ArrayList<>();
-        String[] players = input.split(",");
-        for (int i = 0; i < players.length; i++) {
-            arrayList.add(players[i]);
-        }
-
-        return arrayList;
+    public int inputLadderHeight() {
+        GameDisplay.showGuideToLadderHeight();
+        return Integer.parseInt(sc.nextLine());
     }
 
 }
