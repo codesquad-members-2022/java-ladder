cabe102c44fdcf0d70c8da42e2fb719b49a4a718
WooJin Park <ak2j38@gmail.com>
2022-02-17 21:55:04 +0900

    feature : 사다리의 각 위치 요소에 해당하는 Element 클래스 추가
    1. Element 클래스는 index와 value(세로선, 발판, 빈칸), 발판여부를 갖고 있습니다.
    
    end

---
+++ src/main/java/domain/Element.java
@@ -0,0 +1,36 @@
+package domain;
+
+public class Element {
+
+    private static final String LADDER_VERTICAL_LINE = "|";
+    private static final String LADDER_HORIZONTAL_LINE = "-----";
+    private static final String LADDER_BLANK = "     ";
+
+    private int index;
+    private String value;
+    private boolean hasStep;
+
+    private Element() {
+
+    }
+
+    public void setElement(int index, boolean hasPrevStep, boolean rand) {
+        this.index = index;
+        this.value = setValue(index, hasPrevStep, rand);
+        this.hasStep = false;
+        if (this.value.equals(LADDER_HORIZONTAL_LINE)) {
+            this.hasStep = true;
+        }
+    }
+
+    private String setValue(int index, boolean hasPrevStep, boolean rand) {
+
+        if (index % 2 == 0) {
+            return LADDER_VERTICAL_LINE;
+        }
+        if (index % 2 != 0 && !hasPrevStep && rand) {
+            return LADDER_HORIZONTAL_LINE;
+        }
+        return LADDER_BLANK;
+    }
+}
