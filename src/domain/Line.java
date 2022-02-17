1a522302cb72a66220d6570c589457fce91deed7
leezzangmin <ckdals12345678@gmail.com>
2022-02-17 16:40:46 +0900

    코드 가독성 향상을 위한 Line 클래스 추가
    한 라인의 좌표값을 가지는 객체

---
+++ src/domain/Line.java
@@ -0,0 +1,11 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class Line {
+    private ArrayList<Boolean> points = new ArrayList<>();
+
+    public Line (int countOfPerson){
+
+    }
+}
