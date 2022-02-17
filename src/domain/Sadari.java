1de696f9c3172b35b1a9ad2a11f0ea6050ecf6f5
leezzangmin <ckdals12345678@gmail.com>
2022-02-17 17:01:30 +0900

    Sadari 클래스 메소드에서 Line관련된 메소드 이동 및 수정, toString() 오버라이드
    기존 Sadari 클래스에 존재하는 한 줄씩 처리하는 메소드와 그에 필요한 메소드를 옮기고 자료형에 맞게 수정하였음
    toString 메소드를 오버라이드했음.

--- src/domain/Sadari.java
+++ src/domain/Sadari.java
@@ -1,49 +1,17 @@
 package domain;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Sadari {
-    private ArrayList<boolean[]> sadari;
+    private List<Line> sadari;
 
     public Sadari(int userNumber, int sadariNumber) {
-        //sadari = new boolean[sadariNumber][userNumber - 1];
-        sadari = new ArrayList<boolean[]>();
+        sadari = new ArrayList<Line>();
         for(int i=0;i<sadariNumber;i++){
-            sadari.add(new boolean[userNumber - 1]);
+            Line temp= new Line(userNumber -1);
+            System.out.println( temp.toString() );
+            sadari.add( temp );
         }
-        generateRandomSadari();
-    }
-
-    private void generateRandomSadari() {
-        for (int i = 0; i < sadari.size(); i++){
-            //sadari[i] = generateRandomLineOfSadari( sadari[i] );
-            sadari.set(i, generateRandomLineOfSadari( sadari.get(i) ));
-        }
-    }
-
-    private boolean[] generateRandomLineOfSadari(boolean[] line){
-        for(int i=0;i<line.length;i++){
-            line[i] = getRandomBoolean();
-            i+=isContinuousGaro(line[i]);
-        }
-        return line;
-    }
-
-    // 가로가 연속해서 존재하지 않게 i값을 조정하는 메소드.
-    private int isContinuousGaro(boolean i){
-        if(i==true){
-            return 1;
-        }
-        return 0;
-    }
-
-
-    private boolean getRandomBoolean () {
-        return Math.random() > 0.5 ? false : true;
-    }
-
-    public ArrayList<boolean[]> getSadari () {
-        return sadari;
     }
 
 }
