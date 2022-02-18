712e2ecb9ddb556ee86c70760b6b29c06fde6416
nak253 <nwon4413@naver.com>
2022-02-18 12:09:42 +0900

    참가자의 이름을 최대 5글자를 넘기지 않도록 하는 기능 구현
    - InputView클래스에 최대 글자수를 의미하는 클래스 변수 정의
    - InputView클래스에 참가자를 입력받는 메서드 parsingParicipants 메서드에 최대 글자수를 넘긴 참가자가 있다면 예외처리하도록 작성
    - Validator클래스를 작성하여 참가자 이름이 최대 글자수를 넘겼는지 확인하는 메서드 작성

--- src/ladder/InputView.java
+++ src/ladder/InputView.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+    private static final int MAX_NUM_CHARACTERS = 5;
     private static final String participantsQuestion = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String heightQuestion = "사다리의 높이는 얼마인가요?";
     private Scanner scanner;
@@ -29,6 +30,12 @@ public class InputView {
 
     private List<String> parsingParticipants() {
         String[] participantArr = scanner.nextLine().split(",");
+        try {
+            participantArr = validator.CheckMaximumNumberCharacters(participantArr, MAX_NUM_CHARACTERS);
+        } catch (IllegalArgumentException ie) {
+            System.out.println(ie.getMessage());
+            return parsingParticipants();
+        }
         List<String> participants = new ArrayList<>(Arrays.asList(participantArr));
         return participants;
     }
--- src/ladder/Validator.java
+++ src/ladder/Validator.java
@@ -1,2 +1,12 @@
-package ladder;public class Validator {
+package ladder;
+
+public class Validator {
+    public String[] CheckMaximumNumberCharacters(String[] participantArr, int maxNumCharacters) throws IllegalArgumentException {
+        for (String name : participantArr) {
+            if (maxNumCharacters < name.length()) {
+                throw new IllegalArgumentException("참가자 이름이 " + maxNumCharacters + "넘지 않도록 작성하세요.");
+            }
+        }
+        return participantArr;
+    }
 }
