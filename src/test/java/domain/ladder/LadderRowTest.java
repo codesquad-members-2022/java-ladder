affc8065a1fa2b5bc19b023e094af1b52c873e02
ttasjwi <ttasjwi920@gmail.com>
2022-02-18 14:34:47 +0900

    test : LadderRow - isVerticalLineDrawable 메서드 테스트 코드 추가
    - 짝수 Column에 대해서 이 메서드는 true를 반환해야함
    - 홀수 Column에 대해서 이 메서드는 false를 반환해야함

--- src/test/java/domain/ladder/LadderRowTest.java
+++ src/test/java/domain/ladder/LadderRowTest.java
@@ -24,4 +24,38 @@ class LadderRowTest {
                 .as("LadderRow의 가로 길이는, 2* 인원수 - 1 이어야한다.")
                 .isEqualTo(expectWidth);
     }
+
+    @Test
+    @DisplayName("짝수 Column ->  세로선을 그릴 수 있음이 보장되는지 테스트")
+    void is_VerticalLine_DrawableColumn_Test_O() {
+        //given
+        int numberOfUsers = 5;
+        int evenNumberColumn = 2;
+
+        //when
+        LadderRow ladderRow = new LadderRow(numberOfUsers);
+
+        //then
+        boolean actualResult = ladderRow.isVerticalLineDrawableColumn(evenNumberColumn);
+        assertThat(actualResult)
+                .as("짝수 Column에는 세로선을 그릴 수 있으므로 True가 반환되어야 함.")
+                .isTrue();
+    }
+
+    @Test
+    @DisplayName("홀수 Column ->  세로선을 그릴 수 없음이 보장되는지 테스트")
+    void is_VerticalLine_DrawableColumn_Test_X() {
+        //given
+        int numberOfUsers = 5;
+        int oddNumberColumn = 3;
+
+        //when
+        LadderRow ladderRow = new LadderRow(numberOfUsers);
+
+        //then
+        boolean actualResult = ladderRow.isVerticalLineDrawableColumn(oddNumberColumn);
+        assertThat(actualResult)
+                .as("홀수 Column에는 세로선을 그릴 수 없으므로 False가 반환되어야 함.")
+                .isFalse();
+    }
 }
