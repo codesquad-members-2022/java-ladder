8685bf561c45053598853606056986840019b8f4
jiwoo <zbqmgldjfh@gmail.com>
2022-02-18 13:53:35 +0900

    feat(UserDefineLadder): 사용자의 입력으로 높이, 너비를 받아 사다리를 생성하는 클래스를 만드시오
    - 변경전
    이전의 UserDefineLadder는 단지 Line 1줄에 대한 test만을 진행하였다.
    
    - 변경후
    변경 후, 여러 Lines에 대하여 test를 진행한다.

--- src/main/java/domain/UserDefineLadder.java
+++ src/main/java/domain/UserDefineLadder.java
@@ -2,21 +2,37 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class UserDefineLadder {
-    Line line;
+    private final List<Line> lines;
+    private final int height;
 
-    public UserDefineLadder(int numOfPerson) {
-        line = new Line(numOfPerson);
+    public UserDefineLadder(int height, int numOfPerson) {
+        this.height = height;
+        this.lines = new ArrayList<>();
+        makeLines(numOfPerson);
+    }
+
+    private void makeLines(int numOfPerson) {
+        IntStream.range(0, height).forEach(i -> lines.add(new Line(numOfPerson)));
     }
 
     public int run(int startLine) {
-        if(!line.isMarkedPosition(startLine)){ // 그대로 이동
+        for(int i = 0; i < lines.size(); i++){
+            startLine = switchLadderLine(startLine, i);
+            continue;
+        }
+        return startLine;
+    }
+
+    private int switchLadderLine(int startLine, int i) {
+        if(!lines.get(i).isMarkedPosition(startLine)){ // 그대로 이동
             return startLine;
         }
 
-        if(startLine-1 >= 0) { // 좌측 처리하기
-            if (line.isMarkedPosition(startLine - 1)) {
+        if(startLine -1 >= 0) { // 좌측 처리하기
+            if (lines.get(i).isMarkedPosition(startLine - 1)) {
                 return startLine - 1;
             }
         }
@@ -24,8 +40,8 @@ public class UserDefineLadder {
         return startLine + 1;
     }
 
-    public boolean makeLadderAt(int startPosition) {
-        line.makeLadderAt(startPosition);
+    public boolean makeLadderAt(int height, int startPosition) {
+        lines.get(height).makeLadderAt(startPosition); // 인접한 이웃에게만 연락했으니, 디미터원칙 위배 X??
         return true;
     }
 }
--- src/test/java/domain/LadderGameTest.java
+++ src/test/java/domain/LadderGameTest.java
@@ -7,9 +7,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LadderGameTest {
     @Test
-    public void test() {
+    public void 라인_없을경우_테스트() {
         // given
-        UserDefineLadder ladder = new UserDefineLadder(5);
+        UserDefineLadder ladder = new UserDefineLadder(1, 5);
 
         // when
         int result = ladder.run(0);
@@ -23,9 +23,9 @@ public class LadderGameTest {
     @Test
     public void 라인_있을때_왼쪽으로_이동_테스트() {
         // given
-        UserDefineLadder ladder = new UserDefineLadder(5);
+        UserDefineLadder ladder = new UserDefineLadder(1, 5);
         // 1 1 0 0 0
-        ladder.makeLadderAt(0);
+        ladder.makeLadderAt(0, 0);
 
         // when
         int result = ladder.run(1);
@@ -37,23 +37,23 @@ public class LadderGameTest {
     @Test
     public void 라인_있을때_맨처음지점에서_시작_테스트() {
         // given
-        UserDefineLadder ladder = new UserDefineLadder(5);
+        UserDefineLadder ladder = new UserDefineLadder(1, 5);
         // 1 1 0 0 0
-        ladder.makeLadderAt(0);
+        ladder.makeLadderAt(0, 0);
 
         // when
         int result = ladder.run(0);
 
         // then
-        assertThat(result).isEqualTo(0);
+        assertThat(result).isEqualTo(1);
     }
 
     @Test
     public void 라인_있을때_우측으로_이동_테스트() {
         // given
-        UserDefineLadder ladder = new UserDefineLadder(5);
+        UserDefineLadder ladder = new UserDefineLadder(1, 5);
         // 0 1 1 0 0
-        ladder.makeLadderAt(1);
+        ladder.makeLadderAt(0, 1);
 
         // when
         int result = ladder.run(1);
@@ -65,14 +65,40 @@ public class LadderGameTest {
     @Test
     public void 라인_있을때_맨끝지점에서_시작_테스트() {
         // given
-        UserDefineLadder ladder = new UserDefineLadder(5);
+        UserDefineLadder ladder = new UserDefineLadder(1, 5);
         // 0 0 0 1 1
-        ladder.makeLadderAt(3);
+        ladder.makeLadderAt(0, 3);
 
         // when
         int result = ladder.run(4);
 
         // then
         assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void 여러_라인_있을때_테스트() {
+        // given
+        UserDefineLadder ladder = new UserDefineLadder(3, 5);
+        // 1 1 0 0 0
+        // 0 1 1 0 0
+        // 0 0 1 1 0
+        ladder.makeLadderAt(0, 0);
+        ladder.makeLadderAt(1, 1);
+        ladder.makeLadderAt(2, 2);
+
+        // when
+        int result1 = ladder.run(0);
+        int result2 = ladder.run(1);
+        int result3 = ladder.run(2);
+        int result4 = ladder.run(3);
+        int result5 = ladder.run(4);
+
+        // then
+        assertThat(result1).isEqualTo(3);
+        assertThat(result2).isEqualTo(0);
+        assertThat(result3).isEqualTo(1);
+        assertThat(result4).isEqualTo(2);
+        assertThat(result5).isEqualTo(4);
     }
 }
