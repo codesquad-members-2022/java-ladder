65e88754c37845b88c2066cd14fb1208812b463a
who-hoo <who.ho3ov@gmail.com>
2022-02-18 15:02:12 +0900

    :white_check_mark: 사다리 생성 테스트 코드 작성
    - LadderRow에서 선을 그릴 수 있는지 여부가 판단하는 로직이 정상적으로 작동하였다면, 생성된 Ladder는 겹치는 라인이 없이 생성될 것이므로 Ladder가 겹치는 라인 없이 정상적으로 생성되었는지 검증

--- src/test/java/domain/ladder/LadderTest.java
+++ src/test/java/domain/ladder/LadderTest.java
@@ -1,15 +1,17 @@
 package domain.ladder;
 
-import domain.ladder.Ladder;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LadderTest {
 
     @Test
-    @DisplayName("사다리 생성하여 찍어보기")
+    @DisplayName("정상적으로 동작하는(라인이 겹치지 않는) 사다리가 생성되는지 테스트")
     void createLadder() {
-        Ladder ladder = new Ladder(3, 5);
+        Ladder ladder = new Ladder(11, 10);
+        String overlappedLine = "|-----|-----|";
         System.out.println(ladder);
+        Assertions.assertThat(ladder.toString()).doesNotContain(overlappedLine);
     }
 }
