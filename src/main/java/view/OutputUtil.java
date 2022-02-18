05d2a1c7617092480d60c28ed571b20d62be8f65
함석호 <kalejuice02@gmail.com>
2022-02-18 14:36:19 +0900

    refactor : OutputUtil의 출력 메서드 변경
    - 동일하게 보여야하는 데이터는 View에서 가지고 있도록 변경

--- src/main/java/view/OutputUtil.java
+++ src/main/java/view/OutputUtil.java
@@ -1,7 +1,35 @@
 package view;
 
+import domain.Ladder;
+import domain.Line;
+import domain.User;
+
+import java.util.List;
+
 public class OutputUtil {
-    public static void printLadder(String ladder) {
-        System.out.println(ladder);
+    private static final String USER_LINE = "|";
+    private static final String CONNECTION_LINE = "-----";
+    private static final String EMPTY_LINE = "     ";
+
+    public static void printUsers(List<User> users) {
+        users.forEach(user -> System.out.print(user + " "));
+    }
+
+
+    public static void printLadder(Ladder ladder) {
+            //각 줄별로 출력
+    }
+
+    public static void printResults() {
+        //Result를 담은 List를 매개변수로 받는다
+        //출력
+    }
+
+    private static void printLadderLine(Line line) {
+        //반복문으로 line을 돌면서
+        //-1 = 빈칸
+        //0 = 유저
+        //1 = 연결선
+        //으로 만들어서 반환
     }
 }
