19f677211bf0071a5701f47b6c23fee9130ad6ce
Minseok-Choi <cms05041@gmail.com>
2022-02-18 15:30:18 +0900

    feat : InputView 실행 결과 값 입력 받는 메서드 구현

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -12,11 +12,16 @@ public class InputView {
     private static final String PLAYER_NAMES_COMMAND = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String PLAYER_NAMES_RESTRICTIONS = "[플레이어의 이름은 5자 이내(알파벳,숫자만가능), 가능 플레이어의 수는 2명이상 10명이하입니다.]";
     private static final String LADDER_HEIGHT_COMMAND = "최대 사다리 높이는 몇 개인가요?";
+    private static final String RESULT_VALUES_COMMAND = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final Scanner scanner = new Scanner(System.in);
 
 
     public static LadderGame askSettingLadder() {
-        return new LadderGame(askPlayers(),askLadderHeight());
+        return new LadderGame(askPlayers(),askLadderHeight(),askResultValues());
+    }
+
+    public static void close() {
+        scanner.close();
     }
 
     private static Players askPlayers() {
@@ -37,9 +42,11 @@ public class InputView {
         return Integer.parseInt(scanner.nextLine());
     }
 
-    public static void close() {
-        scanner.close();
+    private static String[] askResultValues() {
+        System.out.println(RESULT_VALUES_COMMAND);
+        return scanner.nextLine().replaceAll(" ","").split(",");
     }
 
 
 }
+
