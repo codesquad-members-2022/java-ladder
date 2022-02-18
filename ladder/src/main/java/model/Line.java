db8e7f8ad3dc7da407fa936cb2656eb6bb76ecaf
geombong <shoy1415@naver.com>
2022-02-18 20:32:27 +0900

    chore Line 클래스 반환타입, 접근제어자 변경

--- ladder/src/main/java/model/Line.java
+++ ladder/src/main/java/model/Line.java
@@ -5,30 +5,27 @@ import java.util.Random;
 
 public class Line {
 
+    private final int lineLength;
     private final Random random = new Random(System.currentTimeMillis());
     private final List<String> ladderUnits = List.of(LadderUnit.SPACE.getLadderUnit(), LadderUnit.STEP.getLadderUnit(), LadderUnit.STEP.getLadderUnit());
-    StringBuilder sb = new StringBuilder();
 
     public Line(int userCount) {
-        int lineLength = userCount + (userCount - 1);
-        createLine(lineLength);
+        this.lineLength = userCount + (userCount - 1);
     }
 
-    private void createLine(int lineLength) {
-        for (int lineNumber = 0; lineNumber < lineLength; lineNumber++) {
+    public String createLine() {
+        StringBuilder sb = new StringBuilder();
+        for (int lineNumber = 0; lineNumber < this.lineLength; lineNumber++) {
             if (isEven(lineNumber)) {
                 sb.append(LadderUnit.RAIL.getLadderUnit());
                 continue;
             }
             sb.append(LadderUnit.STEP.getLadderUnit());
         }
+        return sb.toString();
     }
 
     private boolean isEven(int lineNumber) {
         return lineNumber % 2 == 0;
-    }
-
-    public String printLine() {
-        return sb.toString();
     }
 }
