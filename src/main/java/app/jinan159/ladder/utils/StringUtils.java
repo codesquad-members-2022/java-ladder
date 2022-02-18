f87323242758ea8177bd2de24870e735fab27d5f
jinan159 <wlsdhks0423@naver.com>
2022-02-18 22:38:20 +0900

    StringUtils 의 repeatCharacter 메소드 count 검증 로직 추가

--- src/main/java/app/jinan159/ladder/utils/StringUtils.java
+++ src/main/java/app/jinan159/ladder/utils/StringUtils.java
@@ -5,6 +5,7 @@ import java.util.stream.IntStream;
 public class StringUtils {
 
     private final static String ALERT_SMALL_TOTAL_SIZE = "문자열의 길이보다 작은 크기로 패딩 문자열을 생성할 수 없습니다.";
+    private final static String ALERT_POSITIVE_REQUIRED = "0보다 큰 숫자를 입력하셔야 합니다.";
 
     public static String padLeftRight(String source, int totalSize) {
         if (source == null ||  totalSize < source.length()) throw new IllegalArgumentException(ALERT_SMALL_TOTAL_SIZE);
@@ -14,6 +15,8 @@ public class StringUtils {
     }
 
     public static String repeatCharacter(char chr, int count) {
+        if (count < 1) throw new IllegalArgumentException(ALERT_POSITIVE_REQUIRED);
+
         StringBuilder sb = new StringBuilder();
         IntStream.range(0,count)
                 .forEach(i -> sb.append(chr));
