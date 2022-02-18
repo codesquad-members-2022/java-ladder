4808d461ffd0f86bbbd88c4d9b3198b9185b93a9
jiwoo <zbqmgldjfh@gmail.com>
2022-02-18 19:16:55 +0900

    refactor(InputView): LadderFactory를 사용하도록 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,6 +7,7 @@ public class InputView {
     public static final String INPUT_PERSON_GUIDANCE_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     public static final String INPUT_LADDER_SIZE_GUIDANCE_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     public static final String INPUT_NAME_LENGTH_ERROR_MESSAGE = "이름은 최대 5글자 까지만 가능합니다.";
+    public static final String INPUT_LADDER_RESULT_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static Scanner scanner = new Scanner(System.in);
 
     public static String[] requestPerson() {
@@ -20,6 +21,12 @@ public class InputView {
 
     private static boolean invalidNameLength(String[] names) {
         return Arrays.stream(names).anyMatch(i -> i.length() > 5);
+    }
+
+    public static String[] requestResult(){
+        System.out.println(INPUT_LADDER_RESULT_MESSAGE);
+        String[] results = scanner.nextLine().split(",");
+        return results;
     }
 
     public static int requestHeight() {
