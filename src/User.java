1fc0f0fbe861ff2d039f0fa91c02ca764917efa2
tokaji <fpzk5656@gmail.com>
2022-02-18 12:17:12 +0900

    - UserManager 객체를 추가하였습니다. 사다리 출력 때 표시할 유저 이름들을 종합해서 다듬는 역할을 합니다. - Ladder 클래스에 사다리 가로선이 연결될 곳을 체크하고 연결하는 함수들을 추가하였습니다.

--- src/User.java
+++ src/User.java
@@ -1,13 +1,14 @@
 public class User {
-    //사다리 게임에 참여하는 플레이어의 이름을 최대 5글자까지 부여할 수 있다.
     public static int MAX_NAME_LIMIT = 5;
     String name;
+    boolean nameLimitExcess = false;
 
     User(String inputName) {
         if (inputName.length() <= MAX_NAME_LIMIT) {
             name = inputName;
             return;
         }
-        name = inputName.substring(5);
+        nameLimitExcess = true;
+        name = inputName.substring(0, 5);
     }
 }
