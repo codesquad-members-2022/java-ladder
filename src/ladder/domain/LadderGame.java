322943fc3396168402da5b34ef78d2cded40bac2
seyeong <seyoung7555@naver.com>
2022-02-18 15:10:20 +0900

    feat: 유저이름별로 결과를 출력하는 기능 구현
    - LadderGame에서 사용자별 결과 출력을 담당합니다.
    - LadderGameManager로부터 이름을 전달받고 해당하는 결과를 출력합니다.
    - all을 전달받은 경우 모든 결과를 출력합니다.
    - 기타 메서드 이름을 수정하였습니다.

--- src/ladder/domain/LadderGame.java
+++ src/ladder/domain/LadderGame.java
@@ -1,25 +1,69 @@
 package ladder.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LadderGame {
     private static final String BLANK = "     ";
     private static final String STEP = "-----";
     private static final String RAIL = "|";
     private static final String LEFT_MARGIN = "  ";
     private static final String DEFAULT_NAME = "men";
+    private static final String GET_ALL_RESULT_KEYWORD = "all";
 
     private final LadderGameInfo ladderGameInfo;
     private final Ladder ladder;
+    private final Map<String, String> resultMap;
 
     public LadderGame(LadderGameInfo ladderGameInfo) {
         this.ladderGameInfo = ladderGameInfo;
         ladder = new Ladder(ladderGameInfo.getCountOfPlayer(), ladderGameInfo.getHeight());
+        resultMap = play();
     }
 
-    public void printLadderGame() {
+    private HashMap<String, String> play() {
+        HashMap<String, String> resultMap = new HashMap<>();
+        String[] playerNames = ladderGameInfo.getPlayerNames();
+        for (int indexOfPlayer = 0; indexOfPlayer < playerNames.length; indexOfPlayer++) {
+            resultMap.put(playerNames[indexOfPlayer], ladderGameInfo.getResults()[ladder.getResult(indexOfPlayer)]);
+        }
+        return resultMap;
+    }
+
+    public void print() {
+        printNewLine();
+        printLadderMessage();
         printUserNames();
         for (Line line : ladder.getLadderArr()) {
             printSingleLine(line);
         }
+        printResult();
+        printNewLine();
+    }
+
+    private void printResultMap() {
+        resultMap.forEach((k, v) -> System.out.println(k + ": " + v));
+    }
+
+    public void printResultMapByName(String userName) {
+        printNewLine();
+        System.out.println("실행 결과");
+        if (userName.equals(GET_ALL_RESULT_KEYWORD)) {
+            printResultMap();
+            return;
+        }
+        System.out.println(resultMap.get(userName));
+    }
+
+    private void printLadderMessage() {
+        System.out.println("사다리 결과");
+    }
+
+    private void printResult() {
+        for (String result : ladderGameInfo.getResults()) {
+            System.out.printf("%5s ", result);
+        }
+        printNewLine();
     }
 
     private void printSingleLine(Line line) {
