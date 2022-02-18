0d8c7b59b9ae40c7f3091f91e3cd3b53431369dc
naneun <yhsep7@gmail.com>
2022-02-18 14:25:03 +0900

    📝 InputViewTest -> InputUtilsTest 테스트 클래스 명 변경

--- src/test/java/application/util/InputUtilsTest.java
+++ src/test/java/application/util/InputUtilsTest.java
@@ -1,21 +1,21 @@
-package application.view;
+package application.util;
 
 import application.domain.ladder.Ladder;
 import application.domain.player.Player;
-import application.util.InputUtils;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-class InputViewTest {
+class InputUtilsTest {
 
     @ParameterizedTest()
     @ValueSource(strings = { "geombong,dukcode,leekm0310,who-hoo,Dave,donggi,Riako"
             , "ge,dukc,leekm,who,Dave,don,Riako", "ge,du,le,wh,Da,do,Ri" })
-
+    @DisplayName("플레이어 이름에서 최대 5글자('..' 채움)를 출력한다.")
     void getPlayers(String input) {
         List<Player> players = InputUtils.getPlayers(input);
         for (Player player : players) {
