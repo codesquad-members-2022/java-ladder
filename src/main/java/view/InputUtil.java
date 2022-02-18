6953f4d10026d54d63455729bb466670c84e5a56
함석호 <kalejuice02@gmail.com>
2022-02-18 12:17:04 +0900

    refactor : InputUtil의 질문을 상수로 변경

--- src/main/java/view/InputUtil.java
+++ src/main/java/view/InputUtil.java
@@ -5,19 +5,22 @@ import java.util.Scanner;
 
 public class InputUtil {
     private static final Scanner sc = new Scanner(System.in);
+    private static final String NAME_INPUT_QUESTION = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String HEIGHT_INPUT_QUESTION = "최대 사다리 높이는 몇 개인가요?";
+    private static final String EXCEPTION_MESSAGE = "참가자의 수가 부족합니다.";
 
     public static String[] getUserNameInput() {
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        System.out.println(NAME_INPUT_QUESTION);
 
         String usernames = sc.nextLine();
         if(!validateUsernameFormat(usernames)){
-            throw new InputMismatchException("참가자의 수가 부족합니다.");
+            throw new InputMismatchException(EXCEPTION_MESSAGE);
         }
         return usernames.split(",");
     }
 
     public static int getLadderSizeInput() {
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        System.out.println(HEIGHT_INPUT_QUESTION);
         return sc.nextInt();
     }
 
