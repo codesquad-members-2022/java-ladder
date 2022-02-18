98cdce0e13c172752aa80673f8aa49b92da1741d
nak253 <nwon4413@naver.com>
2022-02-18 12:16:11 +0900

    Validator클래스의 메서드 depth 길이를 줄임
    - Validator클래스 CheckMaximumNumberCharacters메서드 내부를 메서드 분리를 통해서 depth를 줄였다.

--- src/ladder/Validator.java
+++ src/ladder/Validator.java
@@ -3,10 +3,14 @@ package ladder;
 public class Validator {
     public String[] CheckMaximumNumberCharacters(String[] participantArr, int maxNumCharacters) throws IllegalArgumentException {
         for (String name : participantArr) {
-            if (maxNumCharacters < name.length()) {
-                throw new IllegalArgumentException("참가자 이름이 " + maxNumCharacters + "넘지 않도록 작성하세요.");
-            }
+            checkNameExceedsMaximum(name,maxNumCharacters);
         }
         return participantArr;
+    }
+
+    private void checkNameExceedsMaximum(String name, int maxNumCharacters) throws IllegalArgumentException{
+        if (maxNumCharacters < name.length()) {
+            throw new IllegalArgumentException("참가자 이름이 " + maxNumCharacters + "넘지 않도록 작성하세요.");
+        }
     }
 }
