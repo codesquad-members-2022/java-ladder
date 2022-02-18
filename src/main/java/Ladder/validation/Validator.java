3147cb889e4d37bc91d2463e2b128da54a6ab2f1
WooJin Park <ak2j38@gmail.com>
2022-02-18 12:38:48 +0900

    feature : 이름 유효성 검사 이름 길이 최소조건 추가
    1. 입력 이름값은 최소 1글자 이상이어야한다
    
    end

--- src/main/java/Ladder/validation/Validator.java
+++ src/main/java/Ladder/validation/Validator.java
@@ -6,12 +6,13 @@ public class Validator {
 
     private static final String PLAYER_NAME_DELIMITER = ",";
     private static final int MAX_NAME_LENGTH = 5;
+    private static final int MIN_NAME_LENGTH = 1;
 
     public static boolean isNameMaxLengthFive(String playerNames) {
         String[] splitName = playerNames.split(PLAYER_NAME_DELIMITER);
 
         int satisfiedNameCount = (int) Arrays.stream(splitName)
-                .filter(name -> name.length() <= MAX_NAME_LENGTH)
+                .filter(name -> name.length() <= MAX_NAME_LENGTH && name.length() >= MIN_NAME_LENGTH)
                 .count();
         int allNameCount = splitName.length;
 
