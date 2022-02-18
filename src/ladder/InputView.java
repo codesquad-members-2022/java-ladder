b023218a60df547c79e09a76b89a06405271cd7d
nak253 <nwon4413@naver.com>
2022-02-18 12:10:49 +0900

    클래스 변수 명을 대문자로 변경

--- src/ladder/InputView.java
+++ src/ladder/InputView.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
 
 public class InputView {
     private static final int MAX_NUM_CHARACTERS = 5;
-    private static final String participantsQuestion = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
-    private static final String heightQuestion = "사다리의 높이는 얼마인가요?";
+    private static final String PARTICIPANTS_QUESTION = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String HEIGHT_QUESTION = "사다리의 높이는 얼마인가요?";
     private Scanner scanner;
     private Validator validator;
 
@@ -16,15 +16,15 @@ public class InputView {
         this.scanner = scanner;
         this.validator = new Validator();
     }
-    
+
     public List<String> getParticipants() {
-        System.out.println(participantsQuestion);
+        System.out.println(PARTICIPANTS_QUESTION);
         List<String> participants = parsingParticipants();
         return participants;
     }
 
     public int getLadderHeight() {
-        System.out.println(heightQuestion);
+        System.out.println(HEIGHT_QUESTION);
         return scanner.nextInt();
     }
 
