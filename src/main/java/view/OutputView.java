7b77ea7d89cc7cf1a995b4b836a4bfab3b008fa3
jiwoo <zbqmgldjfh@gmail.com>
2022-02-18 19:17:48 +0900

    feat(OutputView): 결과를 보여주는 outputView
    단순하게 전체 결과를 한번에 출력하도록 하였습니다

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,17 +1,24 @@
 package view;
 
 import domain.Line;
+import domain.ladder.Ladder;
 
 import java.util.List;
 
 public class OutputView {
-    public static void showLadder(List<Line> lines, String[] inputNames) {
+    public static void showLadder(Ladder ladder, String[] inputNames, String[] results) {
+        List<Line> lines = ladder.getLines();
+
         for(String name : inputNames){
             System.out.print(addPadding(name));
         }
         System.out.println();
         for (Line line : lines) {
             line.show();
+        }
+
+        for(int i = 0; i < inputNames.length; i++){
+            System.out.println(inputNames[i] + " : " + results[ladder.run(i)]);
         }
     }
 
