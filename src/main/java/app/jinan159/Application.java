32ac7cde7d818f961c32bcae6bb62d15945ce277
jinan159 <wlsdhks0423@naver.com>
2022-02-20 00:41:37 +0900

    Application 에서 IOException 발생시 출력했던 안내 제거
    IOException 은 명확한 오류인데, 기존 메세지는 오히려 추상적이었습니다.
    오류에 대한 내용은 stackTrace 만으로 충분한 설명이 된다고 생각하여,
    메세지를 제거하였습니다.

--- src/main/java/app/jinan159/Application.java
+++ src/main/java/app/jinan159/Application.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 
 public class Application {
 
-    private static final String ALERT_UNEXPECTED_TERMINATION = "프로그램이 예기치 않게 종료되었습니다.";
     private static final int NAME_LENGTH = 5;
 
     public static void main(String[] args) {
@@ -15,7 +14,6 @@ public class Application {
             game.startGame();
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println(ALERT_UNEXPECTED_TERMINATION);
         }
     }
 
