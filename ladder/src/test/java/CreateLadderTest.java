dde68c7fdaa40f87b2af8a0d6beed582a5e193ae
geombong <shoy1415@naver.com>
2022-02-19 15:12:25 +0900

    test 겹치는 가로 라인 생성 테스트

--- ladder/src/test/java/CreateLadderTest.java
+++ ladder/src/test/java/CreateLadderTest.java
@@ -8,17 +8,12 @@ import model.Line;
 class CreateLadderTest {
 
     @Test
-    @DisplayName("사다리 라인 단순생성 테스트")
+    @DisplayName("사다리 라인 생성 테스트")
     void createLadderLineTest() {
-        String testLine = "|-----|";
-        String testLine2 = "|-----|-----|";
-        String testLine3 = "|-----|-----|-----|";
-        Line line = new Line(2);
-        Line line2 = new Line(3);
-        Line line3 = new Line(4);
-
-        assertThat(testLine).isEqualTo(line.createLine());
-        assertThat(testLine2).isEqualTo(line2.createLine());
-        assertThat(testLine3).isEqualTo(line3.createLine());
+        String testLine = "|-----|-----|";
+        for (int i = 0; i < 50; i++) {
+            Line line = new Line(3);
+            assertThat(testLine).isNotEqualTo(line.toString());
+        }
     }
 }
