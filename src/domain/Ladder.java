6470b41b7bb775ca8951beda2fd1272508bad874
nathan29849 <phs960418@gmail.com>
2022-02-21 00:55:03 +0900

    refactor : Ladder 클래스의 불필요한 인스턴스 변수 삭제 및 생성자 파라미터 변수명 수정
    Ladder 클래스의 numberOfPeople 인스턴스 변수 제거 (그냥 players.size()를 사용하는 것으로 대체)
    Ladder 클래스 생성자 파라미터 변수명을 m->height로 수정

--- src/domain/Ladder.java
+++ src/domain/Ladder.java
@@ -4,18 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ladder {
-    private final int WIDTH = 5;
-    private int numberOfPeople;
     private int heightOfLadder;
     private List<String> players;
     private List<String> results;
     private List<Line> lineLadder;
 
-    public Ladder(List<String> players, List<String> results, int m) {
+    public Ladder(List<String> players, List<String> results, int height) {
         this.players = players;
         this.results = results;
-        this.numberOfPeople = players.size();
-        this.heightOfLadder = m;
+        this.heightOfLadder = height;
         this.init();
     }
 
@@ -26,7 +23,7 @@ public class Ladder {
     private List<Line> make() {
         List<Line> ladder = new ArrayList<>();
         for (int i = 0; i < heightOfLadder; i++) {
-            ladder.add(new Line(numberOfPeople));
+            ladder.add(new Line(players.size()));
         }
         return ladder;
     }
@@ -43,32 +40,6 @@ public class Ladder {
     public List<String> getResultsInfo() {
         // deep copy
         return new ArrayList<>(results);
-    }
-
-    private char transValue(boolean element) {
-        if (element) {
-            return '-';
-        }
-        return ' ';
-    }
-
-    private String putPlayers() {
-        StringBuilder sb = new StringBuilder();
-        for (String player : players) {
-            sb.append(Formatter.checkAndPadding(player, WIDTH));
-            sb.append(" ");
-        }
-        sb.append("\n");
-        return sb.toString();
-    }
-
-    private String putResults() {
-        StringBuilder sb = new StringBuilder();
-        for (String result : results) {
-            sb.append(Formatter.checkAndPadding(result, WIDTH));
-            sb.append(" ");
-        }
-        return sb.toString();
     }
 }
 
