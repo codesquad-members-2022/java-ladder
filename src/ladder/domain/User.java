971153f1f3272febe326354ed56791f405ba21a6
hsjang <hsjang0405@gmail.com>
2022-02-21 01:25:09 +0900

    ♻️ User class 원래이름 정보 유지, 생성자역할을 줄임

--- src/ladder/domain/User.java
+++ src/ladder/domain/User.java
@@ -6,37 +6,34 @@ public class User {
 
 	private static final int MAX_USER_NAME_LENGTH = 5;
 	private static final String BLANK = " ";
-	private final String userName;
+
+	private final String originUserName;
+	private String userName;
 
 	public User(String userName) {
-		this.userName = validateUserName(userName);
-	}
-
-	private String validateUserName(String userName) {
-		userName = userName.trim();
-		return setNameLengthToFive(userName);
-	}
-
-	private String setNameLengthToFive(String userName) {
-		if (userName.length() <= MAX_USER_NAME_LENGTH) {
-			return padding(userName);
-		}
-		return reduceName(userName);
-	}
-
-	private String padding(String userName) {
-		int totalPadding = MAX_USER_NAME_LENGTH - userName.length();
-		int leftPadding = totalPadding / 2;
-		int rightPadding = totalPadding - leftPadding;
-		return BLANK.repeat(leftPadding) + userName + BLANK.repeat(rightPadding);
-	}
-
-	private String reduceName(String userName) {
-		return userName.substring(0, 3) + "..";
+		this.originUserName = userName.trim();
 	}
 
 	public String getUserName() {
-		return userName;
+		return setNameLengthToFive(originUserName);
+	}
+
+	private String setNameLengthToFive(String originUserName) {
+		if (originUserName.length() <= MAX_USER_NAME_LENGTH) {
+			return padding(originUserName);
+		}
+		return reduceName(originUserName);
+	}
+
+	private String padding(String originUserName) {
+		int totalPadding = MAX_USER_NAME_LENGTH - originUserName.length();
+		int leftPadding = totalPadding / 2;
+		int rightPadding = totalPadding - leftPadding;
+		return BLANK.repeat(leftPadding) + originUserName + BLANK.repeat(rightPadding);
+	}
+
+	private String reduceName(String originUserName) {
+		return originUserName.substring(0, 3) + "..";
 	}
 
 	@Override
@@ -48,11 +45,11 @@ public class User {
 			return false;
 		}
 		User user = (User) o;
-		return Objects.equals(userName, user.userName);
+		return Objects.equals(originUserName, user.originUserName);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(userName);
+		return Objects.hash(originUserName);
 	}
 }
