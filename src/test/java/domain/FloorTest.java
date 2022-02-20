ceb56a610793dd746eed274816ceb009b36495f7
Minseok-Choi <cms05041@gmail.com>
2022-02-20 23:30:50 +0900

    test : floor isStep 테스트

--- src/test/java/domain/FloorTest.java
+++ src/test/java/domain/FloorTest.java
@@ -1,6 +1,19 @@
 package domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 public class FloorTest {
 
-
+    @Test
+    @DisplayName("floor에 연결선 확인")
+    void floor_isStep() {
+        Floor floor = new Floor(3, List.of(true,false,false));
+        Assertions.assertThat(floor.isSTEP(0)).isTrue();
+        Assertions.assertThat(floor.isSTEP(1)).isFalse();
+        Assertions.assertThat(floor.isSTEP(1)).isFalse();
+    }
 }
