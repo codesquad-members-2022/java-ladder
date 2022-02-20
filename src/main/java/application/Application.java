fe829b8529c8391046e92c2a68265d0c1fbe1cd6
naneun <yhsep7@gmail.com>
2022-02-20 21:48:00 +0900

    🎨 (Application) run 메소드 로직 분리

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -18,12 +18,24 @@ public class Application {
     }
 
     public void run() {
+        init();
+        start();
+        end();
+    }
+
+    private void init() {
         Players players = new Players(InputUtils.getPlayers(iv.playerNames()));
         int height = iv.ladderHeight();
         Ladder ladder = new Ladder(height, players.getTotalNum() - 1);
         ladderGame = new LadderGame(ladder, players);
         ladderGame.start();
+    }
+
+    private void start() {
         ov.printGame(ladderGame);
+    }
+
+    private void end() {
         iv.close();
     }
 }
