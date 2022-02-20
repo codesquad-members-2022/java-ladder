8ea7e7915c7b92a0cfe82d2e326fedeeee50cd03
PhilSoGooood <peelhw@gmail.com>
2022-02-20 21:46:41 +0900

    리팩토링: 패키지 분리
    패키지 분리에 따라 pakage문과 import문을 수정하였습니다.

--- main/com/view/InputView.java
+++ main/com/view/InputView.java
@@ -1,15 +1,16 @@
-package main.java.view;
+package main.com.view;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import main.java.domain.NameEditor;
+import main.com.domain.NameEditor;
 
 public class InputView {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static final String REQUEST_PLAYER_NAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분) \n> ";
 	private static final String REQUEST_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요? \n> ";
+
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 	public String[] getPlayerName() throws IOException {
 		NameEditor nameEditor = new NameEditor();
