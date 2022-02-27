ea9d71c1ee92e257629f09f055701b8247572ca6
함석호 <kalejuice02@gmail.com>
2022-02-27 16:19:25 +0900

    refactor : ItemCreator 클래스의 createResultMap 메서드 매개변수 타입을 List로 변경

--- src/main/java/domain/item/ItemCreator.java
+++ src/main/java/domain/item/ItemCreator.java
@@ -1,10 +1,11 @@
 package domain.item;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ItemCreator {
-    public static Map<Integer, Item> createResultMap(String[] results) {
+    public static Map<Integer, Item> createResultMap(List<String> results) {
         int i = 0;
         Map<Integer, Item> map = new HashMap<>();
         for (String result : results) {
